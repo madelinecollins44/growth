e326fa3b2d62678142a4db21d1703e56d200ed23
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-11-08 14:03:39 -0500

    Update opp_sizing.sql

--- web_conversions/opp_sizing.sql
+++ web_conversions/opp_sizing.sql
@@ -85,4 +85,14 @@ group by all
 --ENTRY VS EXIT POINTS
 ------------------------------------------------------------
 --landing events
+select
+  landing_event,
+  count(distinct visit_id) as traffic,
+  count(distinct case when converted > 0 then visit_id end) as converted_visits,
+  sum(total_gms) as total_gms
+from 
+  etsy-data-warehouse-prod.weblog.visits v
+where _date >= current_date-30
+group by all 
+
 --referrers to listing page
