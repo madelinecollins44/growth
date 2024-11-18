530a2ba4c6e3649d38d78a57578bcc674f3d6a4e
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-11-18 14:28:31 -0500

    Update goals_opp_sizing.sql

--- web_conversions/opp_sizing/goals_opp_sizing.sql
+++ web_conversions/opp_sizing/goals_opp_sizing.sql
@@ -1,4 +1,14 @@
 -------------------------------------------------------
+--GLOBAL COVERAGE
+-------------------------------------------------------
+select
+  count(distinct visit_id) as total_visits,
+  sum(total_gms) as gms
+from etsy-data-warehouse-prod.weblog.visits
+where _date >= current_date-30
+group by all
+
+-------------------------------------------------------
 --LISTING LANDINGS
 -------------------------------------------------------
 
