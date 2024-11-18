e1a7919e1d04ab72461a3f523544d3527bcb8591
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-11-18 14:31:19 -0500

    Update goals_opp_sizing.sql

--- web_conversions/opp_sizing/goals_opp_sizing.sql
+++ web_conversions/opp_sizing/goals_opp_sizing.sql
@@ -17,3 +17,23 @@ group by all
 -------------------------------------------------------
 --SHOP HOME VISITS 
 -------------------------------------------------------
+with shop_home_visits as (
+select
+  distinct visit_id
+from 
+  etsy-data-warehouse-prod.weblog.events
+where 
+  _date >= current_date-30
+  and event_type in ('shop_home')
+)
+select
+  count(distinct a.visit_id) as shop_home_visits,
+  sum(total_gms) as shop_home_gms
+from 
+  shop_home_visits a
+inner join 
+  etsy-data-warehouse-prod.weblog.visits b using (visit_id)
+where 
+  b._date >= current_date-30
+  and b.platform in ('mobile_web','desktop')
+group by all
