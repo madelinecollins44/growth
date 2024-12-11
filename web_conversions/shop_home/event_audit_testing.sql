69d204a115bcc9562adcfcb864bcf69d1d8ce620
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-12-11 16:09:21 -0500

    Update event_audit_testing.sql

--- web_conversions/shop_home/event_audit_testing.sql
+++ web_conversions/shop_home/event_audit_testing.sql
@@ -15,3 +15,29 @@ where
   and v.platform in ('mobile_web','desktop')
 -- and v.platform in ('boe')
 group by all
+
+
+--scroll depth
+select
+  event_type,
+  count(distinct visit_id) as visits
+from 
+    etsy-data-warehouse-prod.weblog.events e  
+inner join 
+    etsy-data-warehouse-prod.weblog.visits v using (visit_id)
+where 
+  v._date >= current_date-30
+  and v.platform in ('mobile_web','desktop')
+  and event_type in (
+    'shop_home_announcement_section_seen', -- optional, fires top of page 
+    'shop_home_branding_section_seen', -- top of page
+    'shop_home_listings_section_seen', -- listing section, middle of page
+    'shop_home_listing_grid_seen'-- middle
+    'shop_home_reviews_section_top_seen', -- bottom
+    'shop_home_reviews_section_seen',-- bottom
+    'shop_home_about_section_seen',-- bottom
+    'shop_home_policies_section_seen',-- bottom
+    'shop_home_updates_section_seen', -- is this optional?
+    'shop_home_faqs_section_seen',-- bottom
+)
+group by all
