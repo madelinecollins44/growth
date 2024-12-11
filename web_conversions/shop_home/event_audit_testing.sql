e0a49f73a5298aaa8efa64a845d8a475a92de30b
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-12-11 16:10:30 -0500

    Update event_audit_testing.sql

--- web_conversions/shop_home/event_audit_testing.sql
+++ web_conversions/shop_home/event_audit_testing.sql
@@ -3,20 +3,6 @@
 ----Scroll depth, clicks, etc
 ----Segment by visitors that purchase in-session vs. not, purchase something from the shop vs. not
 ---------------------------------------------------------------------------------------------------------------------------------------------
-
-select
-  event_type,
-  count(distinct visit_id) as visits
-from etsy-data-warehouse-prod.weblog.events e  
-inner join etsy-data-warehouse-prod.weblog.visits v using (visit_id)
-where 
-  v._date >= current_date-30
-  and event_type like ('shop_home%')
-  and v.platform in ('mobile_web','desktop')
--- and v.platform in ('boe')
-group by all
-
-
 --scroll depth
 select
   event_type,
@@ -41,3 +27,5 @@ where
     'shop_home_faqs_section_seen',-- bottom
 )
 group by all
+
+--clicks 
