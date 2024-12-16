93b8aeee39d652240444fb46c49156eed1051473
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-12-16 15:07:45 -0500

    Update events_and_engagements.sql

--- web_conversions/shop_home/events_and_engagements.sql
+++ web_conversions/shop_home/events_and_engagements.sql
@@ -55,6 +55,35 @@ order by 2 desc
 --NAVIGATION
 ---------------------------------------------------------------------------------------------------------------------------------------------
 -- how many shops opted in to all of the optional fields?
+with shop_basics as (
+select
+  sd.shop_id,
+  sum(case when sd.branding_option is not null then 1 else 0 end) as branding_banner,
+  sum(case when sd.message is not null then 1 else 0 end) as annoucement, 
+  sum(case when ss.shop_id is not null then 1 else 0 end) as shop_sections,
+  sum(case when faq.shop_id is not null then 1 else 0 end) as faq_section,
+  sum(case when ssi.shop_id is not null then 1 else 0 end) as updates,
+  sum(case when spd.shop_id is not null then 1 else 0 end) as seller_details,
+  sum(case when sset.shop_id is not null and sset.name in ('machine_translation') and sset.value in ('off') then 1 else 0 end) as machine_translation,
+  sum(case when sset.shop_id is not null and sset.name in ('custom_orders_opt_in') and sset.value in ('t') then 1 else 0 end) as custom_orders,
+  sum(case when sset.shop_id is not null and sset.name in ('hide_shop_home_page_sold_items') and sset.value in ('t') then 1 else 0 end) as show_sold_items,
+  sum(case when smpo.shop_id is not null then 1 else 0 end) as offers_shop_coupon 
+from 
+  etsy-data-warehouse-prod.etsy_shard.shop_data sd 
+left join 
+  etsy-data-warehouse-prod.etsy_shard.shop_sections ss using (shop_id)
+left join 
+  etsy-data-warehouse-prod.etsy_shard.shop_frequently_asked_questions faq using (shop_id)
+left join 
+  (select * from etsy-data-warehouse-prod.etsy_shard.shop_share_items where is_deleted <> 1) ssi using (shop_id)
+left join 
+  etsy-data-warehouse-prod.etsy_shard.shop_seller_personal_details spd using (shop_id)
+left join 
+  etsy-data-warehouse-prod.etsy_shard.shop_settings sset using (shop_id)
+left join 
+  etsy-data-warehouse-prod.etsy_shard.seller_marketing_promoted_offer smpo using (shop_id)
+group by all 
+)
 
 --scroll depth
 select
