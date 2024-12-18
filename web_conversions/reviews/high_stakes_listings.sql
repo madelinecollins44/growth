c36a236f559efd06985a12d94bcb709a684d0cc9
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-12-18 16:46:25 -0500

    Update high_stakes_listings.sql

--- web_conversions/reviews/high_stakes_listings.sql
+++ web_conversions/reviews/high_stakes_listings.sql
@@ -138,3 +138,27 @@ select * from etsy-data-warehouse-prod.weblog.events where visit_id in ('qPReCCn
 -- 2024-12-16	1734307200	qPReCCnSjYilsQLDYmHiMu5aUXko.1734374482054.1	listing_page_reviews_seen	224	http://www.etsy.com/listing/1737486091/18k-gold-filled-wide-bangle-gold?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=gold+bangles&ref=sc_gallery-1-9&pro=1&sts=1&plkey=8d5bf5ac3f5fbc1d1a98187d499a0dc659d3cd13%3A1737486091	https://www.etsy.com/search?q=gold%20bangles&ref=search_bar&explicit=1&max=15	sc_gallery-1-9	0	0	0					0	3	3	1734374588444
 -- 2024-12-16	1734307200	qPReCCnSjYilsQLDYmHiMu5aUXko.1734374482054.1	listing_page_reviews_seen	256	http://www.etsy.com/listing/1737486091/18k-gold-filled-wide-bangle-gold?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=gold+bangles&ref=sc_gallery-1-9&pro=1&sts=1&plkey=8d5bf5ac3f5fbc1d1a98187d499a0dc659d3cd13%3A1737486091	https://www.etsy.com/search?q=gold%20bangles&ref=search_bar&explicit=1&max=15	sc_gallery-1-9	0	0	0					0	3	3	1734374621156
 -- 2024-12-16	1734307200	qPReCCnSjYilsQLDYmHiMu5aUXko.1734374482054.1	listing_page_reviews_seen	272	http://www.etsy.com/listing/1737486091/18k-gold-filled-wide-bangle-gold?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=gold+bangles&ref=sc_gallery-1-9&pro=1&sts=1&plkey=8d5bf5ac3f5fbc1d1a98187d499a0dc659d3cd13%3A1737486091	https://www.etsy.com/search?q=gold%20bangles&ref=search_bar&explicit=1&max=15	sc_gallery-1-9	0	0	0					0	3	3	1734374627185
+
+
+---------CEHCK TO MAKE SURE VISITS MATCH UP W TOTAL REVIEWS SEEN 
+	with seen_reviews as (
+select
+	date(_partitiontime) as _date,
+	visit_id,
+  regexp_extract(beacon.loc, r'listing/(\d+)') as listing_id,
+  count(visit_id) as reviews_event_seen,
+from
+	`etsy-visit-pipe-prod.canonical.visit_id_beacons`
+where
+	date(_partitiontime) >= current_date-2
+	and beacon.event_name = "listing_page_reviews_seen"
+group by all 
+)
+select * from seen_reviews where visit_id in ('ZpLqvzSRiyJUz1sgFN5SndAMp2HO.1734443446941.2')
+-- _date	visit_id	listing_id	reviews_event_seen
+-- 2024-12-17	ZpLqvzSRiyJUz1sgFN5SndAMp2HO.1734443446941.2	1753543369	1
+-- 2024-12-17	ZpLqvzSRiyJUz1sgFN5SndAMp2HO.1734443446941.2	1776573163	2
+-- 2024-12-17	ZpLqvzSRiyJUz1sgFN5SndAMp2HO.1734443446941.2	1229511942	1
+
+select count(distinct url) from etsy-data-warehouse-prod.weblog.events where visit_id in ('ZpLqvzSRiyJUz1sgFN5SndAMp2HO.1734443446941.2') and event_type in ('listing_page_reviews_seen')
+--3
