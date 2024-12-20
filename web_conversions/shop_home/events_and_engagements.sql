12ff0a1b52a1ab16383451c8400ac064c13166d3
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-12-20 11:02:27 -0500

    Update events_and_engagements.sql

--- web_conversions/shop_home/events_and_engagements.sql
+++ web_conversions/shop_home/events_and_engagements.sql
@@ -138,7 +138,65 @@ select
   sum(unfavorite_events) as unfavorite_events,
 from shop_metrics
 
-----Review stars at top of page
+----Review stars at top of page, what is the distro of ratings across visited shop homes 
+with visit_shop_homes as (
+select
+   -- date(_partitiontime) as _date, 
+  beacon.event_name, 
+  (select value from unnest(beacon.properties.key_value) where key = "shop_id") as shop_id, 
+  count(visit_id) as views, 
+  count(distinct visit_id) as visits
+from
+  `etsy-visit-pipe-prod.canonical.visit_id_beacons`
+where
+  date(_partitiontime) >= current_date-7
+  and (beacon.event_name in ('shop_home'))
+group by all
+)
+, shop_reviews as (
+select
+  shop_id,
+  count(distinct transaction_id) as transactions,
+  count(distinct buyer_user_id) as buyers,
+  avg(rating) as average_rating,
+  count(case when rating = 0 then transaction_id end) as reviews_w_ratings_of_0,
+  count(case when rating = 1 then transaction_id end) as reviews_w_ratings_of_1,
+  count(case when rating = 2 then transaction_id end) as reviews_w_ratings_of_2,
+  count(case when rating = 3 then transaction_id end) as reviews_w_ratings_of_3,
+  count(case when rating = 4 then transaction_id end) as reviews_w_ratings_of_4,
+  count(case when rating = 5 then transaction_id end) as reviews_w_ratings_of_5,
+  count(case when seller_feedback != '' then transaction_id end) as reviews_w_seller_feedback
+from 
+  etsy-data-warehouse-prod.etsy_shard.shop_transaction_review
+where 
+  is_deleted = 0 --  only includes active reviews 
+  and language in ('en') -- only english reviews
+group by all
+)
+select
+  count(distinct v.shop_id) as unique_shops_visited,
+  sum(views) as shop_home_pageviews,
+  sum(transactions) as total_reviews,
+  avg(transactions) as avg_reviews,
+  avg(average_rating) as average_rating,
+  -- sum(reviews_w_ratings_of_0) as reviews_w_ratings_of_0,
+  sum(reviews_w_ratings_of_1) as reviews_w_ratings_of_1,
+  sum(reviews_w_ratings_of_2) as reviews_w_ratings_of_2,
+  sum(reviews_w_ratings_of_3) as reviews_w_ratings_of_3,
+  sum(reviews_w_ratings_of_4) as reviews_w_ratings_of_4,
+  sum(reviews_w_ratings_of_5) as reviews_w_ratings_of_5,
+  -- avg(reviews_w_ratings_of_0) as avg_reviews_w_ratings_of_0,
+  avg(reviews_w_ratings_of_1) as avg_reviews_w_ratings_of_1,
+  avg(reviews_w_ratings_of_2) as avg_reviews_w_ratings_of_2,
+  avg(reviews_w_ratings_of_3) as avg_reviews_w_ratings_of_3,
+  avg(reviews_w_ratings_of_4) as avg_reviews_w_ratings_of_4,
+  avg(reviews_w_ratings_of_5) as avg_reviews_w_ratings_of_5,
+from 
+  visit_shop_homes v
+left join 
+  shop_reviews r 
+    on v.shop_id=cast(r.shop_id as string)
+	
 ----See more description link seen / See more description link clicked
 
 ----Seller people link
