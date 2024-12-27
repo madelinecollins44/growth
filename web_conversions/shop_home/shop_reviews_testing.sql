4d1c9263313309d734d462f7c61dbcba09ba5034
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-12-27 13:33:55 -0500

    Create shop_reviews_testing.sql

---
+++ web_conversions/shop_home/shop_reviews_testing.sql
@@ -0,0 +1,18 @@
+-- how many shops get visits? make sure this matches up with the final code is getting
+with visit_shop_homes as (
+select
+   -- date(_partitiontime) as _date, 
+  beacon.event_name, 
+  (select value from unnest(beacon.properties.key_value) where key = "shop_id") as shop_id, 
+  count(visit_id) as views, 
+  count(distinct visit_id) as visits
+from
+  `etsy-visit-pipe-prod.canonical.visit_id_beacons`
+where
+  date(_partitiontime) >= current_date-14
+  and beacon.event_source in ('web')
+  and (beacon.event_name in ('shop_home'))
+group by all
+)
+select count(distinct shop_id) from visit_shop_homes
+---7080135
