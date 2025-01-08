0b0e5f3da022759be0bd516ffbca52a653ba0468
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-08 15:18:32 -0500

    Update web_listings_reviews.sql

--- web_conversions/opp_sizing/web_listings_reviews.sql
+++ web_conversions/opp_sizing/web_listings_reviews.sql
@@ -21,3 +21,38 @@ from
 where
   _date >= current_date-30
   and platform in ('desktop')
+
+-- % of lv with review attributes
+with listing_views as (
+select
+  listing_id,
+  count(visit_id) as listing_views
+from 
+  etsy-data-warehouse-prod.analytics.listing_views
+where
+  _date >= current_date-30
+  and platform in ('desktop')
+group by all
+)
+, reviews as (
+select
+  listing_id,
+  sum(has_review) as has_review,
+  sum(has_image) as has_image,
+  sum(has_video) as has_video,
+from 
+  etsy-data-warehouse-prod.rollups.transaction_reviews  
+group by all
+)
+select
+  sum(lv.listing_views) as total_listing_views,
+  count(distinct case when r.has_review > 0 then lv.listing_id end) listings_w_review,
+  count(distinct case when r.has_image > 0 then lv.listing_id end) listings_w_image,
+  count(distinct case when r.has_video > 0 then lv.listing_id end) listings_w_video,
+  sum(case when r.has_review > 0 then listing_views end) has_review_lv,
+  sum(case when r.has_image > 0 then listing_views end) has_image_lv,
+  sum(case when r.has_video > 0 then listing_views end) has_video_lv,
+from
+  listing_views lv
+left join 
+  reviews r using (listing_id)
