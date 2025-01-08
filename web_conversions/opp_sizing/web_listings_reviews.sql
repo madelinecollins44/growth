4efe0f651ae30115d606dcf642e4d876508b124a
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-08 15:10:37 -0500

    Update web_listings_reviews.sql

--- web_conversions/opp_sizing/web_listings_reviews.sql
+++ web_conversions/opp_sizing/web_listings_reviews.sql
@@ -1,10 +1,23 @@
 --total desktop visits in the last 30 days
 select 
-  -- case when user_id is not null then 1 else 0 end as is_signed_in,
-  count(distinct visit_id) 
+  count(distinct visit_id) as total_visits,
+  count(distinct case when platform in ('desktop') then visit_id end) as desktop_traffic,
+  count(distinct case when user_id is not null and platform in ('desktop') then visit_id end) as signedin_desktop_traffic,
+  count(distinct case when user_id is null and platform in ('desktop') then visit_id end) as signedout_desktop_traffic,
 from 
   etsy-data-warehouse-prod.weblog.visits 
 where 
  _date >= current_date-30
  and platform in ('desktop')
  group by all 
+
+  
+--desktop listing views
+select
+  count(distinct visit_id) as visits_w_lv,
+  count(visit_id) as listing_views
+from 
+  etsy-data-warehouse-prod.analytics.listing_views
+where
+  _date >= current_date-30
+  and platform in ('desktop')
