f7a20752b3ee150440bc9ff1c61fbf630841eb19
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-08 11:54:01 -0500

    Update high_stakes_listings_lead.sql

--- web_conversions/reviews/high_stakes_listings_lead.sql
+++ web_conversions/reviews/high_stakes_listings_lead.sql
@@ -1,3 +1,125 @@
+--- get listing views + review seen events
+with listing_events as (
+select
+	date(_partitiontime) as _date,
+	visit_id,
+  sequence_number,
+	beacon.event_name as event_name,
+  coalesce((select value from unnest(beacon.properties.key_value) where key = "listing_id"), regexp_extract(beacon.loc, r'listing/(\d+)')) as listing_id 
+from
+	`etsy-visit-pipe-prod.canonical.visit_id_beacons`
+where
+	date(_partitiontime) >= current_date-30
+	and beacon.event_name in ("listing_page_reviews_seen","view_listing")
+	-- and beacon.event_source in ('web')
+group by all 
+)
+, ordered_events as (
+select
+	_date,
+	visit_id,
+	listing_id,
+  sequence_number,
+	event_name,
+	lead(event_name) over (partition by visit_id, listing_id order by sequence_number) as next_event,
+	lead(sequence_number) over (partition by visit_id, listing_id order by sequence_number) as next_sequence_number
+from 
+	listing_events
+)
+, listing_views as (
+select
+	visit_id,
+	listing_id,
+  sequence_number,
+	case when next_event in ('listing_page_reviews_seen') then 1 else 0 end as saw_reviews
+from
+	ordered_events
+where 
+	event_name in ('view_listing')
+)
+, lv_stats as (
+select
+	lv.listing_id,
+	case
+  	when price_usd > 100 then 'high stakes'
+  	else 'low stakes'
+  end as listing_type,
+	case when a.listing_id is null then 1 else 0 end as missing_in_analytics,
+	count(lv.visit_id) as listing_views,
+	count(a.visit_id) as a_listing_views,
+	count(case when saw_reviews = 1 then lv.visit_id end) as views_and_reviews_seen,
+	sum(case when saw_reviews = 1 then purchased_after_view end) as saw_reviews_and_purchased,
+	sum(purchased_after_view) as purchases
+from 
+	listing_views lv
+left join 
+	etsy-data-warehouse-prod.analytics.listing_views a
+		on lv.listing_id=cast(a.listing_id as string)
+		and lv.visit_id=a.visit_id
+		and lv.sequence_number=a.sequence_number		
+where a._date >=current_date-30
+group by all
+)
+, number_of_reviews as (
+select
+  _date,
+  listing_id,
+  max(listing_rating_count) as listing_rating_count,
+   max(shop_rating_count) as shop_rating_count,
+from 
+  etsy-data-warehouse-prod.analytics.listing_views
+where 
+  _date >= current_date-30
+group by all
+qualify row_number() over (partition by listing_id order by _date desc) = 1 -- takes # of reviews from day the query is run 
+)
+, review_score as (
+select
+  listing_id,
+  sum(has_review) as reviews,
+  sum(case when rating = 5 then 1 else 0 end) as count_5_star,
+  sum(case when rating = 4 then 1 else 0 end) as count_4_star,
+  sum(case when rating = 3 then 1 else 0 end) as count_3_star,
+  sum(case when rating = 2 then 1 else 0 end) as count_2_star,
+  sum(case when rating = 1 then 1 else 0 end) as count_1_star,
+  avg(rating) as avg_rating
+from `etsy-data-warehouse-prod.rollups.transaction_reviews` r
+-- where date(review_date) >= "2023-01-01"
+group by all 
+)
+select
+  rv.listing_type,
+	sum(missing_in_analytics) as errors,
+  count(distinct rv.listing_id) as listings_viewed,
+  sum(rv.listing_views) as listing_views,
+  sum(rv.purchases) as purchases,
+  sum(rv.saw_reviews_and_purchased) as saw_reviews_and_purchased,
+  --listing page 
+  sum(rv.views_and_reviews_seen) as views_and_reviews_seen,
+  sum(n.listing_rating_count) as lp_listing_reviews,
+  sum(n.shop_rating_count) as lp_shop_reviews,
+  avg(n.listing_rating_count) as avg_lp_listing_reviews,
+  avg(n.shop_rating_count) as avg_lp_shop_reviews,
+  --transaction reviews 
+  sum(reviews) as transaction_reviews,
+  avg(reviews) as avg_transaction_reviews,
+  sum(count_5_star) as count_5_star,
+  sum(count_4_star) as count_4_star,
+  sum(count_3_star) as count_3_star,
+  sum(count_2_star) as count_2_star,
+  sum(count_1_star) as count_1_star,
+  avg(avg_rating) as avg_rating
+from
+  lv_stats rv
+left join 
+  number_of_reviews n 
+		on rv.listing_id = cast(n.listing_id as string)
+left join 
+  review_score s 		
+		on rv.listing_id = cast(s.listing_id as string)
+group by all
+
+	
 ------------------------------------------------------------------------
 --TESTING WITH SECOND VERSION-- USING LEAD AND LAG VERSIONS
 ------------------------------------------------------------------------
