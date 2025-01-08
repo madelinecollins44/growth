7c09777b306562c92dbf14a9186a38c1d85ceac4
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-08 15:05:27 -0500

    Update web_listings_reviews.sql

--- web_conversions/opp_sizing/web_listings_reviews.sql
+++ web_conversions/opp_sizing/web_listings_reviews.sql
@@ -1,1 +1,10 @@
---
+--total desktop visits in the last 30 days
+select 
+  -- case when user_id is not null then 1 else 0 end as is_signed_in,
+  count(distinct visit_id) 
+from 
+  etsy-data-warehouse-prod.weblog.visits 
+where 
+ _date >= current_date-30
+ and platform in ('desktop')
+ group by all 
