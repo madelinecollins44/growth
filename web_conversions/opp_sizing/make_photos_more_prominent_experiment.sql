f18002ecf9a277001650ce332fdd32757d3e481c
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-10 10:48:47 -0500

    Update make_photos_more_prominent_experiment.sql

--- web_conversions/opp_sizing/make_photos_more_prominent_experiment.sql
+++ web_conversions/opp_sizing/make_photos_more_prominent_experiment.sql
@@ -9,3 +9,16 @@ select
 from 
   etsy-data-warehouse-prod.weblog.visits
 where _date >= current_date-30
+
+-- visits with lp review seen event 
+select
+  count(distinct visit_id) as total_visits,
+  count(distinct case when platform in ('desktop') then visit_id end) as desktop_visits,  
+  count(distinct case when platform in ('mobile_web') then visit_id end) as mweb_visits
+from 
+  etsy-data-warehouse-prod.weblog.visits v
+inner join 
+  etsy-data-warehouse-prod.weblog.events e using (visit_id)
+where 
+  v._date >= current_date-30
+  and e.event_type in ('listing_page_reviews_seen')
