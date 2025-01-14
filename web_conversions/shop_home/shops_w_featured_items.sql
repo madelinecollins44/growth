fbe9e596950eda692e8c8bec2b95269e7b3822a0
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-14 12:43:51 -0500

    Update shops_w_featured_items.sql

--- web_conversions/shop_home/shops_w_featured_items.sql
+++ web_conversions/shop_home/shops_w_featured_items.sql
@@ -2,8 +2,9 @@
 --What % shops have featured items in categories, as opposed to just featured items? 
 -- shop w/ categorized featured items:https://www.etsy.com/shop/MOENAStudio
 -- shop w/ uncategorized featured items: https://www.etsy.com/shop/TiradiaCork
+-- shops w both: https://www.etsy.com/shop/TheInclusiveGiftco
 ----------------------------------------------------------------------------------------------------------------
-with shop_w_categories as (
+with features_sections as (
 select
   distinct shop_id
 from 
@@ -11,13 +12,31 @@ from
 where 
   featured_rank != -1 -- looks at shops with categorized featured listings
 )
+, features_item as (
 select
-  count(distinct sb.shop_id) as active_shops,
-  count(distinct swc.shop_id) as active_shops_w_categorized_features_listings
+  distinct shop_id 
+from etsy-data-warehouse-prod.etsy_shard.listings
+where 
+  featured_rank != - 1
+)
+, shop_agg as (
+  select
+  sb.shop_id, 
+  case when fs.shop_id is not null then 1 else 0 end as features_section, 
+  case when fi.shop_id is not null then 1 else 0 end as features_item, 
 from 
   etsy-data-warehouse-prod.rollups.seller_basics sb
 left join 
-  shop_w_categories swc using (shop_id)
+  features_sections fs using (shop_id)
+left join 
+  features_item fi on sb.shop_id=fi.shop_id
 where
-  active_seller_status = 1 -- only active sellers 
-
+  sb.active_seller_status = 1 -- only active sellers 
+)
+select
+  count(distinct shop_id) as total_active_shops,
+  sum(features_section) as shops_w_featured_section,
+  sum(features_item) as shops_w_featured_items,
+  count(distinct case when features_section >0 and features_item >0 then shop_id end) as shops_w_both_features
+from 
+  shop_agg
