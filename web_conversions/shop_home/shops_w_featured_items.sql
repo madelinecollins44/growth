d10090a7482a40c93309c37e4220db4de558c65a
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-14 14:24:53 -0500

    Update shops_w_featured_items.sql

--- web_conversions/shop_home/shops_w_featured_items.sql
+++ web_conversions/shop_home/shops_w_featured_items.sql
@@ -4,20 +4,40 @@
 -- shop w/ uncategorized featured items: https://www.etsy.com/shop/TiradiaCork, 25742633
 -- shops w both: https://www.etsy.com/shop/TheInclusiveGiftco, 30378868
 ----------------------------------------------------------------------------------------------------------------
-with features_sections as (
+with featured_count as (
 select
-  distinct shop_id
+  shop_id,
+  case 
+    when featured_products_layout = 0 then 4
+    when featured_products_layout = 1 then 5
+    else 0
+  end as featured_products_layout -- this is used to figure out how many featured items each shop has
 from 
-  etsy-data-warehouse-prod.etsy_shard.shop_sections
-where 
-  featured_rank != -1 -- looks at shops with categorized featured listings
+  etsy-data-warehouse-prod.etsy_shard.shop_data
+)
+, features_sections as (
+select 
+    distinct shop_id
+  from 
+    etsy-data-warehouse-prod.etsy_shard.shop_sections
+  left join 
+    featured_count using (shop_id)
+  where 
+    featured_rank != -1 -- looks at shops with categorized featured listings
+    AND featured_rank < featured_products_layout
 )
 , features_item as (
 select
   distinct shop_id 
-from etsy-data-warehouse-prod.etsy_shard.listings
+from 
+  etsy-data-warehouse-prod.etsy_shard.listings
+left join 
+  featured_count using (shop_id)
 where 
-  featured_rank != - 1
+  featured_rank != - 1 and
+  featured_rank < featured_products_layout
+  and is_available != 0 
+  and is_displayable != 0
 )
 , shop_agg as (
   select
@@ -39,39 +59,4 @@ select
   count(distinct case when features_section=0 and features_item > 0 then shop_id end) as shops_w_only_feature_item,
   count(distinct case when features_section > 0 and features_item > 0 then shop_id end) as shops_w_both_features
 from 
-  shop_agg
-
---testing
-with features_sections as (
-select
-  distinct shop_id
-from 
-  etsy-data-warehouse-prod.etsy_shard.shop_sections
-where 
-  featured_rank != -1 -- looks at shops with categorized featured listings
-)
-, features_item as (
-select
-  distinct shop_id 
-from etsy-data-warehouse-prod.etsy_shard.listings
-where 
-  featured_rank != - 1
-)
-  select
-  sb.shop_id, 
-  case when fs.shop_id is not null then 1 else 0 end as features_section, 
-  case when fi.shop_id is not null then 1 else 0 end as features_item, 
-from 
-  etsy-data-warehouse-prod.rollups.seller_basics sb
-left join 
-  features_sections fs using (shop_id)
-left join 
-  features_item fi on sb.shop_id=fi.shop_id
-where
-  sb.active_seller_status = 1 -- only active sellers 
-  and sb.shop_id in (31547401,30378868,25742633)
-
--- shop_id	features_section	features_item
--- 31547401	1	1
--- 30378868	1	1
--- 25742633	0	1
+  shop_agg;
