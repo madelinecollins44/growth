4d6e08904759534ef91a6143d347b1f752f8fdeb
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-16 18:26:13 -0500

    Update review_window.sql

--- web_conversions/reviews/review_window.sql
+++ web_conversions/reviews/review_window.sql
@@ -14,3 +14,23 @@ from
   reviews
 group by all 
 order by 1 desc
+
+
+--trying this w review_start date
+with reviews as (
+select
+  transaction_id,
+  date_diff(date(review_date),date(review_start),day) as transaction_to_review,
+  -- date_diff(date(review_date),date(shipped_date),day) as shipped_to_review
+from 
+  etsy-data-warehouse-prod.rollups.transaction_reviews
+where has_review =1
+)
+select 
+  transaction_to_review,
+  count(distinct transaction_id) as transactions
+from 
+  reviews
+where transaction_to_review > 100
+group by all 
+order by 1 asc
