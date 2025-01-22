7c236bcb299d9820265b4d943c9e32aee497e4bb
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-22 12:16:51 -0500

    Update review_window.sql

--- web_conversions/reviews/review_window.sql
+++ web_conversions/reviews/review_window.sql
@@ -1,32 +1,32 @@
-with reviews as (
-select
-  transaction_id,
-  date_diff(date(review_date),date(review_start),day) as days_till_review,
-from 
-  etsy-data-warehouse-prod.rollups.transaction_reviews
-where has_review =1
-)
+
 select 
  CASE 
-    when days_till_review = 0 then 'First eligible day'
-    when days_till_review = 1 then 'Day after first eligible day'
-    when days_till_review between 2 and 6 then 'Week 1: 2-6 days'
-    when days_till_review between 7 and 13 then 'Week 2: 7-13 days'
-    when days_till_review between 14 and 20 then 'Week 3: 14-20 days'
-    when days_till_review between 21 and 27 then 'Week 4: 21-27 days'
-    when days_till_review between 28 and 59 then 'Month 2: 28-59 days'
-    when days_till_review between 60 and 89 then 'Month 3: 60-89 days'
-    when days_till_review between 90 and 119 then 'Month 4: 90-119 days'
-    when days_till_review between 120 and 149 then 'Month 5: 120-149 days'
-    ELSE 'Over 150 days'
+    when time_until_review < 0 then 'Before first eligible day'
+    when time_until_review = 0 then 'First eligible day'
+    when time_until_review = 1 then 'Day after first eligible day'
+    when time_until_review between 2 and 6 then 'Week 1: 2-6 days'
+    when time_until_review between 7 and 13 then 'Week 2: 7-13 days'
+    when time_until_review between 14 and 20 then 'Week 3: 14-20 days'
+    when time_until_review between 21 and 27 then 'Week 4: 21-27 days'
+    when time_until_review between 28 and 59 then 'Month 2: 28-59 days'
+    when time_until_review between 60 and 89 then 'Month 3: 60-89 days'
+    when time_until_review between 90 and 119 then 'Month 4: 90-119 days'
+    when time_until_review between 120 and 149 then 'Month 5: 120-149 days'
+    when time_until_review >= 155940131130 then 'Over 150 days'
+    when review_start is null then 'Review start is null'
+    ELSE 'Error'
   end as time_eligible,
-  count(distinct transaction_id) as transactions
+  count(distinct transaction_id) as total_transactions 
 from 
-  reviews
+  etsy-data-warehouse-prod.rollups.transaction_reviews
+where 
+  has_review =1
 group by all 
 order by 1 asc
 
 
+
+  
   --testing to see how many transactions are missing review_start but have time_until_review
 select 
   count(distinct transaction_id) as total_reviews,
