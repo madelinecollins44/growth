df1ce5271168fab9b46574373319006ecaf4addd
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-22 13:00:21 -0500

    Update review_window.sql

--- web_conversions/reviews/review_window.sql
+++ web_conversions/reviews/review_window.sql
@@ -32,6 +32,41 @@ where
 group by all 
 order by 1 asc
 
+-- using combo of review start + shipping date to find review eligible date
+ with reviews as (
+select
+  transaction_id,
+  coalesce(date(review_start), date(shipped_date)) as review_start_date, -- if a transaction does not have a review start, then use the shipping date 
+  datetime_diff(date(review_date), (coalesce(date(review_start), date(shipped_date))), day) AS time_until_review, -- find date diff calcs for these situations 
+from 
+  etsy-data-warehouse-prod.rollups.transaction_reviews
+where 
+  has_review =1
+group by all 
+)
+select 
+ CASE 
+    when time_until_review < 0 then 'Before first eligible day'
+    when time_until_review = 0 then 'First eligible day'
+    when time_until_review = 1 then 'Day after first eligible day'
+    when time_until_review between 2 and 6 then 'Week 1: 2-6 days'
+    when time_until_review between 7 and 13 then 'Week 2: 7-13 days'
+    when time_until_review between 14 and 20 then 'Week 3: 14-20 days'
+    when time_until_review between 21 and 27 then 'Week 4: 21-27 days'
+    when time_until_review between 28 and 59 then 'Month 2: 28-59 days'
+    when time_until_review between 60 and 89 then 'Month 3: 60-89 days'
+    when time_until_review between 90 and 119 then 'Month 4: 90-119 days'
+    when time_until_review between 120 and 149 then 'Month 5: 120-149 days'
+    when time_until_review >= 155940131130 then 'Over 150 days'
+    when review_start_date is null then 'Review start is null'
+    ELSE 'Error'
+  end as time_eligible,
+  count(distinct transaction_id) as total_transactions 
+from 
+  reviews
+group by all 
+order by 1 asc
+ 
 --------------------------------------------------------
 --understanding null review start dates
 --------------------------------------------------------
