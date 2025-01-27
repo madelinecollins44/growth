d533bb5aed54a0d44f1f894ca6eb264ee1837c15
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-27 14:00:17 -0500

    Update buyer_together_module_counts.sql

--- web_conversions/reviews/buyer_together_module_counts.sql
+++ web_conversions/reviews/buyer_together_module_counts.sql
@@ -1,1 +1,8 @@
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--There is a 'buy together, get...' module that sometimes surfaces, I think it can be a sale module and sometimes it surfaces as free shipping, but we were wondering what % of listings have it. 
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- free shipping: https://www.etsy.com/listing/1728673967/macrame-table-placemats-natural-white
+-------- event name: recommendations_module_seen, property: lp_free_shipping_bundle
 
+-- get x% off: https://www.google.com/url?q=https://www.etsy.com/listing/1581168558/upcycled-fabric-handmade-boho-round&sa=D&source=editors&ust=1738007917145372&usg=AOvVaw3umrGY6l-Did44U-kSBTL2
+-------- event name: mix_and_match_v2_bundle_lp_shown
