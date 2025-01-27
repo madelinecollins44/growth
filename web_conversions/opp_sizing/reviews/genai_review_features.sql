b84bd9bdff9e62678502a77b261cb47399fe2514
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-27 12:55:03 -0500

    Update genai_review_features.sql

--- web_conversions/opp_sizing/reviews/genai_review_features.sql
+++ web_conversions/opp_sizing/reviews/genai_review_features.sql
@@ -1,4 +1,4 @@
 -------------------------------------------------------------------------------
 -- what does engagement + review distribution look like by top category? 
 -------------------------------------------------------------------------------
--- active listings, listing views, gms coverage, unique visits, conversions, total reviews, review_seen events, review_seen visits, review distrbution by rating
+-- active listings, high vs low stakes, listing views, gms coverage, unique visits, conversions, total reviews, review_seen events, review_seen visits, review distrbution by rating
