f14419a4183e77e6c7e327fa55e0b380ecd8283b
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-27 14:58:17 -0500

    Update buyer_together_module_counts.sql

--- web_conversions/reviews/buyer_together_module_counts.sql
+++ web_conversions/reviews/buyer_together_module_counts.sql
@@ -6,3 +6,61 @@
 
 -- get x% off: https://www.google.com/url?q=https://www.etsy.com/listing/1581168558/upcycled-fabric-handmade-boho-round&sa=D&source=editors&ust=1738007917145372&usg=AOvVaw3umrGY6l-Did44U-kSBTL2
 -------- event name: mix_and_match_v2_bundle_lp_shown
+
+
+with listing_events as (
+select
+	date(_partitiontime) as _date,
+	visit_id,
+  platform,
+  sequence_number,
+	beacon.event_name as event_name,
+  coalesce((select value from unnest(beacon.properties.key_value) where key = "listing_id"), regexp_extract(beacon.loc, r'listing/(\d+)')) as listing_id 
+from
+	`etsy-visit-pipe-prod.canonical.visit_id_beacons`
+inner join 
+  etsy-data-warehouse-prod.weblog.visits using (visit_id)
+where
+	date(_partitiontime) >= current_date-30
+  and _date >= current_date-30
+	and 
+    (beacon.event_name in ("view_listing","mix_and_match_v2_bundle_lp_shown")
+    or beacon.event_name in ("recommendations_module_seen") and (select value from unnest(beacon.properties.key_value) where key = "module_placement") in ("lp_free_shipping_bundle"))
+  and platform in ('mobile_web','desktop')
+group by all 
+)
+, ordered_events as (
+select
+	_date,
+	visit_id,
+  platform,
+	listing_id,
+  sequence_number,
+	event_name,
+	lead(event_name) over (partition by visit_id, listing_id order by sequence_number) as next_event,
+	lead(sequence_number) over (partition by visit_id, listing_id order by sequence_number) as next_sequence_number
+from 
+	listing_events
+)
+, listing_views as (
+select
+	visit_id,
+  platform,
+	listing_id,
+  sequence_number,
+	case when next_event in ('recommendations_module_seen') then 1 else 0 end as saw_free_shipping_bundle, 
+  case when next_event in ('mix_and_match_v2_bundle_lp_shown') then 1 else 0 end as saw_discount_bundle
+from
+	ordered_events
+where 
+	event_name in ('view_listing')
+)
+select
+  count(visit_id) as listing_views,
+  count(case when saw_free_shipping_bundle > 0 then visit_id end) as lv_w_free_shipping_bundle,
+  count(case when saw_discount_bundle > 0 then visit_id end) as lv_w_discount_bundle,
+  count(distinct listing_id) as listings_viewed,
+  count(distinct case when saw_free_shipping_bundle > 0 then listing_id end) as listings_w_free_shipping_bundle,
+  count(distinct case when saw_discount_bundle > 0 then listing_id end) as listings_w_discount_bundle,
+from 
+  listing_views
