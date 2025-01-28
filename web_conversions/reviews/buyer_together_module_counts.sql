8344eea462d9701a272db7379bd0f28190d59a25
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-28 10:45:27 -0500

    Update buyer_together_module_counts.sql

--- web_conversions/reviews/buyer_together_module_counts.sql
+++ web_conversions/reviews/buyer_together_module_counts.sql
@@ -7,28 +7,28 @@
 -- get x% off: https://www.google.com/url?q=https://www.etsy.com/listing/1581168558/upcycled-fabric-handmade-boho-round&sa=D&source=editors&ust=1738007917145372&usg=AOvVaw3umrGY6l-Did44U-kSBTL2
 -------- event name: mix_and_match_v2_bundle_lp_shown
 
--- create or replace table etsy-data-warehouse-dev.madelinecollins.bundle_modules_listing_events as (
--- select
--- 	date(_partitiontime) as _date,
--- 	visit_id,
---   platform,
---   sequence_number,
--- 	beacon.event_name as event_name,
---   coalesce((select value from unnest(beacon.properties.key_value) where key = "listing_id"), regexp_extract(beacon.loc, r'listing/(\d+)')) as listing_id 
--- from
--- 	`etsy-visit-pipe-prod.canonical.visit_id_beacons`
--- inner join 
---   etsy-data-warehouse-prod.weblog.visits using (visit_id)
--- where
--- 	date(_partitiontime) >= current_date-30
---   and _date >= current_date-30
--- 	and 
---     (beacon.event_name in ("view_listing","mix_and_match_v2_bundle_lp_shown") -- listing view, discount bundle
---     or (beacon.event_name in ("recommendations_module_seen") and (select value from unnest(beacon.properties.key_value) where key = "module_placement") in ("lp_free_shipping_bundle"))) -- free shipping
---   and platform in ('mobile_web','desktop')
--- group by all 
--- ); 
-with ordered_events as (
+with listing_events as (
+select
+	date(_partitiontime) as _date,
+	visit_id,
+  platform,
+  sequence_number,
+	beacon.event_name as event_name,
+  coalesce((select value from unnest(beacon.properties.key_value) where key = "listing_id"), regexp_extract(beacon.loc, r'listing/(\d+)')) as listing_id 
+from
+	`etsy-visit-pipe-prod.canonical.visit_id_beacons`
+inner join 
+  etsy-data-warehouse-prod.weblog.visits using (visit_id)
+where
+	date(_partitiontime) >= current_date-14
+  and _date >= current_date-14
+	and 
+    (beacon.event_name in ("view_listing","mix_and_match_v2_bundle_lp_shown") -- listing view, discount bundle
+    or (beacon.event_name in ("recommendations_module_seen") and (select value from unnest(beacon.properties.key_value) where key = "module_placement") in ("lp_free_shipping_bundle"))) -- free shipping
+  and platform in ('mobile_web','desktop')
+group by all 
+) 
+, ordered_events as (
 select
 	_date,
 	visit_id,
@@ -39,7 +39,7 @@ select
 	lead(event_name) over (partition by visit_id, listing_id order by sequence_number) as next_event,
 	lead(sequence_number) over (partition by visit_id, listing_id order by sequence_number) as next_sequence_number
 from 
-	etsy-data-warehouse-dev.madelinecollins.bundle_modules_listing_events
+	listing_events
 )
 , listing_views as (
 select
