9de489990e841d33ba24ed08c4109b00f47bf193
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-28 14:41:06 -0500

    Update genai_review_features.sql

--- web_conversions/opp_sizing/reviews/genai_review_features.sql
+++ web_conversions/opp_sizing/reviews/genai_review_features.sql
@@ -71,7 +71,91 @@ group by all
 
 --adding in top category + price 
 create or replace table etsy-data-warehouse-dev.madelinecollins.genai_category_highstakes_listings_opp_size as (
-with web_gms as (
+with listing_events as (
+select
+	date(_partitiontime) as _date,
+	visit_id,
+  platform,
+  sequence_number,
+	beacon.event_name as event_name,
+  coalesce((select value from unnest(beacon.properties.key_value) where key = "listing_id"), regexp_extract(beacon.loc, r'listing/(\d+)')) as listing_id 
+from
+	`etsy-visit-pipe-prod.canonical.visit_id_beacons`
+inner join 
+  etsy-data-warehouse-prod.weblog.visits using (visit_id)
+where
+	date(_partitiontime) >= current_date-30
+  and _date >= current_date-30
+	and beacon.event_name in ("listing_page_reviews_seen","view_listing")
+  and platform in ('mobile_web','desktop')
+group by all 
+)
+, ordered_events as (
+select
+	_date,
+	visit_id,
+	a.listing_id,
+	b.top_category,
+  sequence_number,
+	event_name,
+	lead(event_name) over (partition by visit_id, a.listing_id order by sequence_number) as next_event,
+	lead(sequence_number) over (partition by visit_id, a.listing_id order by sequence_number) as next_sequence_number
+from 
+	listing_events a 
+inner join   
+  etsy-data-warehouse-prod.rollups.active_listing_basics b  
+    on cast(a.listing_id as int64)=b.listing_id
+)
+, listing_views as (
+select
+	visit_id,
+	listing_id,
+  top_category,
+  sequence_number,
+	case when next_event in ('listing_page_reviews_seen') then 1 else 0 end as saw_reviews
+from
+	ordered_events
+where 
+	event_name in ('view_listing')
+)
+, listing_views_and_reviews_seen as (
+select
+	cast(lv.listing_id as int64) as listing_id,
+  top_category,
+	case
+  	when coalesce((p.price_usd/100), a.price_usd) > 100 then 'high stakes'
+  	else 'low stakes'
+  end as listing_type,
+	count(lv.visit_id) as listing_view_count,	
+  sum(purchased_after_view) as purchases,
+	count(case when saw_reviews = 1 then lv.visit_id end) as views_and_reviews_seen,
+	sum(case when saw_reviews = 1 then purchased_after_view end) as saw_reviews_and_purchased,
+from 
+  listing_views lv 
+left join 
+	etsy-data-warehouse-prod.analytics.listing_views a
+		on lv.listing_id=cast(a.listing_id as string)
+		and lv.visit_id=a.visit_id
+		and lv.sequence_number=a.sequence_number	
+left join 
+  etsy-data-warehouse-prod.listing_mart.listings p 
+    on cast(p.listing_id as string)=lv.listing_id
+where a._date >=current_date-30
+group by all
+)
+, reviews as (
+select
+  listing_id,
+  count(transaction_id) as review_count
+from  
+  etsy-data-warehouse-prod.rollups.transaction_reviews
+where 
+  has_review > 0  
+  and language in ('en')
+group by all
+order by 2 desc
+)
+, web_gms as (
 select
   listing_id,
   sum(trans_gms_net) as gms_net
@@ -86,50 +170,16 @@ where
 	and t.date >= current_date - 365
 group by all 
 )
-, listing_views as (
-select
-  a.listing_id,
-  case
-  	when coalesce((p.price_usd/100), a.price_usd) > 100 then 'high stakes'
-  	else 'low stakes'
-  end as listing_type,
-  b.top_category,
-  count(a.visit_id) as listing_views,
-  sum(a.purchased_after_view) as purchases
-from 
-  etsy-data-warehouse-prod.rollups.active_listing_basics b
-left join 
-  etsy-data-warehouse-prod.listing_mart.listings p using (listing_id)
-left join  
-    etsy-data-warehouse-prod.analytics.listing_views a
-      on a.listing_id=b.listing_id
-where 
-  _date >= current_date-30
-  and a.platform in ('mobile_web','desktop') -- only web lv 
-group by all 
-)
-, reviews as (
-select
-  listing_id,
-  count(transaction_id) as review_count
-from  
-  etsy-data-warehouse-prod.rollups.transaction_reviews
-where 
-  has_review > 0  
-  and language in ('en')
-group by all
-order by 2 desc
-)
 select
   lv.listing_id,
-  listing_type,
-  top_category,
   review_count as reviews,
-  sum(listing_views) as listing_views,
+  sum(listing_view_count) as listing_views,
   sum(purchases) as purchases,
+  sum(views_and_reviews_seen) as views_and_reviews_seen,
+	sum(saw_reviews_and_purchased) as saw_reviews_and_purchased,
   sum(gms_net) as gms_net,
 from 
-  listing_views lv
+  listing_views_and_reviews_seen lv
 left join 
   reviews 
     on lv.listing_id=reviews.listing_id
@@ -137,23 +187,6 @@ left join
   web_gms on lv.listing_id=web_gms.listing_id
 group by all
 );
-
--------------------------------------------------------------------------------
--- what does engagement + review distribution look like by top category? 
--------------------------------------------------------------------------------
--- active listings, high vs low stakes, listing views, gms coverage, unique visits, conversions, total reviews, review_seen events, review_seen visits, review distrbution by rating
-with active_listing_views as (
-select
-  listing_id,
-  price,
-  views,
-  unique_views,
-  purchases,
-  gms (?)
-  )
-, reviews as (
-)
-, reviews_seen 
 
 -------------------------------------------------------------------------------
 -- testing
