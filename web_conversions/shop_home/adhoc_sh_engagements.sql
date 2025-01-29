be898187cffbd8e92b0ab12d3078f24c75a772c3
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-29 16:43:35 -0500

    Update adhoc_sh_engagements.sql

--- web_conversions/shop_home/adhoc_sh_engagements.sql
+++ web_conversions/shop_home/adhoc_sh_engagements.sql
@@ -2,14 +2,13 @@
 select
 		beacon.event_name as event_name,
 		(select value from unnest(beacon.properties.key_value) where key = "tab") as tab_clicked,
-    count(visit_id) as actions
+    count(visit_id) as clicks,
+    count(distinct visit_id) as visits,
 	from
 		`etsy-visit-pipe-prod.canonical.visit_id_beacons`
 	where
 		date(_partitiontime) >= current_date-30
     and beacon.event_source in ('web')
 	and
-		beacon.event_name in ("shop_home_nav_clicked", -- click on one of the tabs, tab property will say which one
-                          'shop_home', -- shop home primary page
-                          'shop_home_announcement_view') -- clicked into annoucement section
+		beacon.event_name in ("shop_home_nav_clicked", 'shop_home','shop_home_announcement_view')
   group by all
