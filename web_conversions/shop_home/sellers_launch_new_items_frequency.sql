49b49c2abcdc82b40972cf21d6565a07a78cf042
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-01-31 13:21:22 -0500

    Update sellers_launch_new_items_frequency.sql

--- web_conversions/shop_home/sellers_launch_new_items_frequency.sql
+++ web_conversions/shop_home/sellers_launch_new_items_frequency.sql
@@ -1,8 +1,16 @@
 ----------------------------------------------------------------------------------------------------------------
 --How often do sellers launch new items? What % of sellers have added a new item in the last 30 days?
 ----------------------------------------------------------------------------------------------------------------
-
 with active_shops as (
+select
+  shop_id, 
+  seller_tier_new,
+  sum(active_listings) as active_listings
+from 
+  etsy-data-warehouse-prod.rollups.seller_basics
+where 	
+  active_seller_status = 1 -- only active sellers
+group by all 
 )
 , visited_shops
 , listings 
