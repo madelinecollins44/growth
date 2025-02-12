fef4b2ffb8a1222a69a0c5d76ce13a7cd42860d2
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-02-12 16:53:53 -0500

    Update free_shipping_listings.sql

--- web_conversions/shop_home/browsability_improvements/free_shipping_listings.sql
+++ web_conversions/shop_home/browsability_improvements/free_shipping_listings.sql
@@ -1,9 +1,22 @@
+with shop_sections as (
 select 
-shop_name,
-count(distinct name) 
-from etsy-data-warehouse-prod.etsy_shard.shop_sections
-inner join etsy-data-warehouse-prod.rollups.seller_basics using (shop_id)
+  shop_id,
+  count(distinct name) as sections
+from 
+  etsy-data-warehouse-prod.etsy_shard.shop_sections
+inner join 
+  etsy-data-warehouse-prod.rollups.seller_basics using (shop_id)
 where
-  active_listing_count >0 
+  active_listing_count > 0 
   and active_seller_status = 1
 group by all
+)
+, shop_visits,
+, shop_gms (
+select
+  seller_user_id,
+  sum(gms_net)
+from 
+  etsy-data-warehouse-prod.transaction_mart.transactions_gms_by_trans
+where date >= current_date-365
+)
