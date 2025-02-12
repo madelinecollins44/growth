f257150fa956a79f4f0929e887ef0f808b2f391e
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-02-12 12:04:07 -0500

    Create view_many_listings_same_shop.sql

---
+++ web_conversions/shop_home/browsability_improvements/view_many_listings_same_shop.sql
@@ -0,0 +1,7 @@
+//--"What % of users on web view multiple listings from the same shop from Shop home? 
+//-- Use case - As a buyer viewing multiple listings from the same shop, I am able to easily access my most recently viewed listings from that shop."
+
+---------------------------------------------------------------------------------------------------------------------------------------
+-- all visits to view multiple listings from the same shop
+---- does not exclude sellers or self-visits
+---------------------------------------------------------------------------------------------------------------------------------------
