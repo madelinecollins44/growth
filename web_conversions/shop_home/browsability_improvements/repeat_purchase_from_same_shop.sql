e9765b275cffdda116c1c0d7a651dc9334ff2ac7
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-02-12 15:13:19 -0500

    Update repeat_purchase_from_same_shop.sql

--- web_conversions/shop_home/browsability_improvements/repeat_purchase_from_same_shop.sql
+++ web_conversions/shop_home/browsability_improvements/repeat_purchase_from_same_shop.sql
@@ -9,22 +9,20 @@ select
   , a.is_guest 
   , a.listing_id 
   , a.seller_user_id
+  , sb.shop_id
   , a.receipt_id 
   , a.transaction_id
-    , a.quantity
-    , a.usd_price 
-    , a.trans_shipping_price
-    , a.usd_subtotal_price
-    , vv.total_gms as visit_gms
+    -- , a.quantity
+    -- , a.usd_price 
+    -- , a.trans_shipping_price
+    -- , a.usd_subtotal_price
+    -- , vv.total_gms as visit_gms
   , case when vv.platform = 'boe' then vv.event_source else vv.platform end as purchased_platform
-  , vv.detected_region
-  , la.full_path
-  , max(vl.is_vintage) as is_vintage
+  -- , vv.detected_region
 from `etsy-data-warehouse-prod.transaction_mart.all_transactions` a 
 join `etsy-data-warehouse-prod.transaction_mart.transactions_visits` v on a.transaction_id = v.transaction_id 
 join `etsy-data-warehouse-prod.weblog.visits` vv on vv.visit_id = v.visit_id  
-left join `etsy-data-warehouse-prod.listing_mart.listing_attributes` la on a.listing_id = la.listing_id
-left join `etsy-data-warehouse-prod.rollups.vintage_listings` vl on a.listing_id = vl.listing_id
+join etsy-data-warehouse-prod.rollups.seller_basics sb on sb.user_id=a.seller_user_id
 where 1=1
   and vv._date >= current_date-365
   and vv.platform in ('boe','desktop','mobile_web')
