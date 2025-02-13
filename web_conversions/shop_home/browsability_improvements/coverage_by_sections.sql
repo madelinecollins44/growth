54f300f8748abb8405da2fac07abebd3c52ccf27
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-02-13 14:22:13 -0500

    Update coverage_by_sections.sql

--- web_conversions/shop_home/browsability_improvements/coverage_by_sections.sql
+++ web_conversions/shop_home/browsability_improvements/coverage_by_sections.sql
@@ -29,7 +29,7 @@ where v._date >= current_date-30
 ----------------------------------------------------------------------
 -- VISIT, GMS, CONVERSION COVERAGE OF VISITS THAT VIEWED SHOP HOME
 ----------------------------------------------------------------------
--- criteria: visits must have been the shop home page for their conversion and gms to contribute to that shop
+---- What do CR & GMS coverage & visits look like for shops with sections vs shops without? 
 -- create or replace table etsy-data-warehouse-dev.madelinecollins.web_shop_visits as (
 -- select
 --   platform,
@@ -50,51 +50,71 @@ where v._date >= current_date-30
 -- group by all
 -- );
 
-with gms_and_conversion as ( -- get gms/ conversion from any visit that viewed shop home in last 30 days 
+with shop_sections as ( -- active shops + if they have sections with listings in them 
+select 
+  b.shop_id,
+  shop_name,
+  b.user_id as seller_user_id, 
+  count(distinct case when active_listing_count > 0 then s.name end) as sections
+from 
+  etsy-data-warehouse-prod.rollups.seller_basics b
+left join 
+  etsy-data-warehouse-prod.etsy_shard.shop_sections s using (shop_id)
+where
+  active_seller_status = 1
+group by all
+)
+, shop_visits as ( -- visits that viewed a shop on web
+select
+  seller_user_id,
+  visit_id,
+  count(sequence_number) as views
+from  
+  etsy-data-warehouse-dev.madelinecollins.web_shop_visits 
+where 
+  platform in ('desktop','mobile_web')
+group by all 
+)
+, shop_gms_converts as ( -- get all shop info at the visit_id level
 select
   g.seller_user_id,
-  v.visit_id as converted_visit, -- if a visit made a purchase from that store, they converted 
-  sum(gms_net) as gms_from_visit
-from 
-  etsy-data-warehouse-dev.madelinecollins.web_shop_visits sv
-inner join 
-  etsy-data-warehouse-prod.transaction_mart.transactions_visits v using (visit_id) -- only looking at transaction data from visits that viewed shop
+  v.visit_id, 
+  sum(gms_net) as gms_net,
+from
+  etsy-data-warehouse-prod.transaction_mart.transactions_visits v 
 inner join
   etsy-data-warehouse-prod.transaction_mart.transactions_gms_by_trans g
     on g.transaction_id=v.transaction_id 
 where 
-  g.date >= current_date-30 -- this will alsohave to be the last 30 days, since looking at a visit level 
+  g.date >= current_date-30 -- this will also have to be the last 30 days, since looking at a visit level 
 group by all 
 )
-, shop_sections as (
-select 
-  shop_id,
-  user_id as seller_user_id, 
-  count(distinct name) as sections
+, shop_level as ( -- get everything to seller_user_id level 
+select
+  v.seller_user_id,
+  count(distinct v.visit_id) as visits,
+  count(distinct gc.visit_id) as converts, 
+  sum(gms_net) as gms_net
 from 
-  etsy-data-warehouse-prod.etsy_shard.shop_sections
-inner join 
-  etsy-data-warehouse-prod.rollups.seller_basics using (shop_id)
-where
-  active_listing_count > 0 
-  and active_seller_status = 1
-group by all
+  shop_visits v
+left join 
+  shop_gms_converts gc 
+    on v.seller_user_id= cast(gc.seller_user_id as string)
+    and v.visit_id=gc.visit_id
+group by all 
 )
 select
-  case when s.seller_user_id is not null then 1 else 0 end as has_section,
-  count(distinct v.visit_id) as total_visits,
-  count(distinct gc.converted_visit) as converted_visits,
-  sum(gms_from_visit) as total_gms
+  case when s.sections > 0 then 1 else 0 end as has_sections,
+  count(distinct l.seller_user_id) as visited_shops,
+  sum(visits) as visits,
+  sum(converts) as converts,
+  sum(gms_net) as gms_net
 from 
-  etsy-data-warehouse-dev.madelinecollins.web_shop_visits v
+  shop_level l
 left join 
-  gms_and_conversion gc
-    on v.seller_user_id= cast(gc.seller_user_id as string)
-    and v.visit_id=gc.converted_visit
-left join 
-  shop_sections s
-    on cast(s.seller_user_id as string) = v.seller_user_id 
-group by all
+  shop_sections s 
+    on l.seller_user_id= cast(s.seller_user_id as string)
+group by all 
 
 ----------------------------------------------------------------------
 -- GMS FROM SHOP (including from traffic that did not see shop home) 
