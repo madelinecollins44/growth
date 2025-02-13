4779bae12977c577cce3d08461311ee219e5a8c3
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-02-13 13:03:59 -0500

    Update free_shipping_listings.sql

--- web_conversions/shop_home/browsability_improvements/free_shipping_listings.sql
+++ web_conversions/shop_home/browsability_improvements/free_shipping_listings.sql
@@ -1,6 +1,6 @@
 select
-  case when regexp_contains(title, r'\b(free shipping|shipping fee|express shipping|express delivery|upgrade shipping)\b') then 1 else 0 end as exact_shipping_title,
-  case when regexp_contains(title, r'\b(shipping|delivery)\b') then 1 else 0 end as general_shipping_title,
+  case when regexp_contains(lower(title), r'\b(free shipping|shipping fee|express shipping|express delivery|upgrade shipping)\b') then 1 else 0 end as exact_shipping_title,
+  case when regexp_contains(lower(title), r'\b(shipping|delivery)\b') then 1 else 0 end as general_shipping_title,
   count(distinct listing_id) as listings,
   count(distinct shop_id) as shops,
   count(v.visit_id) as listing_views
@@ -28,5 +28,21 @@ where
   v._date >= current_date-30
   and  regexp_contains(title, r'\b(free shipping|shipping fee|express shipping|express delivery|upgrade shipping)\b') 
 group by all 
-
 limit 5
+
+--testing specific listings i know should count
+select
+  case when regexp_contains(lower(title), r'\b(free shipping|shipping fee|express shipping|express delivery|upgrade shipping)\b') then 1 else 0 end as exact_shipping_title,
+  case when regexp_contains(lower(title)
+, r'\b(shipping|delivery)\b') then 1 else 0 end as general_shipping_title,
+  count(distinct listing_id) as listings,
+  count(distinct shop_id) as shops,
+  count(v.visit_id) as listing_views
+from 
+  etsy-data-warehouse-prod.rollups.active_listing_basics b
+left join 
+  etsy-data-warehouse-prod.analytics.listing_views v using (listing_id)
+where 
+  v._date >= current_date-30
+and listing_id = 1800528551 
+group by all 
