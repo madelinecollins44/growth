2a48980311e06894fa05775e9afd38c18c64bf3c
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-02-13 12:45:36 -0500

    Update free_shipping_listings.sql

--- web_conversions/shop_home/browsability_improvements/free_shipping_listings.sql
+++ web_conversions/shop_home/browsability_improvements/free_shipping_listings.sql
@@ -1,60 +1,8 @@
-with shop_sections as (
-select 
-  shop_id,
-  user_id as seller_user_id, 
-  count(distinct name) as sections
-from 
-  etsy-data-warehouse-prod.etsy_shard.shop_sections
-inner join 
-  etsy-data-warehouse-prod.rollups.seller_basics using (shop_id)
-where
-  active_listing_count > 0 
-  and active_seller_status = 1
-group by all
-)
-, shop_gms as ( -- gms for sellers over last 
 select
-  seller_user_id,
-  sum(gms_net) as total_gms
+  case when regexp_contains(title, r'\b(free shipping|shipping fee|express shipping|express delivery|upgrade shipping)\b') then 1 else 0 end as exact_shipping_title,
+  case when regexp_contains(title, r'\b(shipping|delivery)\b') then 1 else 0 end as general_shipping_title,
+  count(distinct listing_id) as listings,
+  count(distinct shop_id) as shops
 from 
-  etsy-data-warehouse-prod.transaction_mart.transactions_gms_by_trans
-where 
-  date >= current_date-365
+  etsy-data-warehouse-prod.rollups.active_listing_basics
 group by all 
-)
-, shop_visits as (
-select
-  beacon.event_name, 
-  (select value from unnest(beacon.properties.key_value) where key = "shop_shop_id") as shop_id, 
-  (select value from unnest(beacon.properties.key_value) where key = "shop_id") as seller_user_id, 
-  count(visit_id) as views, 
-  count(distinct visit_id) as visits
-from
-  `etsy-visit-pipe-prod.canonical.visit_id_beacons`
-where
-  date(_partitiontime) >= current_date-30
-  and beacon.event_source in ('web')
-  and (beacon.event_name in ('shop_home'))
-group by all
-)
-, total_visits as (
-select 
-  count(distinct visit_id) as total_visits,
-  sum(total_gms) as total_gms
-from `etsy-data-warehouse-prod.weblog.visits`
-where _date >= current_date-30
-and platform in ('desktop', 'mobile_web')
-)
-select
-  case when sections > 0 then 1 else 0 end as has_sections,
-  count(distinct v.seller_user_id) as visited_shops,
-  sum(g.total_gms) as shop_gms
-from 
-  shop_visits v
-left join 
-  shop_gms g 
-    on v.seller_user_id=cast(g.seller_user_id as string)
-left join 
-  shop_sections s
-    on v.seller_user_id=cast(s.seller_user_id as string)
-group by all
