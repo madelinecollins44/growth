0d31a1e74b41689f2d14dcb3c9702298b0ff7633
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-02-13 14:44:58 -0500

    Update coverage_by_sections.sql

--- web_conversions/shop_home/browsability_improvements/coverage_by_sections.sql
+++ web_conversions/shop_home/browsability_improvements/coverage_by_sections.sql
@@ -178,7 +178,7 @@ group by all
 -- TESTING
 -------------------------------------------------------
 
-  -- TEST 1: see how accurate beacons count are vs events
+/* TEST 1: see how accurate beacons count are vs events*/ 
 select visit_id, count(visit_id) from etsy-data-warehouse-dev.madelinecollins.web_shop_visits
 group by all order by 2 desc limit 5
 -- -- visit_id	f0_
@@ -193,49 +193,7 @@ where event_type in ('shop_home')
 and visit_id in ('1jFttPKXT0fRycXZM4FWopJ-bBsz.1739258732525.14','1jFttPKXT0fRycXZM4FWopJ-bBsz.1739255034370.12','HAZGJf8xWWeiEUqd_JXqx9fpDHGI.1739188263736.11','Cq3mhqPxGRxIT2fsjw9Ny_P-spBa.1738907747378.10')
 group by all
 
--- TEST 2: how many visits went to a shop with a section, shop without a section, or both?
-  
--- what % of visited shops have sections?
-with shop_sections as (
-select 
-  shop_id,
-  user_id as seller_user_id, 
-  count(distinct name) as sections
-from 
-  etsy-data-warehouse-prod.etsy_shard.shop_sections
-inner join 
-  etsy-data-warehouse-prod.rollups.seller_basics using (shop_id)
-where
-  active_listing_count > 0 
-  and active_seller_status = 1
-group by all
-)
-, visit_status as (
-select
-  v.visit_id,
-  coalesce(max(case when s.shop_id IS NOT NULL THEN 1 ELSE 0 END), 0) AS has_section,
-  coalesce(max(case when s.shop_id IS NULL THEN 1 ELSE 0 END), 0) AS no_section
-from 
-  etsy-data-warehouse-dev.madelinecollins.web_shop_visits v
-left join 
-  shop_sections s 
-    on v.seller_user_id = cast(s.seller_user_id as string)
-  group by all )
--- order by has_section, no_section desc limit 5
-SELECT 
-    CASE 
-        WHEN has_section = 1 AND no_section = 0 THEN 'Only Shops with Sections'
-        WHEN has_section = 0 AND no_section = 1 THEN 'Only Shops without Sections'
-        WHEN has_section = 1 AND no_section = 1 THEN 'Both Types of Shops'
-    END AS visit_category,
-    COUNT(DISTINCT visit_id) AS visit_count
-FROM visit_status
-GROUP BY all;
-
-
-
-
--- what % of visited shops have sections?
+/* TEST 2: how many visits went to a shop with a section, shop without a section, or both?*/ 
 with shop_sections as (
 select 
   shop_id,
@@ -271,3 +229,41 @@ SELECT
     COUNT(DISTINCT visit_id) AS visit_count
 FROM visit_status
 GROUP BY all;
+
+
+
+/* TEST 3: check on shop level (looking at most visited shops/ most converts) */ 
+-- shop_name	        shop_id	    seller_user_id	    has_sections	visited_shops	visits	converts	gms_net
+-- Beniouraincarpets	7746216	      16263321	          1	      z1	263747	29	18647.07692312
+-- jmdceramicsart	18990914	183816016	1	1	238216	2	217.69230769
+-- BeadLetsbyNatalie	32078771	539190449	1	1	183179	0	
+-- CYOstore	25571082	371259652	1	1	168237	3	318
+-- XtraDark	57192578	1041209412	0	1	166053	2	132.92307692
+
+-- check visits
+
+-- check gms/ converts 
+select
+  g.seller_user_id,
+  count(distinct v.visit_id) as converted_visits, -- if a visit made a purchase from that store, they converted 
+  count(distinct case when sv.visit_id is not null then v.visit_id end) as converted_shop_visits,
+  sum(gms_net) as gms_net,
+  sum(distinct case when sv.visit_id is not null then gms_net end) as converted_shop_gms
+from
+  etsy-data-warehouse-prod.transaction_mart.transactions_visits v 
+inner join
+  etsy-data-warehouse-prod.transaction_mart.transactions_gms_by_trans g -- only looking at gms from visits that viewed  
+    on g.transaction_id=v.transaction_id 
+left join 
+  etsy-data-warehouse-dev.madelinecollins.web_shop_visits sv
+    on sv.visit_id=v.visit_id
+    and sv.seller_user_id=cast(g.seller_user_id as string)
+where 
+  g.date >= current_date-30 -- this will also have to be the last 30 days, since looking at a visit level 
+  and g.seller_user_id in (16263321,183816016,539190449,371259652,1041209412)
+group by all 
+-- seller_user_id	converted_visits	converted_shop_visits	gms_net	converted_shop_gms
+-- 1041209412	2	2	398.76923076	66.46153846
+-- 183816016	2	2	217.69230769	217.69230769
+-- 16263321	29	29	44663.23076931	17672.92307696
+-- 371259652	3	3	1936.15384616	318
