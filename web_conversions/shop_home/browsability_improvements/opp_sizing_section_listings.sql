1731ff3dca0e0d6d6e70547912a37827b2a08184
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-02-14 11:16:21 -0500

    Update opp_sizing_section_listings.sql

--- web_conversions/shop_home/browsability_improvements/opp_sizing_section_listings.sql
+++ web_conversions/shop_home/browsability_improvements/opp_sizing_section_listings.sql
@@ -1,3 +1,8 @@
+-- need to find visits to shop home from listingp pages (grab listing_id here)
+-- look to see if that listing_id is in a section 
+
+
+-- events
 --shop_home_section_select: section_id, num_sections
 --neu_favorite_click: click listings
 --favorite_toast_notification_shown: see favorite confirmation
