bba1aebc98e7fc841be508da67f84e679cd76001
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-02-18 16:53:16 -0500

    Update favorited_listing.sql

--- web_conversions/shop_home/browsability_improvements/favorited_listing.sql
+++ web_conversions/shop_home/browsability_improvements/favorited_listing.sql
@@ -1,1 +1,14 @@
 
+with favorited_listings as (
+select 
+  mapped_user_id,
+  listing_id,
+  max(update_date) as update_date
+from 
+  etsy-data-warehouse-prod.user_mart.mapped_user_profile
+inner join 
+  etsy-data-warehouse-prod.etsy_shard.users_favoritelistings using (user_id)
+where 
+  is_displayable = 1
+group by all
+)
