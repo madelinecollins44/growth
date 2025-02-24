ce6643a7ae169819b2ab7f336a8f9572f5ad35bc
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-02-24 15:39:02 -0500

    Update shipping_promotions.sql

--- web_conversions/shop_home/shipping_promotions.sql
+++ web_conversions/shop_home/shipping_promotions.sql
@@ -36,6 +36,50 @@ left join
 where is_frozen = 0
   and active_seller_status = 1
 
+-- create or replace table etsy-data-warehouse-dev.madelinecollins.web_shop_visits as (
+-- select
+--   platform,
+--   beacon.event_name, 
+--   (select value from unnest(beacon.properties.key_value) where key = "shop_shop_id") as shop_id, 
+--   (select value from unnest(beacon.properties.key_value) where key = "shop_id") as seller_user_id, 
+--   visit_id, 
+--   sequence_number,
+-- from
+--   `etsy-visit-pipe-prod.canonical.visit_id_beacons`
+-- inner join 
+--   etsy-data-warehouse-prod.weblog.visits using (visit_id)
+-- where
+--   date(_partitiontime) >= current_date-30
+--   and _date >= current_date-30
+--   and platform in ('mobile_web','desktop','boe')
+--   and (beacon.event_name in ('shop_home'))
+-- group by all
+-- );
+
+
+-- shops with a free shipping guarantee, metrics distro
+select 
+  seller_tier_new,
+  -- shops 
+  count(distinct case when buyer_promise_enabled = 1 then sb.shop_id end) as free_shipping_active_shops, -- this is the free shipping guarantee  
+  count(distinct case when buyer_promise_enabled = 1 then v.shop_id end) as free_shipping_visited_shops,
+  count(distinct sb.shop_id) as active_shops,
+  count(distinct v.shop_id) as visited_shops,
+  -- these are all for visited shops  
+  count(case when buyer_promise_enabled = 1 then sequence_number end) as free_shipping_pageviews, 
+  count(sequence_number) as visited_pageviews, 
+  count(distinct case when buyer_promise_enabled = 1 then v.visit_id end) as free_shipping_visits, -- this is the free shipping guarantee  
+  count(distinct v.visit_id) as shop_home_visits,
+from 
+  `etsy-data-warehouse-prod.rollups.seller_basics` sb
+left join 
+  etsy-data-warehouse-dev.madelinecollins.web_shop_visits v 
+    on cast(sb.shop_id as string)=v.shop_id 
+where 1=1
+  and is_frozen = 0
+  and active_seller_status = 1
+group by all
+
 
 -- shipping promotion
 with all_shops as (
