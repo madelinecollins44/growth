5c94caab1de0460a6d7e51d5c2ac96ae6dea281f
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-02-24 17:41:34 -0500

    Update shipping_promotions.sql

--- web_conversions/shop_home/shipping_promotions.sql
+++ web_conversions/shop_home/shipping_promotions.sql
@@ -105,21 +105,26 @@ left join
   etsy-data-warehouse-dev.madelinecollins.web_shop_visits v 
     on cast(a.shop_id as string)=v.shop_id 
 
-  ---all metrics
-with all_shops as (
-select  
-  b.shop_id, -- all active shops
-  case when promotion_type in (3, 6, 7, 8) then 1 else 0 end as shops_w_shipping_promotions -- promotions opted into 
-from 
-  etsy-data-warehouse-prod.rollups.seller_basics b
-left join 
-  etsy-data-warehouse-prod.etsy_shard.seller_marketing_promotion p using (shop_id)
-where 
-  is_frozen = 0
-  and active_seller_status = 1
-)
+/* all metrics */
+-- begin
+-- create or replace temp table all_shops as (
+-- select  
+--   seller_tier_new,
+--   b.shop_id, -- all active shops
+--   case when promotion_type in (3, 6, 7, 8) then 1 else 0 end as shops_w_shipping_promotions -- promotions opted into 
+-- from 
+--   etsy-data-warehouse-prod.rollups.seller_basics b
+-- left join 
+--   etsy-data-warehouse-prod.etsy_shard.seller_marketing_promotion p using (shop_id)
+-- where 
+--   is_frozen = 0
+--   and active_seller_status = 1
+-- );
+-- end
+-- etsy-bigquery-adhoc-prod._scripte8ca0eda791c6804ad785521ee948fcf649fdaae.all_shops
+
 select 
-  -- seller_tier_new,
+  seller_tier_new,
   -- shops 
   count(distinct case when shops_w_shipping_promotions = 1 then sb.shop_id end) as shipping_promo_active_shops, -- this is the free shipping guarantee  
   count(distinct case when shops_w_shipping_promotions = 1 then v.shop_id end) as shipping_promo_visited_shops,
@@ -131,7 +136,7 @@ select
   count(distinct case when shops_w_shipping_promotions = 1 then v.visit_id end) as shipping_promo_visits, -- this is the free shipping guarantee  
   count(distinct v.visit_id) as shop_home_visits,
 from 
-  all_shops sb
+  etsy-bigquery-adhoc-prod._scripte8ca0eda791c6804ad785521ee948fcf649fdaae.all_shops sb
 left join 
   etsy-data-warehouse-dev.madelinecollins.web_shop_visits v 
     on cast(sb.shop_id as string)=v.shop_id 
