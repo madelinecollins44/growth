386bc7fb6533247465372ebc7d699955264c8c7d
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-02-26 13:49:29 -0500

    Update hide_banner_on_desktop_breakpoints.sql

--- web_conversions/opp_sizing/shop_home/hide_banner_on_desktop_breakpoints.sql
+++ web_conversions/opp_sizing/shop_home/hide_banner_on_desktop_breakpoints.sql
@@ -1,1 +1,22 @@
+-- total traffic counts
+select
+  count(distinct visit_id) as total_visits,
+  count(distinct case when platform in ('desktop') then visit_id end) as desktop_visits,  
+  count(distinct case when platform in ('mobile_web') then visit_id end) as mweb_visits
+from 
+  etsy-data-warehouse-prod.weblog.visits
+where _date >= current_date-30
 
+
+-- visits with shop home
+select
+  count(distinct visit_id) as total_visits,
+  count(distinct case when platform in ('desktop') then visit_id end) as desktop_visits,  
+  count(distinct case when platform in ('mobile_web') then visit_id end) as mweb_visits
+from 
+  etsy-data-warehouse-prod.weblog.visits v
+inner join 
+  etsy-data-warehouse-prod.weblog.events e using (visit_id)
+where 
+  v._date >= current_date-30
+  and e.event_type in ('shop_home')
