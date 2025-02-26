e203e4e1a90ee82fa4e92d13be532444d3d2ec61
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-02-26 14:23:48 -0500

    Update hide_banner_on_desktop_breakpoints.sql

--- web_conversions/opp_sizing/shop_home/hide_banner_on_desktop_breakpoints.sql
+++ web_conversions/opp_sizing/shop_home/hide_banner_on_desktop_breakpoints.sql
@@ -1,4 +1,6 @@
--- total traffic counts
+---------------------------------------------------------------
+-- OVERALL TRAFFIC COUNTS 
+----------------------------------------------------------------- total traffic counts
 select
   count(distinct visit_id) as total_visits,
   count(distinct case when platform in ('desktop') then visit_id end) as desktop_visits,  
@@ -20,3 +22,33 @@ inner join
 where 
   v._date >= current_date-30
   and e.event_type in ('shop_home')
+
+---------------------------------------------------------------
+-- LOOKING INTO SHOP HOME SPECIFIC
+---- viewport_width: tv, xl, lg sizes (900px +) 
+---- exclude any visits associated with a seller account
+---- exclude any visits to an etsy plus shop home page
+---- desktop only
+---------------------------------------------------------------
+begin
+create or replace temp table web_shop_home_visits as (
+select 
+  platform,
+  beacon.event_name,
+  viewport_width,
+  (select value from unnest(beacon.properties.key_value) where key = "shop_shop_id") as shop_id, 
+  (select value from unnest(beacon.properties.key_value) where key = "shop_id") as seller_user_id, 
+  visit_id, 
+  sequence_number,
+from
+  `etsy-visit-pipe-prod.canonical.visit_id_beacons`
+inner join 
+  etsy-data-warehouse-prod.weblog.visits using (visit_id)
+where
+  date(_partitiontime) >= current_date-30
+  and _date >= current_date-30
+  and platform in ('mobile_web','desktop')
+  and (beacon.event_name in ('shop_home')) -- pull all shop_home 
+group by all
+);
+end
