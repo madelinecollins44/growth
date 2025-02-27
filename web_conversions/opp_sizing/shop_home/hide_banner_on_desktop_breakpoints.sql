5440e1d6833dbea65f8c90d278f9fc925f64f84a
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-02-27 09:25:47 -0500

    Update hide_banner_on_desktop_breakpoints.sql

--- web_conversions/opp_sizing/shop_home/hide_banner_on_desktop_breakpoints.sql
+++ web_conversions/opp_sizing/shop_home/hide_banner_on_desktop_breakpoints.sql
@@ -110,7 +110,21 @@ where
   is_etsy_plus = 0 -- exclude etsy plus accounts
   and is_seller = 0 -- exclude sellers
   and lg_plus_screen_size = 1  -- only looking at visits with a large screen
-  
+
+-- get overall counts to confirm 
+select
+  has_banner,
+  count(distinct shop_id) as unique_shops,
+  count(distinct visit_id) as visits,
+  count(visit_id) as pageviews,
+  count(distinct case when converted > 0 then visit_id end) as converted_visits
+from 
+  etsy-data-warehouse-dev.madelinecollins.web_shop_home_traffic_opp_sizing
+where 
+  is_etsy_plus = 0 -- exclude etsy plus accounts
+  and is_seller = 0 -- exclude sellers
+  and lg_plus_screen_size = 1  -- only looking at visits with a large screen
+group by all
 ---------------------------------------------------------------
 -- TESTING
 ---------------------------------------------------------------
