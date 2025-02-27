8081fd163893449fa6680c7eefd2a43390cb0f9d
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-02-27 16:17:10 -0500

    Create coverage_by_sections_w_translation_table.sql

---
+++ web_conversions/shop_home/browsability_improvements/coverage_by_sections_w_translation_table.sql
@@ -0,0 +1,25 @@
+with shop_sections as ( -- active shops + if they have sections with listings in them 
+select 
+  b.shop_id,
+  b.seller_tier_new,
+  shop_name,
+  b.user_id as seller_user_id, 
+  count(distinct case when active_listing_count > 0 then coalesce(s.name,t.name) end) as sections
+from 
+  etsy-data-warehouse-prod.rollups.seller_basics b
+left join 
+  etsy-data-warehouse-prod.etsy_shard.shop_sections s using (shop_id)
+left join 
+  etsy-data-warehouse-prod.etsy_shard.shop_sections_translations t 
+    on s.shop_id=t.shop_id
+    and s.id=t.id
+where
+  active_seller_status = 1
+group by all
+)
+select
+  sections,
+  seller_tier_new,
+  count(distinct shop_id) as shops
+from shop_sections
+group by all
