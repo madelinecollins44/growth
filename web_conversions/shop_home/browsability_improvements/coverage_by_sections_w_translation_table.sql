2568a857f0470842412f914c6c1e38b42036a902
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-03-03 16:46:47 -0500

    Update coverage_by_sections_w_translation_table.sql

--- web_conversions/shop_home/browsability_improvements/coverage_by_sections_w_translation_table.sql
+++ web_conversions/shop_home/browsability_improvements/coverage_by_sections_w_translation_table.sql
@@ -44,7 +44,6 @@ from section_count
 --TESTING
 --------------------------------------------------
 -- TEST 1: check to make sure joining to tables make sense 
-
 select 
 --s.*,t.*
   b.shop_id,
@@ -67,3 +66,44 @@ where
   active_seller_status = 1
   and s.shop_id in (46345829)
 group by all
+
+-- TEST 2: make sure has_section count is accurate
+with translated_sections as ( -- grab english translations, or whatever translation is set to 1
+select 
+  *
+from etsy-data-warehouse-prod.etsy_shard.shop_sections_translations
+qualify row_number() over (
+    partition by id 
+    order by
+        case when language = 5 then 1 else 2 end,  -- Prioritize language = 5
+        language asc  -- If no language = 5, take the lowest language number
+) = 1
+)
+, section_count as (
+select 
+  b.shop_id,
+  shop_name,
+  seller_tier_new,
+  case when (s.shop_id is not null or t.shop_id is not null) and active_listing_count > 0 then 1 else 0 end as has_sections,
+  count(case when active_listing_count > 0 then s.id end) as sections,
+  count(case when ((coalesce(nullif(s.name, ''),t.name)) is not null) and active_listing_count > 0 then s.id end) as filled_ids,
+  count(case when ((coalesce(nullif(s.name, ''),t.name)) is null) and active_listing_count > 0 then s.id end) as missing_ids,
+from 
+  etsy-data-warehouse-prod.rollups.seller_basics b
+left join 
+  etsy-data-warehouse-prod.etsy_shard.shop_sections s using (shop_id)
+left join 
+  translated_sections t 
+    on s.shop_id=t.shop_id
+    and s.id=t.id
+where
+  active_seller_status = 1 -- active sellers
+  and is_frozen = 0  -- not frozen accounts 
+  and active_listings > 0 -- shops with active listings
+  -- and b.shop_id in (20077844)
+group by all
+)
+select
+*
+from section_count
+where has_sections = 1 and sections = 0 
