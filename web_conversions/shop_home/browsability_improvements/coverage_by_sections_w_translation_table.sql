7da7d7ce9698250e43e92c4e96e64e14bdb3302f
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-03-03 17:19:23 -0500

    Update coverage_by_sections_w_translation_table.sql

--- web_conversions/shop_home/browsability_improvements/coverage_by_sections_w_translation_table.sql
+++ web_conversions/shop_home/browsability_improvements/coverage_by_sections_w_translation_table.sql
@@ -1,4 +1,64 @@
 --------------------------------------------------
+-- OVERALL COUNTS TO CONFIRM
+--------------------------------------------------
+--overall visit
+select
+  count(distinct seller_user_id) as unique_shops,
+  count(distinct visit_id) as visits,
+  count(sequence_number) as pageviews
+from  
+  etsy-data-warehouse-dev.madelinecollins.web_shop_visits 
+where 
+  platform in ('desktop','mobile_web')
+group by all 
+  
+--------------------------------------------------
+-- CREATE TABLE TO GET SECTIONS FOR ALL SHOPS
+--------------------------------------------------
+begin
+create or replace temp table active_shops_and_section_info as (
+with translated_sections as ( -- grab english translations, or whatever translation is set to 1
+select 
+  *
+from etsy-data-warehouse-prod.etsy_shard.shop_sections_translations
+qualify row_number() over (
+    partition by id 
+    order by
+        case when language = 5 then 1 else 2 end,  -- Prioritize language = 5
+        language asc  -- If no language = 5, take the lowest language number
+) = 1
+)
+select 
+  b.shop_id,
+  b.user_id as seller_user_id,
+  shop_name,
+  is_etsy_plus,
+  seller_tier_new,
+  case when (s.shop_id is not null or t.shop_id is not null) and active_listing_count > 0 then 1 else 0 end as has_sections_w_listings,
+  case when (s.shop_id is not null or t.shop_id is not null) then 1 else 0 end as has_sections,
+  count(s.id) as sections,
+  count(case when active_listing_count > 0 then s.id end) as sections_w_listings,
+  count(case when ((coalesce(nullif(s.name, ''),t.name)) is not null) and active_listing_count > 0 then s.id end) as filled_ids,
+  count(case when ((coalesce(nullif(s.name, ''),t.name)) is null) and active_listing_count > 0 then s.id end) as missing_ids,
+from 
+  etsy-data-warehouse-prod.rollups.seller_basics b
+left join 
+  etsy-data-warehouse-prod.etsy_shard.shop_sections s using (shop_id)
+left join 
+  translated_sections t 
+    on s.shop_id=t.shop_id
+    and s.id=t.id
+where
+  active_seller_status = 1 -- active sellers
+  and is_frozen = 0  -- not frozen accounts 
+  and active_listings > 0 -- shops with active listings
+group by all
+);
+end
+-- etsy-bigquery-adhoc-prod._script32fb9713d90b49e109ed630f241e7819296ce9fa.active_shops_and_section_info
+
+  
+--------------------------------------------------
 -- COMBINE SECTIONS TO GET GMS/ CR/ VISIT COVERAGE
 --------------------------------------------------
 with shop_sections as ( -- active shops + if they have sections with listings in them 
@@ -62,51 +122,6 @@ left join
   shop_level l
     on l.seller_user_id= cast(s.seller_user_id as string)
 group by all 
-
---------------------------------------------------
--- CREATE TABLE TO GET SECTIONS FOR ALL SHOPS
---------------------------------------------------
-begin
-create or replace temp table active_shops_and_section_info as (
-with translated_sections as ( -- grab english translations, or whatever translation is set to 1
-select 
-  *
-from etsy-data-warehouse-prod.etsy_shard.shop_sections_translations
-qualify row_number() over (
-    partition by id 
-    order by
-        case when language = 5 then 1 else 2 end,  -- Prioritize language = 5
-        language asc  -- If no language = 5, take the lowest language number
-) = 1
-)
-select 
-  b.shop_id,
-  b.user_id as seller_user_id,
-  shop_name,
-  is_etsy_plus,
-  seller_tier_new,
-  case when (s.shop_id is not null or t.shop_id is not null) and active_listing_count > 0 then 1 else 0 end as has_sections_w_listings,
-  case when (s.shop_id is not null or t.shop_id is not null) then 1 else 0 end as has_sections,
-  count(s.id) as sections,
-  count(case when active_listing_count > 0 then s.id end) as sections_w_listings,
-  count(case when ((coalesce(nullif(s.name, ''),t.name)) is not null) and active_listing_count > 0 then s.id end) as filled_ids,
-  count(case when ((coalesce(nullif(s.name, ''),t.name)) is null) and active_listing_count > 0 then s.id end) as missing_ids,
-from 
-  etsy-data-warehouse-prod.rollups.seller_basics b
-left join 
-  etsy-data-warehouse-prod.etsy_shard.shop_sections s using (shop_id)
-left join 
-  translated_sections t 
-    on s.shop_id=t.shop_id
-    and s.id=t.id
-where
-  active_seller_status = 1 -- active sellers
-  and is_frozen = 0  -- not frozen accounts 
-  and active_listings > 0 -- shops with active listings
-group by all
-);
-end
--- etsy-bigquery-adhoc-prod._script32fb9713d90b49e109ed630f241e7819296ce9fa.active_shops_and_section_info
 
 --------------------------------------------------
 --TESTING
