3fe39151db29e8781aa80396f51bdde99dbfb350
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-03-03 13:11:29 -0500

    Update coverage_by_sections_w_translation_table.sql

--- web_conversions/shop_home/browsability_improvements/coverage_by_sections_w_translation_table.sql
+++ web_conversions/shop_home/browsability_improvements/coverage_by_sections_w_translation_table.sql
@@ -28,13 +28,15 @@ group by all
 --------------------------------------------------
 --TESTING
 --------------------------------------------------
-with shop_sections as ( -- active shops + if they have sections with listings in them 
+-- TEST 1: check to make sure joining to tables make sense 
+
 select 
 --s.*,t.*
   b.shop_id,
   b.seller_tier_new,
   shop_name,
   b.user_id as seller_user_id, 
+  -- coalesce(nullif(s.name, ''),t.name) ,
   case when active_listing_count > 0 then coalesce(nullif(s.name, ''),t.name) end as sections,
   count(distinct case when active_listing_count > 0 then coalesce(nullif(s.name, ''),t.name) end) as sections
 from 
@@ -45,15 +47,8 @@ left join
   etsy-data-warehouse-prod.etsy_shard.shop_sections_translations t 
     on s.shop_id=t.shop_id
     and s.id=t.id
+    and s.create_date=t.create_date
 where
   active_seller_status = 1
   and s.shop_id in (46345829)
 group by all
-)
-select * from shop_sections where shop_id in (46345829)
--- select
---   sections,
---   seller_tier_new,
---   count(distinct shop_id) as shops
--- from shop_sections
--- group by all
