d04b4973f0daf978dee22f135db69c9ac24cdb10
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-03-06 15:16:13 -0500

    Update add_this_item_badge_photo.sql

--- web_conversions/opp_sizing/reviews/add_this_item_badge_photo.sql
+++ web_conversions/opp_sizing/reviews/add_this_item_badge_photo.sql
@@ -32,3 +32,39 @@ inner join
 where 
   v._date >= current_date-30
   and e.event_type in ('appreciation_photo_overlay_opened')
+
+-- total traffic counts + converted
+select
+  count(distinct visit_id) as total_visits,
+  count(distinct case when platform in ('mobile_web') then visit_id end) as mweb_visits
+from 
+  etsy-data-warehouse-prod.weblog.visits
+where _date >= current_date-30
+  and v.converted > 0
+
+-- visits with view_listing event + converted
+select
+  count(distinct visit_id) as total_visits,
+  count(distinct case when platform in ('mobile_web') then visit_id end) as mweb_visits
+from 
+  etsy-data-warehouse-prod.weblog.visits v
+inner join 
+  etsy-data-warehouse-prod.weblog.events e using (visit_id)
+where 
+  v._date >= current_date-30
+  and e.event_type in ('view_listing')
+  and v.converted > 0
+
+  
+-- visits with review photo clicks (appreciation_photo_overlay_opened) + converted
+select
+  count(distinct visit_id) as total_visits,
+  count(distinct case when platform in ('mobile_web') then visit_id end) as mweb_visits
+from 
+  etsy-data-warehouse-prod.weblog.visits v
+inner join 
+  etsy-data-warehouse-prod.weblog.events e using (visit_id)
+where 
+  v._date >= current_date-30
+  and e.event_type in ('appreciation_photo_overlay_opened')
+  and v.converted > 0
