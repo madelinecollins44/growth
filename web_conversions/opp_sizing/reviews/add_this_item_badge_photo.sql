8b778402af5e3af8860bb4d2a6ecc5a39f36b121
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-03-06 15:13:59 -0500

    Update add_this_item_badge_photo.sql

--- web_conversions/opp_sizing/reviews/add_this_item_badge_photo.sql
+++ web_conversions/opp_sizing/reviews/add_this_item_badge_photo.sql
@@ -1,1 +1,34 @@
+--------------
+--OPP SIZING
+--------------
+-- total traffic counts
+select
+  count(distinct visit_id) as total_visits,
+  count(distinct case when platform in ('mobile_web') then visit_id end) as mweb_visits
+from 
+  etsy-data-warehouse-prod.weblog.visits
+where _date >= current_date-30
 
+-- visits with view_listing event
+select
+  count(distinct visit_id) as total_visits,
+  count(distinct case when platform in ('mobile_web') then visit_id end) as mweb_visits
+from 
+  etsy-data-warehouse-prod.weblog.visits v
+inner join 
+  etsy-data-warehouse-prod.weblog.events e using (visit_id)
+where 
+  v._date >= current_date-30
+  and e.event_type in ('view_listing')
+  
+-- visits with review photo clicks (appreciation_photo_overlay_opened)
+select
+  count(distinct visit_id) as total_visits,
+  count(distinct case when platform in ('mobile_web') then visit_id end) as mweb_visits
+from 
+  etsy-data-warehouse-prod.weblog.visits v
+inner join 
+  etsy-data-warehouse-prod.weblog.events e using (visit_id)
+where 
+  v._date >= current_date-30
+  and e.event_type in ('appreciation_photo_overlay_opened')
