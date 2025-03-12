a98c8ac54fe4ffd03c9fb1651adaa3a804c8ad48
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-03-12 13:45:05 -0400

    Update section_name_coverage.sql

--- web_conversions/shop_home/section_name_coverage.sql
+++ web_conversions/shop_home/section_name_coverage.sql
@@ -187,7 +187,6 @@ where 1=1
 group by all 
 order by 2 desc 
 )
--- 162912016 lv,3285529 pav
 , all_trans as ( -- get all transactions on visit + listing data 
 select
 	tv.visit_id, 
@@ -222,7 +221,6 @@ where 1=1
 group by all
 order by 3 desc
 )
--- 162912016 lv, 3285529 pav
 , section_info as (
 select
   -- s.section_id,
@@ -250,17 +248,17 @@ select
   -- end as recipient_section,
 
   -- case
-  --   when regexp_contains(section_name, r'(?i)(birthday|anniversary|wedding|engagement|baby\s?shower|bridal\s?shower|graduation|retirement|housewarming|promotion|new\s?job|new\s?home|farewell|get\s?well|sympathy|thank\s?you|congratulations|valentine|galentine|easter|mother\'?s\s?day|father\'?s\s?day|christmas|xmas|hanukkah|kwanzaa|new\s?year|thanksgiving|halloween|st\s?patrick\'?s\s?day|4th\s?of\s?july|independence\s?day|holiday|ramadan|eid|diwali|hanukkah|graduation|back\s?to\s?school)\w*')
+  --   when regexp_contains(section_name, r'(?i)(birthday|party|celebrat|event|anniversary|wedding|engagement|baby\s?shower|bridal\s?shower|graduation|retirement|housewarming|promotion|new\s?job|new\s?home|farewell|get\s?well|sympathy|thank\s?you|congratulations|valentine|galentine|easter|mother\'?s\s?day|father\'?s\s?day|christmas|xmas|hanukkah|kwanzaa|new\s?year|thanksgiving|halloween|st\s?patrick\'?s\s?day|4th\s?of\s?july|independence\s?day|holiday|ramadan|eid|diwali|hanukkah|graduation|back\s?to\s?school)\w*')
   --   then 1 else 0 
   -- end as occasion_section,
 
   --   case
-  --   when regexp_contains(section_name, r'(?i)(earring|accessor|keychain|ornament|digital|download|pottery|card|decor|pin|magnet|apparel|gift|comic|book|item|top|flower|ring|bracelet|watch|necklace|tumbler|lighting|set|bundle|journal|calendar|drinkware|cup|patch|pendant|charm|brooch|anklet|jewel|clothing|shirt|t-shirt|sweater|hoodie|jacket|coat|dress|skirt|pant|jean|short|shoe|sneaker|boot|heel|sandal|bag|handbag|backpack|wallet|coaster|belt|hat|scarf|glove|sock|home\s?decor|furniture|candle|vase|mug|glass|plate|bowl|cutlery|bedding|pillow|blanket|rug|towel|lamp|mirror|clock|art|painting|poster|print|sticker|toy|game|puzzle|book|notebook|planner|pen|stationery|craft|yarn|fabric|tool|gadget|tech|phone\s?case|charger|headphones|earbuds|laptop\s?stand|tablet\s?case|kitchen|cookware|bakeware|appliance|utensil|pet\s?toy|pet\s?bed|collar|leash|harness|figurine|statue|doll|button|chain)\w*')
+  --   when regexp_contains(section_name, r'(?i)(earring|design|page|certificate|accessor|keychain|ornament|digital|download|pottery|card|decor|pin|magnet|apparel|gift|comic|book|item|top|flower|ring|bracelet|watch|necklace|tumbler|lighting|set|bundle|journal|calendar|drinkware|cup|patch|pendant|charm|brooch|anklet|jewel|clothing|shirt|t-shirt|sweater|hoodie|jacket|coat|dress|skirt|pant|jean|short|shoe|sneaker|boot|heel|sandal|bag|handbag|backpack|wallet|coaster|belt|hat|scarf|glove|sock|home\s?decor|furniture|candle|vase|mug|glass|plate|bowl|cutlery|bedding|pillow|blanket|rug|towel|lamp|mirror|clock|art|painting|poster|print|sticker|toy|game|puzzle|notebook|planner|pen|stationery|craft|yarn|fabric|tool|carpet|runner|chair|trunk|storage|gadget|tech|phone\s?case|charger|headphones|earbuds|laptop\s?stand|tablet\s?case|kitchen|cookware|bakeware|appliance|utensil|pet\s?toy|pet\s?bed|collar|leash|harness|figurine|statue|doll|button|chain)\w*')
   --   then 1 else 0 
   -- end as item_section,
     
   --   case
-  --   when regexp_contains(section_name, r'(?i)(metal|steel|gold|silver|platinum|copper|brass|aluminum|iron|bronze|titanium|zinc|nickel|lead|tin|pewter|stainless\s?steel|copper\s?alloy|gold\s?plated|silver\s?plated|brass\s?finish|iron\s?cast|aluminum\s?frame|wood|leather|cotton|wool|linen|silk|polyester|nylon|acrylic|rubber|plastic|ceramic|porcelain|stone|marble|granite|clay|glass|paper|bamboo|linen|rattan|satin)\w*')
+  --   when regexp_contains(section_name, r'(?i)(china|pottery|metal|steel|gold|silver|platinum|copper|brass|aluminum|iron|bronze|titanium|zinc|nickel|lead|tin|pewter|stainless\s?steel|copper\s?alloy|gold\s?plated|silver\s?plated|brass\s?finish|iron\s?cast|aluminum\s?frame|wood|leather|cotton|wool|linen|silk|polyester|nylon|acrylic|rubber|plastic|ceramic|porcelain|stone|marble|granite|clay|glass|paper|bamboo|linen|rattan|satin)\w*')
   --   then 1 else 0 
   -- end as material_section,
 
@@ -270,7 +268,7 @@ select
   -- end as shipping_section,
 
         case
-    when regexp_contains(section_name, r'(?i)(vintage|retro|antique|boho|bohemian|minimal|minimalist|hadwrit|modern|contemporary|rustic|industrial|classic|traditional|elegant|chic|aesthetic|goth|gothic|punk|grunge|steampunk|art\s?deco|mid\s?century|farmhouse|coastal|cottage|shabby\s?chic|scandinavian|french\s?country|baroque|rococo|victorian|edgy|casual|formal|streetwear|y2k|kawaii|preppy|sporty|futuristic|avant\s?garde|eclectic|handmade|artisanal|craftsman|tribal|southwestern|luxury|glam|sleek|geometric|floral|abstract|monochrome|ombre|pastel|bold|neutral|warm\s?tone|cool\s?tone|colorful|earthy)\w*')
+    when regexp_contains(section_name, r'(?i)(vintage|mini|little|large|big|retro|antique|boho|bohemian|minimal|minimalist|hadwrit|modern|contemporary|rustic|industrial|classic|traditional|elegant|chic|aesthetic|goth|gothic|punk|grunge|steampunk|art\s?deco|mid\s?century|round|square|oval|rectangle|farmhouse|coastal|cottage|shabby\s?chic|scandinavian|french\s?country|baroque|rococo|victorian|edgy|casual|formal|streetwear|y2k|kawaii|preppy|sporty|futuristic|avant\s?garde|eclectic|handmade|woven|artisanal|craftsman|tribal|southwestern|luxury|glam|sleek|geometric|floral|abstract|monochrome|ombre|pastel|bold|neutral|warm\s?tone|cool\s?tone|colorful|earthy)\w*')
     then 1 else 0 
   end as style_section,
 
@@ -280,8 +278,8 @@ select
 from listing_metrics
 left join section_info using (listing_id)
 group by all 
-order by style_section asc
-
+order by 1 asc
+	
 --------------------------------------------------
 -- share of sections without names 
 --------------------------------------------------
