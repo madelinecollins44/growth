0986a445f0fb9a5963fb6d94cbac38c422158021
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-03-14 12:12:16 -0400

    Update nonus_lv_reviews.sql

--- web_conversions/reviews/nonus_lv_reviews.sql
+++ web_conversions/reviews/nonus_lv_reviews.sql
@@ -1,6 +1,22 @@
 ---------------------------------------------------------------
 -- WHAT % OF LISTING VIEWS ARE FROM NON-US USERS? 
 ---------------------------------------------------------------
+select
+  detected_region,
+  -- case 
+  --   when detected_region in ('US') then 'US'
+  --   else 'non-US'
+  --   end as detected_region,
+  count(sequence_number) as listing_views,
+  sum(purchased_after_view) as purchases,
+  count(distinct visit_id) as unique_visits
+from  
+  etsy-data-warehouse-prod.analytics.listing_views
+where 
+  _date >= current_date-30
+  and platform in ('mobile_web','desktop')
+group by all
+order by 2 desc 
 
 
 ---------------------------------------------------------------
