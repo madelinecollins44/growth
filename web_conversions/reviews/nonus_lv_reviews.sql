d4efe4921feb53e28f6928fb6bcc1de44a673ebc
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-03-14 13:04:47 -0400

    Update nonus_lv_reviews.sql

--- web_conversions/reviews/nonus_lv_reviews.sql
+++ web_conversions/reviews/nonus_lv_reviews.sql
@@ -56,3 +56,52 @@ left join
 group by all
 order by 2 desc
 
+-----------------------------------------------------------------------------
+-- WHAT % OF VIEWED LISTINGS WILL HAVE REVIEWS FROM THE SAME REGION? 
+-----------------------------------------------------------------------------
+with views as (
+select
+  case 
+    when detected_region in ('US') then 'US'
+    else 'non-US'
+    end as region,
+  listing_id,
+  count(sequence_number) as listing_views,
+  sum(purchased_after_view) as purchases,
+  count(distinct visit_id) as unique_visits
+from  
+  etsy-data-warehouse-prod.analytics.listing_views
+where 
+  _date >= current_date-30
+  and platform in ('mobile_web','desktop')
+group by all
+)
+, reviews as (
+select
+  case 
+    when mup.country in ('US') then 'US'
+    else 'non-US'
+  end as region,  
+  listing_id,
+  count(case when has_review > 0 then transaction_id end) as reviews
+from 
+  etsy-data-warehouse-prod.rollups.transaction_reviews tr
+left join 
+  etsy-data-warehouse-prod.user_mart.mapped_user_profile mup
+    on mup.user_id=tr.buyer_user_id
+group by all 
+)
+-- of all non-US listing views, how many of them have non-US reviews
+select
+  region,
+  count(distinct v.listing_id) as listings_viewed,
+  count(distinct r.listing_id) as listings_reviewed,
+  sum(listing_views) as all_listing_views,
+  sum(purchases) as all_purchases,
+  sum(reviews) as all_reviews,
+  sum(case when r.listing_id is not null then listing_views end) as listing_views_for_listings_w_reviews
+from 
+  views v
+left join 
+  reviews r using (listing_id, region)
+group by all 
