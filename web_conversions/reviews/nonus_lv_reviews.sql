78d9d065b37222464d4c825cb7837dbbd6ccf499
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-03-14 12:36:26 -0400

    Update nonus_lv_reviews.sql

--- web_conversions/reviews/nonus_lv_reviews.sql
+++ web_conversions/reviews/nonus_lv_reviews.sql
@@ -1,4 +1,27 @@
 ---------------------------------------------------------------
+-- OVERALL COUNTS TO CONFIRM 
+---------------------------------------------------------------
+-- total listing views
+select
+  count(sequence_number) as listing_views,
+  sum(purchased_after_view) as purchases,
+  count(distinct visit_id) as unique_visits
+from  
+  etsy-data-warehouse-prod.analytics.listing_views
+where 
+  _date >= current_date-30
+  and platform in ('mobile_web','desktop')
+group by all
+order by 2 desc 
+
+-- total reviews 
+select
+  count(case when has_review > 0 then transaction_id end) as reviews
+from 
+  etsy-data-warehouse-prod.rollups.transaction_reviews tr
+group by all
+
+---------------------------------------------------------------
 -- WHAT % OF LISTING VIEWS ARE FROM NON-US USERS? 
 ---------------------------------------------------------------
 select
