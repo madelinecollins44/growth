1e82ce000ddee6c93007a7d8d165aa25097f175d
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-03-21 10:46:52 -0400

    Update sticky_nav.sql

--- web_conversions/opp_sizing/shop_home/sticky_nav.sql
+++ web_conversions/opp_sizing/shop_home/sticky_nav.sql
@@ -27,15 +27,13 @@ where
 group by all
 
 ----browsers that do this at least once 
---traffic to shop home/ listing grid  
 with agg as (
 select
   platform,
-  event_type,
   browser_id,
-  max(case when event_type in ('shop_home') then 1 else 0 end) as shop_home_visit,
-  max(case when event_type in ('shop_home_listing_grid_seen') then 1 else 0 end) as grid_visit,
-  max(case when event_type in ('shop_home_listings_section_seen') then 1 else 0 end) as section_visit,
+  count(case when event_type in ('shop_home') then sequence_number end) as shop_home_visit,
+  count(case when event_type in ('shop_home_listing_grid_seen') then sequence_number end)  as grid_visit,
+  count(case when event_type in ('shop_home_listings_section_seen') then sequence_number end)  as section_visit,
 from 
   etsy-data-warehouse-prod.weblog.events e  
 inner join 
@@ -49,12 +47,11 @@ group by all
 select
   platform,
   count(distinct browser_id) as browsers,
-  count(distinct case when shop_home_visit > 0 then 1 else 0 end) as sh_browsers,
-  count(distinct case when grid_visit > 0 then 1 else 0 end) as grid_browsers,  
-  count(distinct case when section_visit > 0 then 1 else 0 end) as section_browsers,
+  count(distinct case when shop_home_visit > 0 then browser_id end) as sh_browsers,
+  count(distinct case when grid_visit > 0 then browser_id end) as grid_browsers,  
+  count(distinct case when section_visit > 0 then browser_id end) as section_browsers,
 from agg
 group by all 
-
 
 --------------------------------------------------------
 --TESTING
