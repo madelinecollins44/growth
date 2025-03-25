d10ea6de0fb39849179b210b1d6ca3d06984433e
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-03-25 10:51:09 -0400

    Update cart_background_deck.sql

--- web_conversions/evaluation/cart_background_deck.sql
+++ web_conversions/evaluation/cart_background_deck.sql
@@ -1,5 +1,5 @@
-/* START WITH UNDERSTANDING BASIC STATS AROUND THE CART PAGE*/
--- total cart traffic
+/*  TOTAL TRAFFIC */
+
 select 
   platform, 
   count(distinct visit_id) as visits,
@@ -11,7 +11,7 @@ where 1=1
   and v._date >= current_date-30
 group by all 
 
--- referring page
+/*  REFERRING PAGE */
 with event_ordered as (
 select
   platform,
@@ -40,4 +40,31 @@ where
   next_event in ('cart_view')
 group by all
 
--- gms 
+/*  GMS */
+-- cart gms
+with cart_visits as (
+select
+  distinct visit_id
+from 
+  etsy-data-warehouse-prod.weblog.events
+where event_type in ('cart_view')
+)
+select 
+  platform, 
+  sum(total_gms) as total_gms
+from cart_visits
+inner join etsy-data-warehouse-prod.weblog.visits v using (visit_id)
+where 1=1
+  and platform in ('boe','mobile_web','desktop')
+  and v._date >= current_date-30
+group by all 
+
+-- total gms
+select 
+  platform, 
+  sum(total_gms) as total_gms
+from etsy-data-warehouse-prod.weblog.visits v
+where 1=1
+  and platform in ('boe','mobile_web','desktop')
+  and v._date >= current_date-30
+group by all 
