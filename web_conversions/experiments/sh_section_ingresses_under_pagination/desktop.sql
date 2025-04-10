50fb6fd1fbc3da69ca795c7223b95dc8017a70d3
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-04-10 16:49:19 -0400

    Update desktop.sql

--- web_conversions/experiments/sh_section_ingresses_under_pagination/desktop.sql
+++ web_conversions/experiments/sh_section_ingresses_under_pagination/desktop.sql
@@ -2,6 +2,7 @@
 This analysis is meant to figure out if some channels responded better to this. 
 -- desktop: https://atlas.etsycorp.com/catapult/1361091594266 (growth_regx.sh_section_ingresses_under_pagination_desktop)
 -- mobile web: https://atlas.etsycorp.com/catapult/1361101148193 (growth_regx.sh_section_ingresses_under_pagination_mweb) */
+
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- DESKTOP
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -19,6 +20,18 @@ SET (start_date, end_date) = (
     `etsy-data-warehouse-prod.catapult_unified.experiment`
   WHERE
     experiment_id = config_flag_param
+);
+
+CREATE OR REPLACE TEMPORARY TABLE xp_units_raw AS (
+  SELECT 
+    bucketing_id,
+    variant_id,
+    bucketing_ts
+  FROM
+    `etsy-data-warehouse-prod.catapult_unified.bucketing_period`
+  WHERE
+    _date = end_date
+    AND experiment_id = config_flag_param
 );
 
 -- Get experiment's bucketed units
@@ -45,21 +58,21 @@ CREATE OR REPLACE TEMPORARY TABLE xp_units AS (
 QUALIFY row_number () over (partition by bucketing_id order by visit_id asc) = 1 -- only grab the channel from the first visit
 );
 
--- Get experiment's bucketed visits
-CREATE OR REPLACE TEMPORARY TABLE xp_visits AS (
-  SELECT
-    v.visit_id,
-    xp.bucketing_id,
-  FROM
-    `etsy-data-warehouse-prod.weblog.visits` AS v
-  INNER JOIN
-    xp_units AS xp
-      ON
-        xp.bucketing_id = v.browser_id
-        AND TIMESTAMP_TRUNC(xp.bucketing_ts, SECOND) <= v.end_datetime
-  WHERE
-    v._date BETWEEN start_date AND end_date
-);
+-- -- Get experiment's bucketed visits
+-- CREATE OR REPLACE TEMPORARY TABLE xp_visits AS (
+--   SELECT
+--     v.visit_id,
+--     xp.bucketing_id,
+--   FROM
+--     `etsy-data-warehouse-prod.weblog.visits` AS v
+--   INNER JOIN
+--     xp_units AS xp
+--       ON
+--         xp.bucketing_id = v.browser_id
+--         AND TIMESTAMP_TRUNC(xp.bucketing_ts, SECOND) <= v.end_datetime
+--   WHERE
+--     v._date BETWEEN start_date AND end_date
+-- );
 
 /* HERE, RECREATE METRICS IN CATAPULT USING EVENT FILTER */
 -- Get browsers who saw the listing grid
@@ -70,7 +83,8 @@ CREATE OR REPLACE TEMPORARY TABLE browsers_with_key_event AS (
   FROM
     `etsy-data-warehouse-prod.weblog.events` AS e
   INNER JOIN 
-    xp_visits AS v USING(visit_id)
+    xp_units AS v 
+        ON v.bucketing_id=split(e.visit_id,'.')[safe_offset(0)] -- this is how to get browser_id from visit
   WHERE
     e._date BETWEEN start_date AND end_date
     AND e.event_type = "shop_home_listing_grid_seen" -- event fires when a browser sees the listing grid 
@@ -127,9 +141,10 @@ CREATE OR REPLACE TEMPORARY TABLE xp_khm_agg_events_by_unit AS (
 );
 
 -- Key Health Metrics (Winsorized ACBV and AOV) - Total (To compare with Catapult as a sanity check)
-create or replace table etsy-data-warehouse-dev.madelinecollins.xp_feature_tags_desktop as (
+create or replace table etsy-data-warehouse-dev.madelinecollins.xp_feature_tags_channel_desktop as (
 SELECT
   xp.variant_id,
+  xp.top_channel,
   COUNT(xp.bucketing_id) AS browsers,
   -- metrics
   SAFE_DIVIDE(COUNTIF(e.orders > 0), COUNT(xp.bucketing_id)) AS conversion_rate,
@@ -150,13 +165,12 @@ FROM
   xp_units AS xp
 LEFT JOIN
   xp_khm_agg_events_by_unit AS e USING (bucketing_id)
-GROUP BY
-  1
+GROUP BY ALL
 ORDER BY
   1);
 
 -- Key Health Metrics (Winsorized ACBV and AOV) - Only browsers who viewed a listing page with review photos
-create or replace table etsy-data-warehouse-dev.madelinecollins.xp_section_ingress_desktop_filtered as (
+create or replace table etsy-data-warehouse-dev.madelinecollins.xp_section_ingress_desktop_channel_filtered as (
 SELECT
   xp.variant_id,
   b.top_channel,
@@ -186,6 +200,63 @@ GROUP BY ALL
 ORDER BY
   1);
 
+-- Key Health Metrics (Winsorized ACBV and AOV) - Total (To compare with Catapult as a sanity check)
+create or replace table etsy-data-warehouse-dev.madelinecollins.xp_feature_tags_desktop as (
+SELECT
+  xp.variant_id,
+  COUNT(xp.bucketing_id) AS browsers,
+  -- metrics
+  SAFE_DIVIDE(COUNTIF(e.orders > 0), COUNT(xp.bucketing_id)) AS conversion_rate,
+  SAFE_DIVIDE(COUNTIF(e.bounced_visits > 0), COUNT(xp.bucketing_id)) AS bounce_rate,
+  SAFE_DIVIDE(COUNTIF(e.atc_count > 0), COUNT(xp.bucketing_id)) AS pct_with_atc,
+  SAFE_DIVIDE(COUNTIF(e.checkout_start_count > 0), COUNT(xp.bucketing_id)) AS pct_with_checkout_start,
+  SAFE_DIVIDE(SUM(e.engaged_visits), COUNT(xp.bucketing_id)) AS mean_engaged_visits,
+  SAFE_DIVIDE(SUM(e.visits), COUNT(xp.bucketing_id)) AS mean_visits,
+  SAFE_DIVIDE(SUM(e.orders), COUNTIF(e.orders > 0)) AS ocb,
+  SAFE_DIVIDE(SUM(e.completed_checkouts), COUNT(xp.bucketing_id)) AS orders_per_browser,
+  SAFE_DIVIDE(SUM(e.page_count), COUNT(xp.bucketing_id)) AS pages_per_browser,
+  SAFE_DIVIDE(SUM(e.winsorized_gms), COUNTIF(e.orders > 0)) AS winsorized_acbv,
+  SAFE_DIVIDE(SUM(e.winsorized_order_value_sum), SUM(e.completed_checkouts)) AS winsorized_aov,
+  --browser counts
+  COUNTIF(e.orders > 0) AS converted_browsers,
+  COUNTIF(e.atc_count > 0) AS atc_browsers
+FROM
+  xp_units AS xp
+LEFT JOIN
+  xp_khm_agg_events_by_unit AS e USING (bucketing_id)
+GROUP BY ALL
+ORDER BY
+  1);
+
+-- Key Health Metrics (Winsorized ACBV and AOV) - Only browsers who viewed a listing page with review photos
+create or replace table etsy-data-warehouse-dev.madelinecollins.xp_section_ingress_desktop_filtered as (
+SELECT
+  xp.variant_id,
+  COUNT(xp.bucketing_id) AS browsers,
+  -- metrics
+  SAFE_DIVIDE(COUNTIF(e.orders > 0), COUNT(xp.bucketing_id)) AS conversion_rate,
+  SAFE_DIVIDE(COUNTIF(e.bounced_visits > 0), COUNT(xp.bucketing_id)) AS bounce_rate,
+  SAFE_DIVIDE(COUNTIF(e.atc_count > 0), COUNT(xp.bucketing_id)) AS pct_with_atc,
+  SAFE_DIVIDE(COUNTIF(e.checkout_start_count > 0), COUNT(xp.bucketing_id)) AS pct_with_checkout_start,
+  SAFE_DIVIDE(SUM(e.engaged_visits), COUNT(xp.bucketing_id)) AS mean_engaged_visits,
+  SAFE_DIVIDE(SUM(e.visits), COUNT(xp.bucketing_id)) AS mean_visits,
+  SAFE_DIVIDE(SUM(e.orders), COUNTIF(e.orders > 0)) AS ocb,
+  SAFE_DIVIDE(SUM(e.completed_checkouts), COUNT(xp.bucketing_id)) AS orders_per_browser,
+  SAFE_DIVIDE(SUM(e.page_count), COUNT(xp.bucketing_id)) AS pages_per_browser,
+  SAFE_DIVIDE(SUM(e.winsorized_gms), COUNTIF(e.orders > 0)) AS winsorized_acbv,
+  SAFE_DIVIDE(SUM(e.winsorized_order_value_sum), SUM(e.completed_checkouts)) AS winsorized_aov,
+  --browser counts
+  COUNTIF(e.orders > 0) AS converted_browsers,
+  COUNTIF(e.atc_count > 0) AS atc_browsers
+FROM
+  xp_units AS xp
+INNER JOIN
+  browsers_with_key_event AS b USING (bucketing_id)
+LEFT JOIN
+  xp_khm_agg_events_by_unit AS e USING (bucketing_id)
+GROUP BY ALL
+ORDER BY
+  1);
 
 -- how many bucketed browsers saw the treatment? 
 select
