5c20b12899cabedc7ff839f8b9daee679619801e
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-04-11 11:04:59 -0400

    Create opp_sizings.sql

---
+++ strategic_focus/opp_sizings.sql
@@ -0,0 +1,9 @@
+/* Shop Trust
+-- surfaces: shop home page, 'meet the seller' on listing page */
+
+/* Reviews
+-- surfaces: reviews on listing page */
+
+/* Listing Scannability 
+-- surfaces: listing page */
+
