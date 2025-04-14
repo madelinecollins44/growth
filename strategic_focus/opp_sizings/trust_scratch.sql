2eac634f62e9f4a2473c1f25ebb4dc7ae8ae24ea
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-04-14 10:14:54 -0400

    Update trust_scratch.sql

--- strategic_focus/opp_sizings/trust_scratch.sql
+++ strategic_focus/opp_sizings/trust_scratch.sql
@@ -10,58 +10,95 @@ where
   and _date >= current_date-30
 
 -- All web shop home / view listing traffic over last 30 days
+with sh_lp as (
 select 
-  count(distinct visit_id) as total_visits,
-  count(distinct case when converted > 0 then visit_id end) as converted_visits,
-  count(distinct case when converted > 0 then visit_id end) / count(distinct visit_id) as conversion_rate
+  distinct visit_id 
 from 
-  etsy-data-warehouse-prod.weblog.visits v
-inner join 
-  etsy-data-warehouse-prod.weblog.events e using (visit_id)
-where 1=1
-  and platform in ('mobile_web','desktop')
-  and v._date >= current_date-30
-  and e.event_type in ('shop_home','view_listing')
-
--- All web view listing traffic over last 30 days
-select 
-  count(distinct visit_id) as total_visits,
-  count(distinct case when converted > 0 then visit_id end) as converted_visits,
-  count(distinct case when converted > 0 then visit_id end) / count(distinct visit_id) as conversion_rate
-from 
-  etsy-data-warehouse-prod.weblog.visits v
-inner join 
-  etsy-data-warehouse-prod.weblog.events e using (visit_id)
-where 1=1
-  and platform in ('mobile_web','desktop')
-  and v._date >= current_date-30
-  and e.event_type in ('view_listing')
-
--- All web shop home traffic over last 30 days
-select 
-  count(distinct visit_id) as total_visits,
-  count(distinct case when converted > 0 then visit_id end) as converted_visits,
-  count(distinct case when converted > 0 then visit_id end) / count(distinct visit_id) as conversion_rate
-from 
-  etsy-data-warehouse-prod.weblog.visits v
-inner join 
-  etsy-data-warehouse-prod.weblog.events e using (visit_id)
-where 1=1
-  and platform in ('mobile_web','desktop')
-  and v._date >= current_date-30
-  and e.event_type in ('shop_home')	
-
-/* SHOP TRUST-- surfaces: shop home page, 'meet the seller' on listing page */
+  etsy-data-warehouse-prod.weblog.events
+where
+  _date >= current_date-30
+  and event_type in ('shop_home','view_listing')
+)
 select
   count(distinct v.visit_id) as visits_w_engagement,
-  count(distinct case when v.converted > 0 then v.visit_id end) as visits_w_engagement_convert
+  count(distinct case when v.converted > 0 then v.visit_id end) as visits_w_engagement_convert,
+  sum(total_gms) as total_gms
 from
-  etsy-data-warehouse-prod.weblog.events e 
+  sh_lp e 
 inner join 
   etsy-data-warehouse-prod.weblog.visits v using (visit_id)
 where
   v._date >= current_date-30
+  and platform in ('mobile_web','desktop')
+group by all 
+
+-- All web view listing traffic over last 30 days
+with sh_lp as (
+select 
+  distinct visit_id 
+from 
+  etsy-data-warehouse-prod.weblog.events
+where
+  _date >= current_date-30
+  and event_type in ('view_listing')
+)
+select
+  count(distinct v.visit_id) as visits_w_engagement,
+  count(distinct case when v.converted > 0 then v.visit_id end) as visits_w_engagement_convert,
+  sum(total_gms) as total_gms
+from
+  sh_lp e 
+inner join 
+  etsy-data-warehouse-prod.weblog.visits v using (visit_id)
+where
+  v._date >= current_date-30
+  and platform in ('mobile_web','desktop')
+group by all 
+
+-- All web shop home traffic over last 30 days
+with sh_lp as (
+select 
+  distinct visit_id 
+from 
+  etsy-data-warehouse-prod.weblog.events
+where
+  _date >= current_date-30
+  and event_type in ('shop_home')
+)
+select
+  count(distinct v.visit_id) as visits_w_engagement,
+  count(distinct case when v.converted > 0 then v.visit_id end) as visits_w_engagement_convert,
+  sum(total_gms) as total_gms
+from
+  sh_lp e 
+inner join 
+  etsy-data-warehouse-prod.weblog.visits v using (visit_id)
+where
+  v._date >= current_date-30
+  and platform in ('mobile_web','desktop')
+group by all 
+
+/* SHOP TRUST-- surfaces: shop home page, 'meet the seller' on listing page */
+with sh_lp as (
+select 
+  distinct visit_id 
+from 
+  etsy-data-warehouse-prod.weblog.events
+where
+  _date >= current_date-30
 	and event_type in ('shop_home','shop_owners_seen')
+)
+select
+  count(distinct v.visit_id) as visits_w_engagement,
+  count(distinct case when v.converted > 0 then v.visit_id end) as visits_w_engagement_convert,
+  sum(total_gms) as total_gms
+from
+  sh_lp e 
+inner join 
+  etsy-data-warehouse-prod.weblog.visits v using (visit_id)
+where
+  v._date >= current_date-30
+  and platform in ('mobile_web','desktop')
 group by all 
 
 
