ff91ef0e2092b1086f293b804748b900dc7e26aa
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-04-15 14:00:37 -0400

    Create custom_events.sql

---
+++ web_conversions/experiments/custom_events.sql
@@ -0,0 +1,8 @@
+val ReviewEngagement = List(
+  EventType("listing_page_reviews_pagination") , 
+  EventType("appreciation_photo_overlay_opened") , 
+  EventType("listing_page_reviews_content_toggle_opened"),
+  EventType("sort_reviews"),
+  EventType("shop_home_reviews_pagination"),
+  EventType("inline_appreciation_photo_click_shop_page") 
+  )
