ff5ef83b54bf7b00c04ee427d55da8267ab8dcf6
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-04-18 12:40:57 -0400

    Update custom_events.scala

--- web_conversions/experiments/custom_events.scala
+++ web_conversions/experiments/custom_events.scala
@@ -1,8 +1,16 @@
-val ReviewEngagement = List(
-  EventType("listing_page_reviews_pagination") , 
-  EventType("appreciation_photo_overlay_opened") , 
-  EventType("listing_page_reviews_content_toggle_opened"),
-  EventType("sort_reviews"),
-  EventType("shop_home_reviews_pagination"),
-  EventType("inline_appreciation_photo_click_shop_page") 
+def reviewEngagement(events: Vector[MiniEvent], startIndexes: Vector[Int]): Map[Int, Vector[Double]] = {
+  val reviewEngagement = List(
+    "listing_page_reviews_pagination", 
+    "appreciation_photo_overlay_opened", 
+    "listing_page_reviews_content_toggle_opened",
+    "reviews_categorical_tag_clicked", 
+    "sort_reviews",
+    "shop_home_reviews_pagination",
+    "inline_appreciation_photo_click_shop_page"
   )
+  matchExistsOneOrZero(
+    events,
+    startIndexes,
+    (e: MiniEvent) => reviewEngagement.contains(e.eventType)
+  )
+}
