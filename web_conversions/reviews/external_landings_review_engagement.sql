4f7e370d4694b67393d426e8185fa7091ead637e
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-04-22 11:48:23 -0400

    Update external_landings_review_engagement.sql

--- web_conversions/reviews/external_landings_review_engagement.sql
+++ web_conversions/reviews/external_landings_review_engagement.sql
@@ -1,20 +1,3 @@
---
-with all_lv as (
-select 
-  case when url like ('%external=1%') then 1 else 0 end as external_listing,
-  visit_id, 
-  sequence_number, 
-  purchased_after_view,
-from 
-  etsy-data-warehouse-prod.weblog.events e
-inner join 
-  etsy-data-warehouse-prod.analytics.listing_views lv using (visit_id, sequence_number, listing_id)
-where 
-  event_type in ('view_listing') 
-  and lv_date >= current_date-30
-
-
-
 with listing_events as (
 select
 	date(_partitiontime) as _date,
