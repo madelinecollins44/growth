a8b1848e2aa0dbf94e2ec61761c807400dd18f03
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-04-29 15:03:20 -0400

    Update significance.r

--- web_conversions/experiments/cat_tags/significance.r
+++ web_conversions/experiments/cat_tags/significance.r
@@ -23,19 +23,18 @@ t.test(treat_f$event_count, control_f$event_count)
 ## order value
 sql <- "select * from `etsy-data-warehouse-dev.madelinecollins.all_units_events_browser_level_order_value`;"
 tb <- bq_project_query(billing, sql)
-df <- bq_table_download(tb,page_size=500) 
+df <- bq_table_download(tb,page_size=1000) 
 treat_f <- df[df$variant_id == "on", ]
 control_f <- df[df$variant_id == "off", ]
-t.test(treat_f$event_count, control_f$event_count)
-control_f <- df[df$ab_variant == "off", ]
-
 t.test(treat_f$event_count, control_f$event_count)
 
 ## conversion 
 sql <- "select * from `etsy-data-warehouse-dev.madelinecollins.all_units_events_browser_level_conversion`;"
 tb <- bq_project_query(billing, sql)
-df <- bq_table_download(tb,page_size=1000) 
-treat_f <- df[df$variant_id == "on", ]
-control_f <- df[df$variant_id == "off", ]
-t.test(treat_f$event_count, control_f$event_count)
+df <- bq_table_download(tb, page_size = 1000)
+treat <- df[df$variant_id == "on", ]
+control <- df[df$variant_id == "off", ]
+t.test(treat$event_count, control$event_count)
+
+
 
