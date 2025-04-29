8c415c0c0fc5e12511376d30c637412badf7612b
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-04-29 14:10:05 -0400

    Create significance.r

---
+++ web_conversions/experiments/cat_tags/significance.r
@@ -0,0 +1,41 @@
+# run the following in the terminal to log into bq
+gcloud auth application-default login
+
+## setting up r 
+library(bigrquery)
+library(DBI)
+
+# bq project setup
+billing <- 'etsy-bigquery-adhoc-prod'
+
+# Authenticate to bq
+bq_auth()
+
+# pull in relevant data
+## acbv
+sql <- "select * from `etsy-data-warehouse-dev.madelinecollins.all_units_events_browser_level_acbv`;"
+tb <- bq_project_query(billing, sql)
+df <- bq_table_download(tb,page_size=1000) 
+treat_f <- df[df$variant_id == "on", ]
+control_f <- df[df$variant_id == "off", ]
+t.test(treat_f$event_count, control_f$event_count)
+
+## order value
+sql <- "select * from `etsy-data-warehouse-dev.madelinecollins.all_units_events_browser_level_order_value`;"
+tb <- bq_project_query(billing, sql)
+df <- bq_table_download(tb,page_size=500) 
+treat_f <- df[df$variant_id == "on", ]
+control_f <- df[df$variant_id == "off", ]
+t.test(treat_f$event_count, control_f$event_count)
+control_f <- df[df$ab_variant == "off", ]
+
+t.test(treat_f$event_count, control_f$event_count)
+
+## conversion 
+sql <- "select * from `etsy-data-warehouse-dev.madelinecollins.all_units_events_browser_level_conversion`;"
+tb <- bq_project_query(billing, sql)
+df <- bq_table_download(tb,page_size=1000) 
+treat_f <- df[df$variant_id == "on", ]
+control_f <- df[df$variant_id == "off", ]
+t.test(treat_f$event_count, control_f$event_count)
+
