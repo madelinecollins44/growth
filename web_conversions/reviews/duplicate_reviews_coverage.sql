5689ff7ce3399f26a9d852ace5aa64dc6477cfd1
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-04-30 13:36:32 -0400

    Update duplicate_reviews_coverage.sql

--- web_conversions/reviews/duplicate_reviews_coverage.sql
+++ web_conversions/reviews/duplicate_reviews_coverage.sql
@@ -30,15 +30,39 @@ where
   and platform in ('mobile_web','desktop')
 group by all 
 )
+, listing_gms as ( 
+select
+	t.listing_id, 
+	sum(tg.trans_gms_net) as gms_net
+from
+	`etsy-data-warehouse-prod`.transaction_mart.transactions_visits tv
+join
+	`etsy-data-warehouse-prod`.transaction_mart.transactions_gms_by_trans tg
+using(transaction_id)
+join
+	`etsy-data-warehouse-prod`.transaction_mart.all_transactions t
+on
+	tv.transaction_id = t.transaction_id
+where
+	tv.date >= current_date-365
+	and tv.platform_app in ('mobile_web','desktop')
+group by all 
+)
 select
   count(distinct lvs.listing_id) as viewed_listings,
   count(distinct dl.listing_id) as duped_viewed_listings,
   sum(views) as total_lv,
   sum(case when dl.listing_id is not null then views end) as duped_lv,
   sum(purchases) as total_purchases,
-  sum(case when dl.listing_id is not null then purchases end) as duped_purchases
+  sum(case when dl.listing_id is not null then purchases end) as duped_purchases,
+  sum(gms_net) as gms,
+  sum(case when dl.listing_id is not null then gms_net end) as duped_gms
 from 
   lv_stats lvs
 left join 
-  duped_listings dl using (listing_id)
+  listing_gms gms 
+    using (listing_id)
+left join 
+  duped_listings dl 
+    on lvs.listing_id=dl.listing_id
   
