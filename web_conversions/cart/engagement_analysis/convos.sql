d29f511c3e733e11e5f5ba91ac1bbbd205d77ab8
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-05-09 12:04:40 -0400

    Update convos.sql

--- web_conversions/cart/engagement_analysis/convos.sql
+++ web_conversions/cart/engagement_analysis/convos.sql
@@ -7,3 +7,18 @@ select
   where date(convo_start_date) >= '2024-01-01' and date(convo_start_date) <= '2024-12-31'
   group by all 
   order by 1 desc
+
+-- buyer intiiated convos
+select 
+  buyer_segment,
+  avg(message_count) as avg_message_count,
+  context_type,
+  count(distinct conversation_id) as total_convos
+from
+  `etsy-data-warehouse-prod.rollups.convo_transactions` a
+where 
+  date(convo_start_date) >= current_date-365
+  and started_post_purchase > 0
+  and convo_type in ('buyer initiated, seller received')
+group by all 
+order by 1 desc
