d28a1cf1bd3692a8ab4f19796a6e474d30e24486
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-05-09 10:56:23 -0400

    Create convos.sql

---
+++ web_conversions/cart/engagement_analysis/convos.sql
@@ -0,0 +1,9 @@
+select 
+  started_post_purchase,  
+  convo_type,
+  count(distinct conversation_id) as total_convos
+  from
+    `etsy-data-warehouse-prod.rollups.convo_transactions` a
+  where date(convo_start_date) >= '2024-01-01' and date(convo_start_date) <= '2024-12-31'
+  group by all 
+  order by 1 desc
