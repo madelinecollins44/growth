903ef5d0c3f22bf80f546075b84f3d5215275e18
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-05-09 16:17:08 -0400

    Update actions_in_cart.sql

--- web_conversions/cart/engagement_analysis/actions_in_cart.sql
+++ web_conversions/cart/engagement_analysis/actions_in_cart.sql
@@ -4,21 +4,29 @@
 -- SAVE FOR LATER/ REMOVE FROM CART
 select
   platform,
-  -- buyer_segment,
-  new_visitor,
+  buyer_segment,
+  -- new_visitor,
   --browser level metrics
   count(distinct case when event_type in ('cart_view') then browser_id end) as browsers_w_cart,
   count(distinct case when event_type in ('checkout_add_to_saved_for_later') then browser_id end) as browsers_w_save_for_later,
   count(distinct case when event_type in ('cart_listing_removed') then browser_id end) as browsers_w_listing_removed,
   -- event level metrics 
-  count(case when event_type in ('cart_view') then sequence_number end) as cart_views,
-  count(case when event_type in ('checkout_add_to_saved_for_later') then sequence_number end) as save_for_laters,
-  count(case when event_type in ('cart_listing_removed') then sequence_number end) as remove_listings,
+  count(case when event_type in ('cart_view') then visit_id end) as cart_views,
+  count(case when event_type in ('checkout_add_to_saved_for_later') then visit_id end) as save_for_laters,
+  count(case when event_type in ('cart_listing_removed') then visit_id end) as remove_listings,
+  count(distinct case when event_type in ('cart_view') then visit_id end) as visits_w_cart_view,
+  count(distinct case when event_type in ('checkout_add_to_saved_for_later') then visit_id end) as visits_w_s4l,
+  count(distinct case when event_type in ('cart_listing_removed') then visit_id end) as visits_w_remove_listing,
+  -- count(case when event_type in ('cart_view') then visit_id end) as visits_w_cart_view,
+  -- count(case when event_type in ('checkout_add_to_saved_for_later') then visit_id end) as visits_w_s4l,
+  -- count(case when event_type in ('cart_listing_removed') then visit_id end) as visits_w_remove_listing,
 from 
   etsy-data-warehouse-dev.madelinecollins.cart_engagement_browsers
 inner join 
   etsy-data-warehouse-prod.weblog.events using (visit_id)
-where event_type in ('cart_view','checkout_add_to_saved_for_later','cart_listing_removed')
+where
+  event_type in ('cart_view','checkout_add_to_saved_for_later','cart_listing_removed') 
+  and _date >= current_date-30
 group by all 
 order by 1,2 desc
 
