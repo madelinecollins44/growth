fc32609a09ffafaa69bf267687cc6982fa332a10
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-05-09 09:36:03 -0400

    Update basic_tables_and_stats.sql

--- web_conversions/cart/engagement_analysis/basic_tables_and_stats.sql
+++ web_conversions/cart/engagement_analysis/basic_tables_and_stats.sql
@@ -17,29 +17,3 @@ where
   and platform in ('desktop','mobile_web','boe')
   and event_type in ('cart_view')
 ); 
-
-
--- how many visits to cart will a browser have over one month?
-with agg as (
-select
-  platform,
-  -- buyer_segment,
-  -- new_visitor, 
-  browser_id,
-  count(visit_id) as cart_views,
-from 
-    etsy-data-warehouse-dev.madelinecollins.cart_engagement_browsers
-group by all
-)
-select 
-  platform,
-  -- buyer_segment,
-  -- new_visitor, 
-  count(distinct browser_id) as browsers,
-  avg(cart_views) as avg_cart_views,
-  sum(cart_views) as cart_views
-from 
-  agg
-group by all 
-
-
