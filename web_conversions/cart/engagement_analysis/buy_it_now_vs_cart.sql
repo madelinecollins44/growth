4b481e4d8fb841d09861eb03a05030d92a615e2d
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-05-09 10:03:32 -0400

    Update buy_it_now_vs_cart.sql

--- web_conversions/cart/engagement_analysis/buy_it_now_vs_cart.sql
+++ web_conversions/cart/engagement_analysis/buy_it_now_vs_cart.sql
@@ -3,15 +3,17 @@ select
   case when v.user_id is null or v.user_id = 0 then 0 else 1 end as buyer_segment,
   new_visitor, 
   -- browser metrics 
+  count(distinct browser_id) as browsers,
   count(distinct case when event_type in ('cart_view') then browser_id end) as browsers_w_cart_view,
-  count(distinct case when event_type in ('main_buybox_express_checkout_clicked') then browser_id end) as browsers_w_buy_it_now,
-  count(distinct case when event_type in ('main_buybox_express_checkout_clicked','cart_view') then browser_id end) as browsers_w_both,
+  count(distinct case when event_type in ('main_buybox_express_checkout_clicked','buy_it_now_tapped') then browser_id end) as browsers_w_buy_it_now,
+  count(distinct case when event_type in ('main_buybox_express_checkout_clicked','cart_view','buy_it_now_tapped') then browser_id end) as browsers_w_both,
   -- visit metrics 
+  count(distinct visit_id) as total_visits,
   count(distinct case when event_type in ('cart_view') then visit_id end) as visits_w_cart_view,
-  count(distinct case when event_type in ('main_buybox_express_checkout_clicked') then visit_id end) as visits_w_buy_it_now,
-  count(distinct case when event_type in ('main_buybox_express_checkout_clicked','cart_view') then visit_id end) as visits_w_both,
+  count(distinct case when event_type in ('main_buybox_express_checkout_clicked','buy_it_now_tapped') then visit_id end) as visits_w_buy_it_now,
+  count(distinct case when event_type in ('main_buybox_express_checkout_clicked','cart_view','buy_it_now_tapped') then visit_id end) as visits_w_both,
   count(case when event_type in ('cart_view') then visit_id end) as cart_views,
-  count(case when event_type in ('main_buybox_express_checkout_clicked') then visit_id end) as buy_it_nows,
+  count(case when event_type in ('main_buybox_express_checkout_clicked','buy_it_now_tapped') then visit_id end) as buy_it_nows,
 from
   etsy-data-warehouse-prod.weblog.visits v
 inner join 
@@ -19,6 +21,6 @@ inner join
 where
   v._date >= current_date-30
   and platform in ('desktop','mobile_web','boe')
-  and event_type in ('cart_view','main_buybox_express_checkout_clicked')
+  and event_type in ('cart_view','main_buybox_express_checkout_clicked','buy_it_now_tapped')
   and converted > 0 -- only converted visits 
 group by all 
