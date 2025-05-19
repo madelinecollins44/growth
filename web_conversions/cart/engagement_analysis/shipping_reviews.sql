fc8443b94045548b7ca8480fca7cc142500b7d65
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-05-19 09:55:20 -0400

    Update shipping_reviews.sql

--- web_conversions/cart/engagement_analysis/shipping_reviews.sql
+++ web_conversions/cart/engagement_analysis/shipping_reviews.sql
@@ -1,1 +1,21 @@
 
+select
+  case 
+    when regexp_contains(lower(review), r'\b(ship(ping)?|delivery|arrive|arrival|transit|dispatch|estimated time|ETA|receive)\b') then 1
+    else 0
+  end as mentions_shipping,
+  top_category,
+  avg(rating) as avg_rating,
+  count(distinct transaction_id)
+from 
+  etsy-data-warehouse-prod.rollups.transaction_reviews
+where 
+  has_review > 0 
+and DATE(transaction_date) >= current_date-365
+group by all 
+order by 1,2 desc
+
+  -- REGEXP_CONTAINS(LOWER(your_column), r'\b((fast|quick|on[- ]time|prompt|speed(y|ily)|efficient|earl(y|ier))\b.*\b(ship(ping)?|deliver(ed|y)?|arriv(e|al|ing)?|transit|dispatch|receive(d)?)\b|\b(ship(ping)?|deliver(ed|y)?|arriv(e|al|ing)?|transit|dispatch|receive(d)?)\b.*\b(fast|quick|on[- ]time|prompt|speed(y|ily)|efficient|earl(y|ier))\b)') AS said_positive_shipping
+
+-- SELECT *,
+  -- REGEXP_CONTAINS(LOWER(your_column), r'\b((slow|late|delay(ed)?|never arrived|long wait|terrible|bad|poor|awful|missing|lost|didn[’'\']?t (arrive|come|ship|receive))\b.*\b(ship(ping)?|deliver(ed|y)?|arriv(e|al|ing)?|transit|dispatch|receive(d)?)\b|\b(ship(ping)?|deliver(ed|y)?|arriv(e|al|ing)?|transit|dispatch|receive(d)?)\b.*\b(slow|late|delay(ed)?|never arrived|long wait|terrible|bad|poor
