e4c42515b878e06dfd4c57e5d75fd16105d8b812
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-05-19 14:31:13 -0400

    Update basic_tables_and_stats.sql

--- web_conversions/cart/engagement_analysis/basic_tables_and_stats.sql
+++ web_conversions/cart/engagement_analysis/basic_tables_and_stats.sql
@@ -21,3 +21,30 @@ group by all
 ); 
 
 
+with key_events as (
+select
+  distinct visit_id
+from 
+  etsy-data-warehouse-prod.weblog.events
+where 
+  _date >= current_date-30 
+  and event_type in ('cart_view')
+)
+select
+  platform,
+  new_visitor,
+  case when v.user_id is null or v.user_id = 0 then 0 else 1 end as buyer_segment,
+  count(distinct browser_id) as browsers,
+  count(distinct visit_id) as visits,
+  sum(total_gms) as total_gms, 
+  count(distinct case when ev.visit_id is not null then browser_id end) as cart_browsers,
+  count(distinct case when ev.visit_id is not null then visit_id end) as cart_visits,
+  sum(case when ev.visit_id is not null then total_gms end) as cart_gms,
+from 
+  etsy-data-warehouse-prod.weblog.visits v
+left join 
+  key_events ev using (visit_id)
+where 
+  v._date >= current_date-30
+group by all 
+  
