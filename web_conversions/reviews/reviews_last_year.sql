ab42dde12c3d9793f6e9f1f441d944e13dccbc06
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-05-29 13:10:16 -0400

    Update reviews_last_year.sql

--- web_conversions/reviews/reviews_last_year.sql
+++ web_conversions/reviews/reviews_last_year.sql
@@ -1,3 +1,49 @@
+with listing_reviews as (
+select
+  listing_id,
+  -- case when date(transaction_date) >= current_date-365 then 1 else 0 end as reviews_in_last_year,
+  -- case when date(transaction_date) < current_date-365 then 1 else 0 end as reviews_before_last_year,
+  count(distinct transaction_id) as total_reviews,
+  count(distinct case when date(transaction_date) >= current_date-365 then transaction_id end) reviews_in_last_year,
+  count(distinct case when date(transaction_date) < current_date-365 then transaction_id end) reviews_in_before_last_year
+from 
+  etsy-data-warehouse-prod.rollups.transaction_reviews  
+where 
+  has_review > 0 -- only listings 
+group by all
+)
+, listing_views as (
+select
+  platform,
+  listing_id,
+  count(sequence_number) as total_views
+from 
+  etsy-data-warehouse-prod.analytics.listing_views
+where 
+  _date >= current_date-30
+  and platform in ('desktop','mobile_web','boe')
+group by all
+) 
+select
+  case 
+    when reviews_in_last_year > 0 and reviews_in_before_last_year = 0 then 'only_reviews_this_year'
+    when reviews_in_last_year = 0 and reviews_in_before_last_year > 0 then 'only_reviews_before_this_year'
+    when reviews_in_last_year > 0 and reviews_in_before_last_year > 0 then 'reviews_in_both'
+  end as reviews_type,
+  count(distinct a.listing_id) as active_listings,
+  count(distinct v.listing_id) as viewed_listings,
+  count(distinct case when platform in ('desktop') then v.listing_id end) as desktop_listing_views,
+  count(distinct case when platform in ('mobile_web') then v.listing_id end) as mweb_listing_views,
+  count(distinct case when platform in ('boe') then v.listing_id end) as boe_listing_views
+from
+  listing_reviews r 
+left join  
+  etsy-data-warehouse-prod.rollups.active_listing_basics a using (listing_id)
+left join 
+  listing_views v
+    on a.listing_id=r.listing_id
+group by all 
+  
 --------------------------------------------------------------------------------------------------------
 -- TESTING
 --------------------------------------------------------------------------------------------------------
