f416cef8e467154231039297b4e97d56747210bb
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-06-11 10:09:32 -0400

    Update analysis_code.sql

--- web_conversions/experiments/lp_move_appreciation_photos_mweb/analysis_code.sql
+++ web_conversions/experiments/lp_move_appreciation_photos_mweb/analysis_code.sql
@@ -1,29 +1,42 @@
+with photo_reviews as (
 select
-  case 
-    when reviews = 0 then '0'  
-    when reviews = 1 then '1'  
-    when reviews = 2 then '2'  
-    when reviews = 3 then '3'  
-    when reviews = 4 then '4' 
-    else '5+'
-  end as photo_count,
-  count(distinct bl.bucketing_id) as bucketed_units,
-  count(distinct bl.segment_value) as listings,
-  count(lv.sequence_number) as listing_views,
-  sum(purchased_after_view) as purchases, 
-from 
-  etsy-bigquery-adhoc-prod._script719e520a9cef39b06b88b49724324a3998ec5543.bucketing_listing_ids bl -- listing_ids of bucketed units 
-left join 
-  (select 
-    listing_id, 
-    coalesce(sum(has_review),0) as reviews 
-  from etsy-data-warehouse-prod.rollups.transaction_reviews 
-  group by all) tr
-    on bl.segment_value = cast(tr.listing_id as string)
-left join 
-  etsy-data-warehouse-prod.analytics.listing_views lv 
-    on bl.segment_value = cast(lv.listing_id as string)
-    and bl.bucketing_id = split(lv.visit_id, ".")[0] -- browser_id
-    and bl.sequence_number= lv.sequence_number 
-    and _date is not null 
+  listing_id,
+  sum(has_image) as images
+from etsy-data-warehouse-prod.rollups.transaction_reviews
+where has_review > 0
 group by all 
+order by 2 desc
+)
+, post_bucketing_views as (
+select 
+  lv.listing_id,
+  count(distinct bl.bucketing_id) as browsers_that_viewed,
+  count(lv.sequence_number) as listing_views,
+  sum(purchased_after_view) as purchases
+from
+  etsy-bigquery-adhoc-prod._script7472bfed173f9e1e2d8ad0bb22386768877334ae.bucketing_listing bl -- all info from first listing unit was bucketed on 
+left join
+  etsy-data-warehouse-prod.analytics.listing_views lv
+    on bl.bucketing_id=split(lv.visit_id, ".")[0] -- browser ids
+    and bl.sequence_number <= lv.sequence_number -- all listing views bucketing moment and after 
+where lv._date between date('2025-05-20') and date('2025-05-27') -- dates of the experiment 
+group by all 
+)
+select
+  case
+    when images = 0 then '0'
+    when images = 1 then '1'
+    when images = 2 then '2'
+    when images = 3 then '3'
+    when images = 4 then '4'
+    else '5+' 
+  end as review_photos,
+  count(distinct listing_id) as listings,
+  sum(listing_views) as listing_views,
+  sum(purchases) as purchases 
+from 
+  post_bucketing_views
+left join 
+  photo_reviews using (listing_id)
+group by all 
+order by 1 asc
