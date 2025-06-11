0dc726e8ee81ecdfb5d383ddbf3cefbf6d7bdfcb
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-06-11 00:01:19 -0400

    Create listing_views_table.sql

---
+++ web_conversions/experiments/listing_id_from_bucketing_moment/listing_views_table.sql
@@ -0,0 +1,38 @@
+with listing_views as (
+select
+  _date,
+  split(visit_id, ".")[0] as bucketing_id, -- browser_id
+  listing_id,
+  timestamp_millis(epoch_ms) as listing_ts
+from 
+  etsy-data-warehouse-prod.analytics.listing_views
+where 
+  _date >= current_date-30 -- this will be within time of experiment 
+)
+, bucketing_moment as ( -- pulls out bucketing moment 
+select 
+  min(_date) as _date,
+  min(bucketing_ts) as bucketing_ts,  -- first bucketing moment, this is what will be used to join time 
+  bucketing_id,
+  experiment_id,
+from
+  `etsy-data-warehouse-prod.catapult_unified.bucketing`
+where
+  experiment_id = 'growth_regx.lp_move_appreciation_photos_mweb'
+group by all 
+) 
+, agg as (
+select
+  bm.bucketing_id,
+  bm.bucketing_ts,
+  lv.listing_id,
+  lv.listing_ts,
+  abs(timestamp_diff(bm.bucketing_ts,lv.listing_ts,second)) as abs_time_between
+from 
+  bucketing_moment bm
+left join  
+  listing_views lv 
+    using (bucketing_id, _date)
+qualify row_number() over (partition by bucketing_id order by abs(timestamp_diff(bm.bucketing_ts,lv.listing_ts,second)) asc) = 1  -- takes listing id closest to bucketing moment
+)
+select count(distinct bucketing_id), count(distinct case when listing_id is null then bucketing_id end), count(distinct case when listing_id is null then bucketing_id end)/ count(distinct bucketing_id) from agg group by all --order by 2 desc limit 5
