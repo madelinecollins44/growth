72ef23a2ed46b0ef279078864a7ba9f0552b1144
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-06-17 12:24:16 -0400

    Update review_engagement.sql

--- web_conversions/reviews/revamped_review_on_listing_page/review_engagement.sql
+++ web_conversions/reviews/revamped_review_on_listing_page/review_engagement.sql
@@ -12,5 +12,5 @@ inner join
 where 1=1
 	and date(_partitiontime) between date('2025-03-01') and date('2025-03-15') -- two weeks before first reviews experiment was ramped 
   -- and date(_partitiontime) between date('2025-06-10') and date('2025-06-24') -- two weeks after last reviews experiment was ramped 
-  and beacon.event_name  in ('view_listing',,'listing_page_reviews_seen','listing_page_reviews_container_top_seen','listing_page_review_engagement_frontend','listing_page_reviews_pagination','appreciation_photo_overlay_opened','sort_reviews','reviews_categorical_tag_clicked','listing_page_reviews_content_toggle_opened')
+  and beacon.event_name  in ('view_listing','listing_page_reviews_seen','listing_page_reviews_container_top_seen','listing_page_review_engagement_frontend','listing_page_reviews_pagination','appreciation_photo_overlay_opened','sort_reviews','reviews_categorical_tag_clicked','listing_page_reviews_content_toggle_opened')
   group by all 
