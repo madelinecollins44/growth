2625a0bc012d77be371c9e6c5ce33a8d55bdb2ab
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-06-18 14:12:22 -0400

    Create listing_page_conversion.sql

---
+++ web_conversions/reviews/revamped_review_on_listing_page/listing_page_conversion.sql
@@ -0,0 +1,25 @@
+select
+	date(_partitiontime) as _date,
+	v.visit_id,
+	vb.sequence_number,
+  v.converted,
+	beacon.event_name as event_name,
+  (select value from unnest(beacon.properties.key_value) where key = "tag_name") as tag_name, 
+  (select value from unnest(beacon.properties.key_value) where key = "tag_type") as tag_type, 
+from
+	`etsy-visit-pipe-prod.canonical.visit_id_beacons` vb
+inner join 
+  etsy-data-warehouse-prod.weblog.visits v -- only looking at browsers in the experiment 
+    on v.visit_id = vb.visit_id -- everything that happens on bucketing moment and after (cant do sequence number bc there is only one)
+where 1=1
+  and v._date between date('2025-06-10') and date('2025-06-24') -- two weeks after last reviews experiment was ramped 
+  and date(_partitiontime) between date('2025-06-10') and date('2025-06-24') -- two weeks after last reviews experiment was ramped
+  and platform in ('desktop')
+  and beacon.event_name in 
+    ('view_listing', 
+    'listing_page_reviews_container_top_seen', -- scrolls far enough to see tags 
+    'reviews_categorical_tag_clicked', -- clicks on a tag
+    'listing_page_review_engagement_frontend' -- review engagement event 
+    )
+  group by all 
+);
