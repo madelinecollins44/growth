637488b1ae3b4d7ac3a356353def6f7f2ad6e928
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-06-18 11:16:02 -0400

    Update cat_tag_clicks.sql

--- web_conversions/reviews/revamped_review_on_listing_page/cat_tag_clicks.sql
+++ web_conversions/reviews/revamped_review_on_listing_page/cat_tag_clicks.sql
@@ -1,4 +1,5 @@
-with tags as (
+begin
+create or replace temp table tag_info as (
 select
 	date(_partitiontime) as _date,
 	v.visit_id,
@@ -13,7 +14,9 @@ inner join
   etsy-data-warehouse-prod.weblog.visits v -- only looking at browsers in the experiment 
     on v.visit_id = vb.visit_id -- everything that happens on bucketing moment and after (cant do sequence number bc there is only one)
 where 1=1
-  -- and date(_partitiontime) between date('2025-06-10') and date('2025-06-24') -- two weeks after last reviews experiment was ramped 
+  and v._date between date('2025-06-10') and date('2025-06-24') -- two weeks after last reviews experiment was ramped 
+  and date(_partitiontime) between date('2025-06-10') and date('2025-06-24') -- two weeks after last reviews experiment was ramped 
+  and platform in ('desktop')
   and beacon.event_name in 
     ('view_listing', 
     'listing_page_reviews_container_top_seen', -- scrolls far enough to see tags 
@@ -22,6 +25,16 @@ where 1=1
     'reviews_categorical_tag_filter_applied' -- clicks on a tag and reviews filter 
     )
   group by all 
-)
+);
+end
+
+-- clicks by tag name
 select
-  
+  tag_name,
+  tag_type,
+  count(sequence_number) as clicks,
+  count(distinct listing_id) as listings_w_clicks
+from 
+  tag_info
+where 
+  event_type in ('reviews_categorical_tag_clicked')
