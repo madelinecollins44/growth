04b6dce4975ae270073d87e36e3f914e12e2e21d
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-06-18 14:22:10 -0400

    Update listing_page_conversion.sql

--- web_conversions/reviews/revamped_review_on_listing_page/listing_page_conversion.sql
+++ web_conversions/reviews/revamped_review_on_listing_page/listing_page_conversion.sql
@@ -1,24 +1,41 @@
+with agg as (
 select
-	date(_partitiontime) as _date,
+  platform,
 	v.visit_id,
-  v.converted,
-	beacon.event_name as event_name,
-  (select value from unnest(beacon.properties.key_value) where key = "tag_name") as tag_name, 
-  (select value from unnest(beacon.properties.key_value) where key = "tag_type") as tag_type, 
+  converted,
+  event_type, 
+  count(sequence_number) as views
 from
-	`etsy-visit-pipe-prod.canonical.visit_id_beacons` vb
+	etsy-data-warehouse-prod.weblog.events e
 inner join 
   etsy-data-warehouse-prod.weblog.visits v -- only looking at browsers in the experiment 
-    on v.visit_id = vb.visit_id -- everything that happens on bucketing moment and after (cant do sequence number bc there is only one)
+    on v.visit_id = e.visit_id -- everything that happens on bucketing moment and after (cant do sequence number bc there is only one)
 where 1=1
   and v._date between date('2025-06-10') and date('2025-06-24') -- two weeks after last reviews experiment was ramped 
-  and date(_partitiontime) between date('2025-06-10') and date('2025-06-24') -- two weeks after last reviews experiment was ramped
-  and platform in ('desktop')
-  and beacon.event_name in 
+  and platform in ('desktop','mobile_web')
+  and event_type in 
     ('view_listing', 
     'listing_page_reviews_container_top_seen', -- scrolls far enough to see tags 
     'reviews_categorical_tag_clicked', -- clicks on a tag
     'listing_page_review_engagement_frontend' -- review engagement event 
     )
   group by all 
-);
+)
+select
+-- view listing  
+  count(distinct visit_id) as visits,
+  count(distinct case when converted > 0 then visit_id end) as converted_visits,
+-- view listing  
+  count(distinct case when event_type in ('view_listing') then visit_id end) as lv_visits,
+  count(distinct case when converted > 0 and event_type in ('view_listing') then visit_id end) as lv_converted_visits,
+-- listing_page_reviews_container_top_seen 
+  count(distinct case when event_type in ('listing_page_reviews_container_top_seen') then visit_id end) as reviews_seen_visits,
+  count(distinct case when converted > 0 and event_type in ('listing_page_reviews_container_top_seen') then visit_id end) as reviews_seen_converted_visits,
+  -- reviews_categorical_tag_clicked
+  count(distinct case when event_type in ('reviews_categorical_tag_clicked') then visit_id end) as cattag_click_visits,
+  count(distinct case when converted > 0 and event_type in ('reviews_categorical_tag_clicked') then visit_id end) as cattag_click_converted_visits,
+  -- listing_page_review_engagement_frontend
+  count(distinct case when event_type in ('listing_page_review_engagement_frontend') then visit_id end) as review_engagement_visits,
+  count(distinct case when converted > 0 and event_type in ('listing_page_review_engagement_frontend') then visit_id end) as review_engagement_converted_visits,
+from 
+  agg
