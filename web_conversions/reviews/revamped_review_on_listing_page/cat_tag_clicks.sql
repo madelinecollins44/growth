047a003f9f9bf999d407002604ac700e324ed9de
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-06-18 11:52:43 -0400

    Update cat_tag_clicks.sql

--- web_conversions/reviews/revamped_review_on_listing_page/cat_tag_clicks.sql
+++ web_conversions/reviews/revamped_review_on_listing_page/cat_tag_clicks.sql
@@ -83,3 +83,66 @@ from
 left join 
   (select * from tag_info where event_type in ('reviews_categorical_tag_clicked')) c   
     using (listing_id)
+
+
+-- conversion among visits that clicked on a tag
+begin
+create or replace temp table tag_info as (
+select
+	date(_partitiontime) as _date,
+	v.visit_id,
+	v.sequence_number,
+	beacon.event_name as event_name,
+  (select value from unnest(beacon.properties.key_value) where key = "tag_name") as tag_name, 
+  (select value from unnest(beacon.properties.key_value) where key = "tag_type") as tag_type, 
+  coalesce((select value from unnest(beacon.properties.key_value) where key = "listing_id"), regexp_extract(beacon.loc, r'listing/(\d+)')) as listing_id 
+from
+	`etsy-visit-pipe-prod.canonical.visit_id_beacons` vb
+inner join 
+  etsy-data-warehouse-prod.weblog.visits v -- only looking at browsers in the experiment 
+    on v.visit_id = vb.visit_id -- everything that happens on bucketing moment and after (cant do sequence number bc there is only one)
+where 1=1
+  and v._date between date('2025-06-10') and date('2025-06-24') -- two weeks after last reviews experiment was ramped 
+  and date(_partitiontime) between date('2025-06-10') and date('2025-06-24') -- two weeks after last reviews experiment was ramped 
+  and platform in ('desktop')
+  and beacon.event_name in 
+    ('view_listing', 
+    'listing_page_reviews_container_top_seen', -- scrolls far enough to see tags 
+    'reviews_categorical_tags_seen', -- sees the tags 
+    'reviews_categorical_tag_clicked', -- clicks on a tag
+    'reviews_categorical_tag_filter_applied' -- clicks on a tag and reviews filter 
+    )
+  group by all 
+);
+end
+
+-- clicks by tag name / listing attribute 
+with lv_stats (
+select
+  listing_id,
+  visit_id,
+  count(sequence_number) as views,
+  sum(purchased_after_view) as purchases,
+from 
+  etsy-data-warehouse-prod.analytics.listing_views 
+where 1=1
+  and _date between date('2025-06-10') and date('2025-06-24') -- two weeks after last reviews experiment was ramped 
+  and .platform in ('desktop')
+group by all 
+)
+, lv_engagement as (
+select
+  v.visit_id,
+  v.listing_id,
+  count(v.sequence_number) as views,
+  count(case when c.visit_id is not null then c.sequence_number) as cat_tag_clicks,
+from
+    tag_info v
+left join 
+    tag_info c
+    on v.visit_id = c.visit_id
+    and v.listing_id = c.listing_id
+    and c.event_name = 'cat tag clicked'
+where
+    v.event_name = 'view_listing'
+)
