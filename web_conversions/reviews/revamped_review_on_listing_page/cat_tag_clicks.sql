24ce6744fd828dafa9bdff9c241b5753ee0d6dbf
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-06-18 11:08:10 -0400

    Update cat_tag_clicks.sql

--- web_conversions/reviews/revamped_review_on_listing_page/cat_tag_clicks.sql
+++ web_conversions/reviews/revamped_review_on_listing_page/cat_tag_clicks.sql
@@ -1,1 +1,27 @@
-
+with tags as (
+select
+	date(_partitiontime) as _date,
+	v.visit_id,
+	v.sequence_number,
+	beacon.event_name as event_name,
+  (select value from unnest(beacon.properties.key_value) where key = "tag_name") as tag_name, 
+  (select value from unnest(beacon.properties.key_value) where key = "tag_type") as tag_type, 
+  coalesce((select value from unnest(beacon.properties.key_value) where key = "listing_id"), regexp_extract(beacon.loc, r'listing/(\d+)')) as listing_id 
+from
+	`etsy-visit-pipe-prod.canonical.visit_id_beacons` vb
+inner join 
+  etsy-data-warehouse-prod.weblog.visits v -- only looking at browsers in the experiment 
+    on v.visit_id = vb.visit_id -- everything that happens on bucketing moment and after (cant do sequence number bc there is only one)
+where 1=1
+  -- and date(_partitiontime) between date('2025-06-10') and date('2025-06-24') -- two weeks after last reviews experiment was ramped 
+  and beacon.event_name in 
+    ('view_listing', 
+    'listing_page_reviews_container_top_seen', -- scrolls far enough to see tags 
+    'reviews_categorical_tags_seen', -- sees the tags 
+    'reviews_categorical_tag_clicked', -- clicks on a tag
+    'reviews_categorical_tag_filter_applied' -- clicks on a tag and reviews filter 
+    )
+  group by all 
+)
+select
+  
