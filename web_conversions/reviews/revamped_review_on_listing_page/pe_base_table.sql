a16616bae36fadf2c825ce18a0fc9a4586dd9173
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-07-02 15:34:15 -0400

    Update pe_base_table.sql

--- web_conversions/reviews/revamped_review_on_listing_page/pe_base_table.sql
+++ web_conversions/reviews/revamped_review_on_listing_page/pe_base_table.sql
@@ -1,6 +1,3 @@
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- This table aggregates all the browsers in the local buyer trust pe. it includes every visit after the bucketing moment, as well as the bucketing time stamp.
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 DECLARE config_flag_param STRING DEFAULT "local_pe.q2_2025.buyer_trust_accelerator.browser";
 DECLARE start_date DATE; -- DEFAULT "2023-08-22";
 DECLARE end_date DATE; -- DEFAULT "2023-09-04";
@@ -55,60 +52,69 @@ END IF;
 -- Get the first bucketing moment for each experimental unit (e.g. browser or user).
 -- If is_event_filtered is true, then only select experimental unit whose `filtered_bucketing_ts` is defined.
 CREATE OR REPLACE TABLE `etsy-data-warehouse-dev.madelinecollins.ab_first_bucket` AS ( -- find the 
-    SELECT
-        bucketing_id,
-        bucketing_id_type,
-        variant_id,
-        date(bucketing_ts) as bucketing_date,
-        IF(is_event_filtered, filtered_bucketing_ts, bucketing_ts) AS bucketing_ts,
-    FROM
-        `etsy-data-warehouse-prod.catapult_unified.bucketing_period`
-    WHERE
-        _date = end_date
-        AND experiment_id = config_flag_param
-        AND ((NOT is_event_filtered) OR (filtered_bucketing_ts IS NOT NULL))
+with agg as (
+sELECT 
+  _date,
+  variant_id,
+  bucketing_ts, 
+  bucketing_id,
+  experiment_id,
+  (select id from unnest(associated_ids) where id_type = 3) as visit_id,
+  (select cast(id as int) from unnest(associated_ids) where id_type = 4) as sequence_number
+from
+  `etsy-data-warehouse-prod.catapult_unified.bucketing`
+where
+  experiment_id = 'local_pe.q2_2025.buyer_trust_accelerator.browser'
+qualify row_number() over (partition by bucketing_id order by bucketing_ts asc) = 1  -- takes the info from the first bucketing moment 
+)
+select * from agg where _date >= date('2025-06-10') 
 );
 
 -------------------------------------------------------------------------------------------
--- GET ALL VISITS FOR BROWSERS BUCKETED INTO THE PE POST CAT TAGS BEING RAMPED UP 
+-- Create table to get all listing engagements for browsers in the pe 
 -------------------------------------------------------------------------------------------
-create or replace table etsy-data-warehouse-dev.madelinecollins.browsers_in_pe_post_cattags as (
+create or replace table etsy-data-warehouse-dev.madelinecollins.browsers_in_pe_listing_views as (
 select
-  f.bucketing_date,
-  f.bucketing_ts, -- this is the bucketing timestamp, use this to look at all events AFTER the bucketing timestamp 
-  f.variant_id,
-  f.bucketing_id,
-  v.browser_id,
-  v.visit_id,
-  v._date as visit_date
-from 
-  etsy-data-warehouse-dev.madelinecollins.ab_first_bucket f
+	a._date,
+    v.variant_id,
+	a.visit_id,
+    v.bucketing_id, 
+	a.sequence_number,
+	'view_listing'as event_name,
+    listing_id,
+from
+	etsy-data-warehouse-prod.analytics.listing_views a 
 inner join 
-  etsy-data-warehouse-prod.weblog.visits v
-    on f.bucketing_id=v.browser_id -- browsers in the pe 
-    and f.bucketing_ts <= v.start_datetime -- all visits after the initial bucketed visit
+  etsy-data-warehouse-dev.madelinecollins.ab_first_bucket v -- only looking at browsers in the experiment 
+    on split(a.visit_id, ".")[0] = v.bucketing_id 
+    and v.bucketing_ts <= timestamp_millis(a.epoch_ms) -- listing views before or after bucketing moment 
+    -- and ((vb.visit_id = v.visit_id AND vb.sequence_number >= v.sequence_number) -- in the bucketing visit, anything after the bucketing sequence number 
+    --        or (vb.visit_id > v.visit_id)) -- everything after the bucketing visit 
 where 1=1
-  and f.bucketing_date >= date('2025-06-10') -- two weeks after the cat tag experiment was ramped up 
-  and v._date >=  date('2025-06-10') -- two weeks after the cat tag experiment was ramped up 
+  and a._date >= date('2025-06-10') 
+group by all 
 );
 
--------------------------------------------------------------------------------------------
--- Pull all listing events for a browser post bucketing
--------------------------------------------------------------------------------------------
-create or replace table etsy-data-warehouse-dev.madelinecollins.browsers_in_pe_listing_engagements as (
+create or replace table etsy-data-warehouse-dev.madelinecollins.browsers_in_pe_review_engagements as (
 select
 	date(_partitiontime) as _date,
-  v.variant_id,
-	v.visit_id,
-	v.sequence_number,
+    v.variant_id,
+	vb.visit_id,
+    v.bucketing_id,
+	vb.sequence_number,
 	beacon.event_name as event_name,
   coalesce((select value from unnest(beacon.properties.key_value) where key = "listing_id"), regexp_extract(beacon.loc, r'listing/(\d+)')) as listing_id 
 from
 	`etsy-visit-pipe-prod.canonical.visit_id_beacons` vb
 inner join 
-  etsy-data-warehouse-dev.madelinecollins.browsers_in_pe_post_cattags v -- only looking at browsers in the experiment 
-    on v.bucketing_ts <= vb.beacon.timestamp -- join to the browser table for everything AFTER the browser was bucketed into the PE  
+  etsy-data-warehouse-dev.madelinecollins.ab_first_bucket v -- only looking at browsers in the experiment 
+    on split(vb.visit_id, ".")[0] = v.bucketing_id 
+    and v.bucketing_ts <= timestamp_millis(vb.beacon.timestamp) 
+    -- and ((vb.visit_id = v.visit_id AND vb.sequence_number >= v.sequence_number) -- in the bucketing visit, anything after the bucketing sequence number 
+    --        or (vb.visit_id > v.visit_id)) -- everything after the bucketing visit 
 where 1=1
-  and beacon.event_name in ('view_listing','listing_page_reviews_seen','listing_page_reviews_container_top_seen','listing_page_review_engagement_frontend','listing_page_reviews_pagination','appreciation_photo_overlay_opened','sort_reviews','reviews_categorical_tag_clicked','reviews_categorical_tags_seen','listing_page_reviews_content_toggle_opened')
+  and date(_partitiontime) >= date('2025-06-10') 
+  and beacon.event_name in ('listing_page_reviews_seen','listing_page_reviews_container_top_seen','listing_page_review_engagement_frontend','listing_page_reviews_pagination','appreciation_photo_overlay_opened','sort_reviews','reviews_categorical_tag_clicked','reviews_categorical_tags_seen','listing_page_reviews_content_toggle_opened')
   group by all 
 );
+ 
