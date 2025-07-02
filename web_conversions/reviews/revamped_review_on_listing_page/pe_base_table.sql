aa08693643c24401dd395dcf74e42e6f769e06be
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-07-02 12:39:08 -0400

    Update pe_base_table.sql

--- web_conversions/reviews/revamped_review_on_listing_page/pe_base_table.sql
+++ web_conversions/reviews/revamped_review_on_listing_page/pe_base_table.sql
@@ -91,3 +91,24 @@ where 1=1
   and f.bucketing_date >= date('2025-06-10') -- two weeks after the cat tag experiment was ramped up 
   and v._date >=  date('2025-06-10') -- two weeks after the cat tag experiment was ramped up 
 );
+
+-------------------------------------------------------------------------------------------
+-- Pull all listing events for a browser post bucketing
+-------------------------------------------------------------------------------------------
+create or replace table etsy-data-warehouse-dev.madelinecollins.browsers_in_pe_listing_engagements as (
+select
+	date(_partitiontime) as _date,
+  v.variant_id,
+	v.visit_id,
+	v.sequence_number,
+	beacon.event_name as event_name,
+  coalesce((select value from unnest(beacon.properties.key_value) where key = "listing_id"), regexp_extract(beacon.loc, r'listing/(\d+)')) as listing_id 
+from
+	`etsy-visit-pipe-prod.canonical.visit_id_beacons` vb
+inner join 
+  etsy-data-warehouse-dev.madelinecollins.browsers_in_pe_post_cattags v -- only looking at browsers in the experiment 
+    on v.bucketing_ts <= vb.beacon.timestamp -- join to the browser table for everything AFTER the browser was bucketed into the PE  
+where 1=1
+  and beacon.event_name in ('view_listing','listing_page_reviews_seen','listing_page_reviews_container_top_seen','listing_page_review_engagement_frontend','listing_page_reviews_pagination','appreciation_photo_overlay_opened','sort_reviews','reviews_categorical_tag_clicked','reviews_categorical_tags_seen','listing_page_reviews_content_toggle_opened')
+  group by all 
+);
