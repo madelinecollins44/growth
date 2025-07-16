0f24b7cef0c21836d2b3bea5173607519fc8d5b4
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-07-16 13:30:15 -0400

    Update base_tables.sql

--- web_conversions/reviews/zero_reviews_deep_dive/base_tables.sql
+++ web_conversions/reviews/zero_reviews_deep_dive/base_tables.sql
@@ -1,1 +1,15 @@
-
+with shops_wo_reviews as (
+select 
+  shop_id,
+  shop_name,
+  seller_user_id,
+  count(distinct transaction_id) as transactions,
+  sum(has_review) as total_reviews
+from 
+  etsy-data-warehouse-prod.rollups.transaction_reviews
+inner join  
+  etsy-data-warehouse-prod.rollups.seller_basics using (shop_id)
+group by all 
+having sum(has_review) = 0
+)
+select * from shops_wo_reviews order by transactions desc limit 40
