5d1a38d41e3244fd35f44093fa26caf9c37936ff
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-07-17 15:26:32 -0400

    Update analysis.sql

--- web_conversions/reviews/zero_reviews_deep_dive/analysis.sql
+++ web_conversions/reviews/zero_reviews_deep_dive/analysis.sql
@@ -2,21 +2,20 @@
 with shops_wo_reviews as (
 select 
   shop_id,
-  shop_name,
   seller_user_id,
   count(distinct transaction_id) as transactions,
   sum(has_review) as total_reviews
 from 
   etsy-data-warehouse-prod.rollups.transaction_reviews
-inner join  
-  etsy-data-warehouse-prod.rollups.seller_basics using (shop_id)
 group by all 
 )
 select
+  l.platform,
   case when total_reviews = 0 then 0 else 1 end as has_shop_reviews,
   case when transactions = 0 then 0 else 1 end as has_transactions,
   case when user_id is null or user_id= 0 then 0 else 1 end as signed_in,
   -- seller_user_id,
+  count(distinct l.listing_id) as viewed_listings, 
   sum(purchased_after_view) as purchases,
   count(sequence_number) as views, 
 from 
@@ -24,12 +23,9 @@ from
 left join 
   shops_wo_reviews r using (seller_user_id)
 inner join 
-  (select
-      user_id,
-      visit_id
-    from 
-      etsy-data-warehouse-prod.weblog.visits
-    where _date >= current_date-30) v
+  etsy-data-warehouse-prod.weblog.visits v
       on l.visit_id=v.visit_id
-where _date >= current_date-30 
+where 
+  l._date >= current_date-30 
+  and v._date >= current_date-30 
 group by all 
