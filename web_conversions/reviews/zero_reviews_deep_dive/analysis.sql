4efdf59eb74b7c26449baaa3f1f1f007fc00ad7c
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-07-17 16:11:37 -0400

    Update analysis.sql

--- web_conversions/reviews/zero_reviews_deep_dive/analysis.sql
+++ web_conversions/reviews/zero_reviews_deep_dive/analysis.sql
@@ -1,4 +1,35 @@
-/* SIGNED IN VS SIGNED OUT CONVERSION */
+/* SHOP REVIEW ON LISTING LEVEL */
+
+with shops_reviews as ( -- this looks at all listings that have been purchased and whether or not they have a review
+select 
+  shop_id,
+  seller_user_id,
+  count(distinct transaction_id) as transactions,
+  sum(has_review) as total_reviews
+from 
+  etsy-data-warehouse-prod.rollups.transaction_reviews
+group by all 
+)
+, listing_shop_reviews as (
+select
+  case when b.total_reviews > 0 or b.seller_user_id is null then 1 else 0 end has_shop_reviews,
+  a.listing_id,
+  sum(purchased_after_view) as purchases,
+  count(sequence_number) as views, 
+from
+  etsy-data-warehouse-prod.analytics.listing_views a
+left join 
+  shops_reviews b
+    using (seller_user_id)
+where  1=1
+  and a._date >= current_date-30 
+  and a.platform in ('mobile_web','desktop','boe')
+group by all
+)
+select  has_shop_reviews, sum(views) from agg group by all 
+
+  
+  /* SIGNED IN VS SIGNED OUT CONVERSION */
 with shops_wo_reviews as (
 select 
   shop_id,
