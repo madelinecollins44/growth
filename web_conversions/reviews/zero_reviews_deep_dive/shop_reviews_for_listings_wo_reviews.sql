9daa9f17eb548e0a91914b2a73957113dbc8f5c6
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-07-22 10:39:18 -0400

    Update shop_reviews_for_listings_wo_reviews.sql

--- web_conversions/reviews/zero_reviews_deep_dive/shop_reviews_for_listings_wo_reviews.sql
+++ web_conversions/reviews/zero_reviews_deep_dive/shop_reviews_for_listings_wo_reviews.sql
@@ -8,35 +8,21 @@ from
   etsy-data-warehouse-prod.rollups.transaction_reviews
 group by all 
 )
-, listing_reviews as (
-select 
-  listing_id,
-  count(distinct transaction_id) as transactions,
-  sum(has_review) as total_reviews
-from 
-  etsy-data-warehouse-prod.rollups.transaction_reviews
-group by all 
-)
 select
-  -- platform,
-  case when (s.seller_user_id is null or s.total_reviews = 0) then 0 else 1 end as has_shop_reviews,
-  -- avg(s.total_reviews) as avg_shop_review_count,
-  -- case 
-  --   s.total_reviews 
-  count(distinct a.listing_id) as listings,
+  case when r.total_reviews = 0 or r.shop_id is null then 0 else 1 end has_shop_reviews,
+  count(distinct b.listing_id) as active_listings,
   sum(purchased_after_view) as purchases,
   count(sequence_number) as views, 
 from
-  etsy-data-warehouse-prod.analytics.listing_views a
-inner join 
-  listing_reviews l
-    on a.listing_id=l.listing_id
+  etsy-data-warehouse-prod.rollups.active_listing_basics b
 left join 
-  shops_reviews s 
-   on s.seller_user_id=a.seller_user_id
+  etsy-data-warehouse-prod.analytics.listing_views a
+      on a.listing_id=b.listing_id
+left join 
+  shops_reviews r
+    on r.shop_id=b.shop_id
 where  1=1
   and a._date >= current_date-30 
   and a.platform in ('mobile_web','desktop','boe')
-  and (l.listing_id is null or l.total_reviews=0) -- listings without reviews
 group by all
--- order by 1,2,3 desc
+order by 1,2,3 desc
