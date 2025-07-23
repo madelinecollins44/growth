afc815109df44e94c57bb38df44a0cb6101be508
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-07-23 17:08:00 -0400

    Create taxo_by_shops_wo_reviews.sql

---
+++ web_conversions/reviews/zero_reviews_deep_dive/taxo_by_shops_wo_reviews.sql
@@ -0,0 +1,50 @@
+d a._date >= current_date-30 
+  and a.platform in ('mobile_web','desktop','boe')
+group by all
+order by 1,2,3 desc
+
+-- LISTING AND SHOP LEVEL
+with shops_reviews as ( -- this looks at all listings that have been purchased and whether or not they have a review
+select 
+  seller_user_id,
+  count(distinct transaction_id) as transactions,
+  sum(has_review) as total_reviews
+from 
+  etsy-data-warehouse-prod.rollups.transaction_reviews
+group by all 
+)
+, listing_reviews as ( -- this looks at all listings that have been purchased and whether or not they have a review
+select 
+  listing_id,
+  seller_user_id,
+  count(distinct tr.transaction_id) as listing_transactions,
+  sum(tr.has_review) as listing_reviews,
+  sr.total_reviews as shop_reviews 
+from 
+  etsy-data-warehouse-prod.rollups.transaction_reviews tr 
+left join 
+  shops_reviews sr
+    using (seller_user_id)
+group by all 
+)
+select
+  platform,
+  case when r.total_reviews = 0 or r.shop_id is null then 0 else 1 end has_shop_reviews,
+  top_category,
+  count(distinct b.listing_id) as active_listings,
+  sum(purchased_after_view) as purchases,
+  count(sequence_number) as views, 
+from
+  etsy-data-warehouse-prod.rollups.active_listing_basics b
+left join 
+  etsy-data-warehouse-prod.analytics.listing_views a
+      on a.listing_id=b.listing_id
+left join 
+  shops_reviews r
+    on r.shop_id=b.shop_id
+where  1=1
+  and a._date >= current_date-30 
+  and a.platform in ('mobile_web','desktop','boe')
+  and (r.shop_id is null or r.total_reviews = 0) -- shop either has no transactions or no reviews
+group by all
+order by 1,2,3 desc
