8279b59a7a4b2781cbe5058a57b8a64a21b841e1
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-07-23 10:56:11 -0400

    Update shop_reviews_for_listings_wo_reviews.sql

--- web_conversions/reviews/zero_reviews_deep_dive/shop_reviews_for_listings_wo_reviews.sql
+++ web_conversions/reviews/zero_reviews_deep_dive/shop_reviews_for_listings_wo_reviews.sql
@@ -57,3 +57,52 @@ where  1=1
   and a.platform in ('mobile_web','desktop','boe')
 group by all
 order by 1,2,3 desc
+
+-- LISTING AND SHOP LEVEL
+with shops_reviews as ( -- this looks at all listings that have been purchased and whether or not they have a review
+select 
+  seller_user_id,
+  count(distinct transaction_id) as transactions,
+  sum(has_review) as total_reviews
+from 
+  etsy-data-warehouse-prod.rollups.transaction_reviews
+group by all 
+)
+, listing_reviews as ( -- this looks at all listings that have been purchased and whether or not they have a review
+select 
+  listing_id,
+  seller_user_id,
+  count(distinct tr.transaction_id) as listing_transactions,
+  sum(tr.has_review) as listing_reviews,
+  sr.total_reviews as shop_reviews 
+from 
+  etsy-data-warehouse-prod.rollups.transaction_reviews tr 
+left join 
+  shops_reviews sr
+    using (seller_user_id)
+group by all 
+)
+select
+  platform,
+  -- case when lr.listing_reviews = 0 or lr.listing_id is null then 0 else 1 end has_listing_reviews,
+  -- case when lr.shop_reviews = 0 or lr.seller_user_id is null then 0 else 1 end has_shop_reviews,
+  case 
+    when lr.shop_reviews = 0 or lr.seller_user_id is null then 0 
+    when lr.shop_reviews <= 10 then '1-10'
+    when lr.shop_reviews <= 50 and lr.shop_reviews > 10 then '11-50'
+    when lr.shop_reviews <= 5 then '1-5'
+    else 1 end shop_reviews,
+  count(distinct a.listing_id) as active_listings,
+  sum(a.purchased_after_view) as purchases,
+  count(a.sequence_number) as views, 
+from
+  etsy-data-warehouse-prod.analytics.listing_views a
+left join 
+  listing_reviews lr
+    on lr.listing_id=a.listing_id
+where  1=1
+  and a._date >= current_date-30 
+  and a.platform in ('mobile_web','desktop','boe')
+  and (lr.listing_id is null or lr.listing_reviews = 0) -- listings without item reviews 
+group by all
+order by 1,2,3 desc
