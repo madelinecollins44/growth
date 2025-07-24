0b2f6ba7dd9d3eb310ce9836b2cd42331d8dd155
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-07-24 11:14:31 -0400

    Update taxo_by_shops_wo_reviews.sql

--- web_conversions/reviews/zero_reviews_deep_dive/taxo_by_shops_wo_reviews.sql
+++ web_conversions/reviews/zero_reviews_deep_dive/taxo_by_shops_wo_reviews.sql
@@ -43,3 +43,27 @@ where  1=1
   and (r.shop_id is null or r.total_reviews = 0) -- shop either has no transactions or no reviews
 group by all
 order by 2,1 asc
+
+
+
+
+-- what % of paper + party supply listings are digital 
+select
+  is_digital,
+  count(distinct a.listing_id) as listings,
+  -- case when a.quantity = 1 and (total_gms = 0 or total_gms is null) then 1 else 0 end as ooak_listing,
+  -- case when r.total_reviews = 0 or r.listing_id is null then 0 else 1 end has_listing_reviews,
+  -- count(distinct a.listing_id) as active_listings, 
+  -- count(listing_id) as viewed_listings,
+  count(sequence_number) as views,
+  sum(purchased_after_view) as purchases
+from 
+  `etsy-data-warehouse-prod.rollups.active_listing_basics` a
+inner join 
+ etsy-data-warehouse-prod.analytics.listing_views v 
+    on a.listing_id=v.listing_id
+where 1=1 
+  and top_category in ('paper_and_party_supplies')
+  and v._date >= current_date-30 
+  and v.platform in ('mobile_web','desktop','boe') 
+group by all 
