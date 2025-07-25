96371df9edc9dcb902b56d083d550d554f35678b
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-07-25 09:28:48 -0400

    Update overall_stats.sql

--- web_conversions/reviews/zero_reviews_deep_dive/overall_stats.sql
+++ web_conversions/reviews/zero_reviews_deep_dive/overall_stats.sql
@@ -45,3 +45,28 @@ left join
 where 
   active_seller_status = 1  -- only active sellers 
 group by all 
+------------------------------------------------------------------------------
+-- HOW MANY ACTIVE SHOPS DONT HAVE TRANSACTIONS + REVIEWS? 
+------------------------------------------------------------------------------
+with shop_reviews as ( -- this looks at all listings that have been purchased and whether or not they have a review
+select 
+  shop_id,
+  seller_user_id,
+  count(distinct transaction_id) as transactions,
+  sum(has_review) as total_reviews
+from 
+  etsy-data-warehouse-prod.rollups.transaction_reviews
+group by all 
+)
+select
+  case when total_reviews = 0 or r.seller_user_id is null then 0 else 1 end as has_shop_reviews,
+  case when transactions = 0 or r.transactions is null then 0 else 1 end as has_shop_transactions,
+  count(distinct b.shop_id) as active_shops
+from  
+  etsy-data-warehouse-prod.rollups.seller_basics b
+left join 
+  shop_reviews r
+   using (shop_id)
+where 
+  active_seller_status = 1  -- only active sellers 
+group by all 
