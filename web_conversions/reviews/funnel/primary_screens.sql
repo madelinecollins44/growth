439c6eb6e23977009ea2dad46f95c311698e4edf
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-07-29 14:04:12 -0400

    Update primary_screens.sql

--- web_conversions/reviews/funnel/primary_screens.sql
+++ web_conversions/reviews/funnel/primary_screens.sql
@@ -12,15 +12,15 @@ where 1=1
   and e._date >= current_date-30
   and platform in ('desktop','mobile_web')
   and event_type in (
-      -- 'hp_review_nudger_delivered', --Homepage review nudger is delivered
-      -- 'review_purchases_nav_v3_click', -- Purchases: Click on header nav "Reviews" link
+      'hp_review_nudger_delivered', --Homepage review nudger is delivered
+      'review_purchases_nav_v3_click', -- Purchases: Click on header nav "Reviews" link
       'review_form_open', -- Screen 1: Review form opened/loaded
       'review_form_selected_rating', -- Screen 1: Select star rating
-      'multistage_review_form_rating_submit'-- Screen 2: Submit star rating 
-      'multistage_review_form_text_submit'-- Screen 3: Submit text review 
-      -- 'multistage_review_form_photo_skipped', -- Screen 4: Skip review photo
-      -- 'multistage_review_form_photo_submit',-- Screen 4: Submit review photo
-      -- 'choose_your_own_review_card_clicked', -- Screen 5: Select item to review
-      -- 'choose_your_own_review_submitted' -- Screen 5: Screen after review was fired
+      'multistage_review_form_rating_submit',-- Screen 2: Submit star rating 
+      'multistage_review_form_text_submit',-- Screen 3: Submit text review 
+      'multistage_review_form_photo_skipped', -- Screen 4: Skip review photo
+      'multistage_review_form_photo_submit',-- Screen 4: Submit review photo
+      'choose_your_own_review_card_clicked', -- Screen 5: Select item to review
+      'choose_your_own_review_submitted' -- Screen 5: Screen after review was fired
       )
 group by all 
