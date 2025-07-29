dcc8b7fa8771ec671b43a2637712db0f7a2c2f7d
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-07-29 13:54:33 -0400

    Update primary_screens.sql

--- web_conversions/reviews/funnel/primary_screens.sql
+++ web_conversions/reviews/funnel/primary_screens.sql
@@ -13,14 +13,14 @@ where 1=1
   and platform in ('desktop','mobile_web')
   and event_type in (
       -- 'hp_review_nudger_delivered', --Homepage review nudger is delivered
+      -- 'review_purchases_nav_v3_click', -- Purchases: Click on header nav "Reviews" link
       'review_form_open', -- Screen 1: Review form opened/loaded
+      'review_form_selected_rating', -- Screen 1: Select star rating
       'multistage_review_form_rating_submit'-- Screen 2: Submit star rating 
       'multistage_review_form_text_submit'-- Screen 3: Submit text review 
-      -- 'hp_review_nudger_delivered',
-      -- 'review_form_selected_rating',
-      -- 'multistage_review_form_photo_skipped',
-      -- 'multistage_review_form_photo_submit',
-      -- 'review_purchases_nav_v3_click',
-      -- 'choose_your_own_review_card_clicked',
+      -- 'multistage_review_form_photo_skipped', -- Screen 4: Skip review photo
+      -- 'multistage_review_form_photo_submit',-- Screen 4: Submit review photo
+      -- 'choose_your_own_review_card_clicked', -- Screen 5: Select item to review
+      -- 'choose_your_own_review_submitted' -- Screen 5: Screen after review was fired
       )
 group by all 
