193f4ed55f27c25459ac0703d91feb9ccb37a417
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-08-05 15:50:14 -0400

    Update analysis.sql

--- web_conversions/evaluation/lv_before_atc/analysis.sql
+++ web_conversions/evaluation/lv_before_atc/analysis.sql
@@ -2,7 +2,7 @@ with first_atc as (
 select
   visit_id,
   -- split(visit_id, ".")[0] as browser_id, 
-  -- min(sequence_number) as _min
+  min(sequence_number) as sequence_number
 from 
   etsy-data-warehouse-prod.analytics.listing_views 
 where 
@@ -10,3 +10,13 @@ where
   and platform in ('boe','mobile_web','desktop')
   and added_to_cart = 1
 )
+select
+  platform,
+  case when sequence_number >= f.sequence_number then 1 else 0 end as after_atc,
+  count(sequence_number) as listing_views,
+  count(distinct listing_id) as listings
+from 
+  etsy-data-warehouse-prod.analytics.listing_views lv
+left join 
+  first_atc f
+    using (visit_id, sequence_number)
