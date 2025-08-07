6026a7da7438a18b736f04dad948a50c1a579139
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-08-07 13:40:01 -0400

    Update browser_level_analysis.sql

--- web_conversions/evaluation/lv_before_atc/browser_level_analysis.sql
+++ web_conversions/evaluation/lv_before_atc/browser_level_analysis.sql
@@ -1,15 +1,30 @@
-/*create or replace table etsy-data-warehouse-dev.madelinecollins.holder_table as (
-select  
-  platform,
-  split(visit_id, ".")[0] as browser_id, 
-  visit_id,
-  sequence_number, 
-  listing_id, 
-  added_to_cart,
-  row_number() over (partition by split(visit_id, ".")[0]  order by visit_id) AS visit_order
-from 
-  etsy-data-warehouse-prod.analytics.listing_views 
-where 
-  _date >= current_date-14
-  and platform in ('boe','mobile_web','desktop')
-); */
+/*
+create or replace table etsy-data-warehouse-dev.madelinecollins.holder_table as (
+with unique_visits as (
+  select
+    split(visit_id, ".")[0] as browser_id,
+    visit_id,
+    start_datetime,
+    platform,
+    row_number() over (
+      partition by split(visit_id, ".")[0]
+      order by start_datetime, visit_id
+    ) as visit_order
+  from etsy-data-warehouse-prod.weblog.visits
+  where _date >= current_date - 14
+    and platform in ('boe', 'mobile_web', 'desktop')
+)
+ select  
+    uv.platform,
+    uv.browser_id, 
+    lv.visit_id,
+    uv.start_datetime,
+    lv.sequence_number, 
+    lv.listing_id, 
+    lv.added_to_cart,
+    uv.visit_order
+  from etsy-data-warehouse-prod.analytics.listing_views lv
+  inner join unique_visits uv using (visit_id)
+  where lv._date >= current_date - 14
+);
+*/
