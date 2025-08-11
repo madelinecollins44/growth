c99a548c7ddf1416900668c0845a19cd07dff6a6
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-08-11 16:40:10 -0400

    Update testing.sql

--- web_conversions/experiments/star_seller_gms_before_and_after/testing.sql
+++ web_conversions/experiments/star_seller_gms_before_and_after/testing.sql
@@ -14,6 +14,32 @@ where 1=1
   and _date >= current_date-30 
 group by all 
 order by 3 desc limit 5 
-
+/* 
+_date	browser_id	visits
+2025-08-06	RGRL26AW4MkEfFpVF1mJ0aXUebad	26594
+2025-08-01	D5a2bouxu09VvD2M1lPLFu8d5zgg	14015
+2025-08-11	ALJeZ2Fs5OEthxGMDYh1OwHq5_Hl	13151
+2025-08-03	RGRL26AW4MkEfFpVF1mJ0aXUebad	11525
+2025-08-07	RGRL26AW4MkEfFpVF1mJ0aXUebad	11153
+*/
 
 -- events table 
+select 
+  _date,
+  split(visit_id, ".")[0] as browser_id,
+  count(*)
+from 
+  etsy-data-warehouse-prod.weblog.events
+where 
+  _date in ('2025-08-06')
+  and split(visit_id, ".")[0] in ('RGRL26AW4MkEfFpVF1mJ0aXUebad')
+  and event_type in ('shop_home')
+group by 1,2
+/* 
+_date	browser_id	visits
+2025-08-06	RGRL26AW4MkEfFpVF1mJ0aXUebad	26594
+2025-08-01	D5a2bouxu09VvD2M1lPLFu8d5zgg	14015
+2025-08-11	ALJeZ2Fs5OEthxGMDYh1OwHq5_Hl	13151
+2025-08-03	RGRL26AW4MkEfFpVF1mJ0aXUebad	11525
+2025-08-07	RGRL26AW4MkEfFpVF1mJ0aXUebad	11153
+*/
