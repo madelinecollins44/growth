1ff085926a59301f606ecd96a52f4725b87b25db
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-08-11 16:34:19 -0400

    Create star_seller_gms_before_and_after.sql

---
+++ web_conversions/experiments/star_seller_gms_before_and_after.sql
@@ -0,0 +1,33 @@
+/* We removed the value props from the shop home banner in June and the 'star seller' label, leaving only the star seller icon to differentiate. 
+Star sellers typically have a higher CR & GMS than non star sellers, so I am curious if we have seen this shift since this launch. This will impact our next steps.
+Catapult: https://atlas.etsycorp.com/catapult/1386410949401
+*/
+
+--------------------------------------------------------------------------------
+-- GET ALL SHOP HOME TRAFFIC INFO 
+--------------------------------------------------------------------------------
+with shop_home_traffic as (
+select
+   _date
+  , browser_id
+  , (select kv.value from unnest(properties.map) as kv where kv.key = "shop_shop_id") as shop_id
+  , count(*) as visits 
+from 
+  etsy-visit-pipe-prod.canonical.beacon_main_2025_06 -- june data
+where 1=1
+  and event_name = "shop_home"
+  and _date is not null 
+group by all 
+UNION ALL 
+select
+   _date
+  , browser_id
+  , (select kv.value from unnest(properties.map) as kv where kv.key = "shop_shop_id") as shop_id
+  , count(*) as visits 
+from 
+  etsy-visit-pipe-prod.canonical.beacon_main_2025_07 -- june data
+where 1=1
+  and event_name = "shop_home"
+  and _date is not null 
+group by all 
+); 
