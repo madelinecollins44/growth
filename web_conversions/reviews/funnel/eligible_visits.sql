ddf0fa102f8db2392156874aa22ef79938ac51b1
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-08-13 15:44:19 -0400

    Update eligible_visits.sql

--- web_conversions/reviews/funnel/eligible_visits.sql
+++ web_conversions/reviews/funnel/eligible_visits.sql
@@ -1,6 +1,12 @@
+with all_receipts as (
 select
+  buyer_user_id,
   receipt_id,
   initial_edd_min,
-  date_diff(initial_edd_min, current_date,day) as elgible_for_review
+  date_diff(initial_edd_min, current_date,day) as dates_between_edd
 from etsy-data-warehouse-prod.rollups.receipt_shipping_basics
-group by 1,2
+group by 1,2,3
+)
+select * 
+from all_receipts
+where dates_between_edd <= 100 -- can leave reviews up to 100 days after edd
