10cf49dd46a49c70392fc202df25fc5502c4b4b2
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-08-13 15:50:18 -0400

    Update eligible_visits.sql

--- web_conversions/reviews/funnel/eligible_visits.sql
+++ web_conversions/reviews/funnel/eligible_visits.sql
@@ -1,12 +1,46 @@
 with all_receipts as (
 select
-  buyer_user_id,
   receipt_id,
   initial_edd_min,
   date_diff(initial_edd_min, current_date,day) as dates_between_edd
 from etsy-data-warehouse-prod.rollups.receipt_shipping_basics
 group by 1,2,3
 )
-select * 
+, eligible_receipts as (
+select 
+  distinct receipt_id 
 from all_receipts
 where dates_between_edd <= 100 -- can leave reviews up to 100 days after edd
+)
+, browsers_for_receipts as (
+select
+  split(visit_id, ".")[0] as browser_id, 
+  count(distinct receipt_id) -- elgibile receipts for reviews
+from 
+  etsy-data-warehouse-prod.transaction_mart.receipts_visits
+inner join  
+  eligible_receipts
+    using (receipt_id)
+group by 1
+)
+-- , browser_visits as (
+select 
+  count(distinct v.visit_id) as total_visits,
+  count(distinct case when event_type in ('hp_review_nudger_delivered') then v.visit_id end) as visits_w_nudge,
+  count(distinct case when event_type in ('review_purchases_nav_v3_click') then v.visit_id end) as visits_w_nav_clicks,
+  count(distinct v.browser_id) as total_browsers,
+  count(distinct case when event_type in ('hp_review_nudger_delivered') then v.browser_id end) as browsers_w_nudge,
+  count(distinct case when event_type in ('review_purchases_nav_v3_click') then v.browser_id end) as browsers_w_nav_clicks,
+from 
+  etsy-data-warehouse-prod.weblog.visits v
+inner join 
+  etsy-data-warehouse-prod.weblog.events e
+    using (visit_id)
+inner join 
+  browsers_for_receipts b
+    on v.browser_id=b.browser_id 
+where 1=1
+  and v._date >= current_date-30
+  and e._date >= current_date-30
+  and platform in ('desktop','mobile_web')
+-- )
