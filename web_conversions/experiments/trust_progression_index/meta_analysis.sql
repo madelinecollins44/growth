648983c117b1779713040eef2f4705f816b0792a
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-08-18 16:02:17 -0400

    Update meta_analysis.sql

--- web_conversions/experiments/trust_progression_index/meta_analysis.sql
+++ web_conversions/experiments/trust_progression_index/meta_analysis.sql
@@ -241,5 +241,109 @@ inner join trust_measurements t
   and k.metric_variant_name=t.variant_id
 order by end_date,variant_id asc 
 ; 
-
 END
+
+
+/*  -- COMPARING TRUST METRICS
+with experiments as (
+select 
+  launch_id,
+  end_date, 
+  config_flag, 
+  status,
+  ramp_decision,
+  platform,
+  subteam,
+  group_name,
+  initiative
+from 
+  etsy-data-warehouse-prod.rollups.experiment_reports 
+where 1=1
+  and (trim(lower(initiative)) like '%drive conversion%')
+  and end_date >= '2025-04-01'
+  and subteam in ('RegX')
+)
+, events as (
+select 
+  launch_id,
+  a.experiment_id,
+  variant_id,
+  platform,
+  a.event_id,
+  count(*) as counts, 
+  sum(event_value) as total_events
+from 
+  etsy-data-warehouse-prod.catapult_unified.aggregated_event_daily a
+inner join experiments c on a.experiment_id = c.config_flag
+where a._date >= '2025-04-01'
+and event_id in 
+  ( /* TRUST BUILDING */
+    'view_listing',  --view listing
+  'product_details_content_toggle_open' --- open description
+  'shop_home', --- shop home
+  'cart_view', -- cart view
+  'search', --search
+  'listing_page_image_carousel_changed','image_carousel_swipe' ---image scrolling
+  'listing_page_review_engagement_frontend', -- engagement
+ /* FUNNEL PROGRESSION */
+  'backend_favorite_item2', -- favorited
+  'add_to_cart', --A2C
+  'checkout_start', --- checkout start
+  'backend_cart_payment', --- conversion rate
+  'backend_send_convo' -- convo
+  )
+group by all
+)
+-- , variants as (
+-- select
+--   *,
+--   concat("variant - ", abs(ranked1 - total_variants)) as variant_id --- cleaning up variants for google sheet
+-- from (
+-- select
+--   experiment_id,
+--   variant_id,
+--   platform,
+--   count(case when variant_id != 'off' then variant_id else null end) over (partition by experiment_id) as total_variants,
+--   case when variant_id != 'off' then rank() over (partition by experiment_id order by variant_id desc) else null end as ranked1,
+-- from `etsy-data-warehouse-dev.madelinecollins.web_trust_experiments_events_q2` 
+-- group by all)
+-- )
+select 
+  launch_id,
+  variant_id,
+  experiment_id,
+  total_trust_building_actions,
+  total_funnel_progression,
+  total_trust_building_actions/total_funnel_progression as tpi,
+  convos_sent_count
+from (
+  select
+    launch_id,
+    experiment_id,
+    variant_id,
+    platform,
+    count(case when variant_id not in ('off', 'control') then variant_id else null end) over (partition by experiment_id) as total_variants,
+    coalesce(case when variant_id not in ('off', 'control') then rank() over (partition by experiment_id order by variant_id desc) else null end,0) as ranked1,
+    sum(case when event_id in (
+        'view_listing',  --view listing
+        'listing_expand_description_open','product_details_content_toggle_open' --- open description
+        'shop_home', --- shop home
+        'cart_view', -- cart view
+        'search', --search
+        'listing_page_image_carousel_changed','image_carousel_swipe' ---image scrolling
+        'listing_page_review_engagement_frontend' -- engagement
+      ) then total_events else null end) as total_trust_building_actions,
+    sum(case when event_id in ('add_to_cart',
+        'backend_favorite_item2',
+        'checkout_start',
+        'backend_cart_payment'
+      ) then total_events else null end) as total_funnel_progression,    
+    sum(case when event_id in ('backend_send_convo') then total_events else null end) as convos_sent_count,
+  from 
+   events
+  group by all
+  )
+group by all
+order by experiment_id, variant_id desc
+
+*/
