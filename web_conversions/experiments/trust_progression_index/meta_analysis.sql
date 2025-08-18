ad45dce59424fffd25e35c6e00ac4ab82cc8cc8b
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-08-18 14:14:38 -0400

    Update meta_analysis.sql

--- web_conversions/experiments/trust_progression_index/meta_analysis.sql
+++ web_conversions/experiments/trust_progression_index/meta_analysis.sql
@@ -81,6 +81,77 @@ inner join
 ---------------------------------------------------------------------------------
 -- PULL TRUST INDICATOR METRICS
 ---------------------------------------------------------------------------------
+
+---------------------------------------------------------------------------------
+-- PULL EXPERIMENT BACKGROUND INFO 
+---------------------------------------------------------------------------------
+create or replace temp table key_metrics as (
+with experiments as (
+select 
+  launch_id,
+  end_date, 
+  config_flag, 
+  status,
+  ramp_decision,
+  platform,
+  subteam,
+  group_name,
+  initiative
+from 
+  etsy-data-warehouse-prod.rollups.experiment_reports 
+where 1=1
+  and (trim(lower(initiative)) like '%drive conversion%')
+  and end_date >= '2025-04-01'
+  and subteam in ('RegX')
+)
+, coverages as (
+select 
+  e.launch_id,
+  coverage_name,
+  coverage_value,
+from 
+  etsy-data-warehouse-prod.catapult.results_coverage_day rcd
+inner join 
+  experiments e
+   on rcd.launch_id=e.launch_id
+   and rcd._date=e.end_date
+where 1=1
+  and coverage_name in ('GMS coverage','Traffic coverage')
+  and unit in ('PERCENTAGE')
+  and lower(segmentation) in ('any')
+  and lower(segment) in('all')
+)
+select
+  e.*,
+  coverage_name,
+  coverage_value,
+  metric_display_name,
+  metric_value_control,
+  metric_value_treatment,
+  relative_change,
+  p_value
+from 
+  etsy-data-warehouse-prod.catapult.results_metric_day rmd
+inner join 
+  experiments e
+   on rmd.launch_id=e.launch_id
+   and rmd._date=e.end_date
+inner join 
+  coverages cvg
+    on cvg.launch_id=e.launch_id
+where 1=1
+  and segmentation in ('any')
+  and segment in ('all')
+  and metric_id in (
+      1029227163677, -- CR
+      1275588643427, -- GMS per Unit
+      1227229423992 -- Ads CR
+  )); 
+
+
+---------------------------------------------------------------------------------
+-- PULL TRUST INDICATOR METRICS
+---------------------------------------------------------------------------------
 create or replace temp table trust_measurements as (
 with experiments as (
 select 
@@ -130,20 +201,20 @@ and event_id in
   )
 group by all
 )
-, variants as (
-select
-  *,
-  concat("variant - ", abs(ranked1 - total_variants)) as variant_id --- cleaning up variants for google sheet
-from (
-select
-  experiment_id,
-  variant_id,
-  platform,
-  count(case when variant_id != 'off' then variant_id else null end) over (partition by experiment_id) as total_variants,
-  case when variant_id != 'off' then rank() over (partition by experiment_id order by variant_id desc) else null end as ranked1,
-from `etsy-data-warehouse-dev.madelinecollins.web_trust_experiments_events_q2` 
-group by all)
-)
+-- , variants as (
+-- select
+--   *,
+--   concat("variant - ", abs(ranked1 - total_variants)) as variant_id --- cleaning up variants for google sheet
+-- from (
+-- select
+--   experiment_id,
+--   variant_id,
+--   platform,
+--   count(case when variant_id != 'off' then variant_id else null end) over (partition by experiment_id) as total_variants,
+--   case when variant_id != 'off' then rank() over (partition by experiment_id order by variant_id desc) else null end as ranked1,
+-- from `etsy-data-warehouse-dev.madelinecollins.web_trust_experiments_events_q2` 
+-- group by all)
+-- )
 select 
   *,
   case 
@@ -166,12 +237,9 @@ from (
         'listing_page_image_carousel_changed','image_carousel_swipe' ---image scrolling
         'listing_page_review_engagement_frontend' -- engagement
       ) then total_events else null end) as total_trust_building_actions,
-    sum(case when event_id in ('add_to_cart',
-          'backend_favorite_item2',
-          'checkout_start',
-          'backend_cart_payment') then total_events else null end) as total_funnel_progression
+    sum(case when event_id in ('backend_send_convo') then total_events else null end) as convos,
   from 
-    `etsy-data-warehouse-dev.madelinecollins.web_trust_experiments_events_q2`
+    events
   group by all)
 );
 
