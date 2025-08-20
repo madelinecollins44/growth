8810cf1b25af4b2f3ea741e9ae5dcb3409ca16df
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2025-08-20 13:00:12 -0400

    Update analysis.sql

--- web_conversions/experiments/lp_rating_histogram_shop_header/analysis.sql
+++ web_conversions/experiments/lp_rating_histogram_shop_header/analysis.sql
@@ -1,36 +1,30 @@
--- CREATE TABLE TO GET RATINGS ACROSS ALL LISTINGS
-create or replace table etsy-data-warehouse-dev.madelinecollins.listings_by_ratings as ( -- looks across all purchased listings, not just viewed listings
+-- CREATE TABLE TO GET REVIEWS ACROSS ALL LISTINGS
+create or replace table etsy-data-warehouse-dev.madelinecollins.holder_table as ( -- looks across all purchased listings, not just viewed listings
 with listings_agg as (
 select
   listing_id,
   coalesce(count(distinct case when has_review > 0 then transaction_id end),0) as reviews,
-  coalesce(count(distinct case when date(transaction_date) >= current_date-365 and has_review > 0 then transaction_id end),0) as reviews_in_last_year,
-  coalesce(count(distinct case when date(transaction_date) < current_date-365 and has_review > 0 then transaction_id end),0) as  reviews_in_before_last_year,
-  coalesce(round(avg(case when date(transaction_date) >= current_date-365 then rating end),1),0) as avg_rating_in_last_year,
-  coalesce(round(avg(case when date(transaction_date) < current_date-365  then rating end),1),0) as avg_rating_in_before_last_year,
 from 
   etsy-data-warehouse-prod.rollups.transaction_reviews
 group by all 
 )
 select
+  listing_id,
   case
     when reviews = 0 then '0 reviews'
-    when reviews_in_last_year = 0 and reviews_in_before_last_year > 0 then '0 rating in past year but has reviews'
-    when round(avg_rating_in_last_year) = 5 then 'avg 5 stars rating'
-    when round(avg_rating_in_last_year) = 4 then 'avg 4 stars rating'
-    when round(avg_rating_in_last_year) = 3 then 'avg 3 stars rating'
-    when round(avg_rating_in_last_year) = 2 then 'avg 2 stars rating'
-    when round(avg_rating_in_last_year) = 1 then 'avg 1 stars rating'
+    when reviews >= 1 and reviews < 10 then '1- 9'
+    when reviews >= 10 and reviews < 30 then '10-29'
+    when reviews >= 30 and reviews < 50 then '30-50'
+    when reviews >= 50 and reviews < 100 then '50-99'
+    when reviews >= 100 then '100+'
     else 'error'
-  end as rating_status,
- listing_id
+  end as review_count,
 from 
  listings_agg
 group by all 
 );
 
 
-	
 -- CREATE TEMP TABLE TO GET ALL BROWSERS, VARIANTS BUCKETED IN EXPERIMENT
 create or replace table etsy-data-warehouse-dev.madelinecollins.bucketing_listing as (
 with listing_views as ( -- get all listing views that happened during time of experiment 
@@ -82,36 +76,28 @@ qualify row_number() over (partition by bucketing_id order by abs(timestamp_diff
 -- CREATE TEMP TABLE TO GET ALL BEACONS EVENTS 
 create or replace table etsy-data-warehouse-dev.madelinecollins.beacons_events as (
 select
-	v.visit_id,
-  split(v.visit_id, ".")[0] as bucketing_id,
+	v.browser_id,
   variant_id,
-  coalesce(case 
-    when v.visit_id = bl.visit_id and v.sequence_number >= bl.sequence_number then 1 -- if within the same visit AND on bucketing sequence number or after 
-    when v.visit_id > bl.visit_id then 1 -- after the bucketing visit_id
-  end,0) as after_bucketing_flag,
-  beacon.event_name,
-  v.sequence_number,
-  coalesce((select value from unnest(beacon.properties.key_value) where key = "listing_id"), -- view_listing
-            (regexp_extract(beacon.loc, r'listing/(\d+)')), -- reviews_anchor_click
-            (split((select value from unnest(beacon.properties.key_value) where key = "listing_ids"), ',')[offset(0)])) -- checkout_start 
+  event_name,
+  event_timestamp as sequence_number,
+  coalesce((select kv.value from unnest(properties.map) as kv where kv.key = "listing_id"), -- view_listing
+            (regexp_extract(loc, r'listing/(\d+)'))) -- reviews_anchor_click, checkout_start, listing_page_reviews_seen
   as listing_id,
 from
-	`etsy-visit-pipe-prod.canonical.visit_id_beacons` v
+	etsy-visit-pipe-prod.canonical.beacon_main_2025_06 v
 inner join 
   etsy-data-warehouse-dev.madelinecollins.bucketing_listing bl -- only looking at browsers in the experiment 
-    on bl.bucketing_id= split(v.visit_id, ".")[0] -- joining on browser_id
-    and v.visit_id >= bl.visit_id -- everything that happens on bucketing moment and after (cant do sequence number bc there is only one)
+    on bl.bucketing_id= v.browser_id -- joining on browser_id
 where
-	date(_partitiontime) between date('2025-06-13') and date('2025-06-22') -- dates of the experiment 
-	and beacon.event_name in ('reviews_anchor_click','view_listing','checkout_start','listing_page_reviews_seen')
+	_date between date('2025-06-13') and date('2025-06-22') -- dates of the experiment 
+	and event_name in ('reviews_anchor_click','view_listing','checkout_start','listing_page_reviews_seen')
 group by all 
 );
 
 -- PUT IT ALL TOGETHER
 with listing_events as ( -- get listing_id for all clicks on review signals in buy box + listing views 
 select
-	visit_id,
-  split(visit_id, ".")[0] as bucketing_id,
+	browser_id,
   variant_id,
   listing_id,
   count(case when event_name in ('view_listing') then sequence_number end) as listing_views, 
@@ -120,8 +106,6 @@ select
   count(case when event_name in ('listing_page_reviews_seen') then sequence_number end) as reviews_seen, 
 from
 	etsy-data-warehouse-dev.madelinecollins.beacons_events 
-where 
-  after_bucketing_flag > 0 -- only looks at things after bucketing moment 
 group by all 
 )
 , listing_stats as (
@@ -175,7 +159,7 @@ group by all
 )
 select
   variant_id,
-  coalesce(rating_status, 'no transaction') as rating_status,
+  coalesce(review_count, 'error') as rating_status,
   sum(listing_views) as listing_views, 
   sum(review_clicks) as review_clicks,   
   sum(checkout_starts) as checkout_starts,
@@ -187,7 +171,7 @@ select
 from 
   agg_listing_stats s
 left join 
-  etsy-data-warehouse-dev.madelinecollins.listings_by_ratings r
+  etsy-data-warehouse-dev.madelinecollins.holder_table r
     on s.listing_id=cast(r.listing_id as string) 
 group by all 
 order by 1,2 desc
